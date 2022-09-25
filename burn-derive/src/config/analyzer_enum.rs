1bcf50da74c5122f7d0c4d138bfbff13ce0d7f5b
nathaniel <nathaniel.simard.42@gmail.com>
2022-09-25 14:42:22 -0400

    feat: support enum as config

---
+++ burn-derive/src/config/analyzer_enum.rs
@@ -0,0 +1,143 @@
+use super::ConfigAnalyzer;
+use proc_macro2::{Ident, TokenStream};
+use quote::quote;
+use syn::Variant;
+
+#[derive(Debug)]
+pub struct ConfigEnumAnalyzer {
+    name: Ident,
+    data: syn::DataEnum,
+}
+
+impl ConfigEnumAnalyzer {
+    pub fn new(name: Ident, data: syn::DataEnum) -> Self {
+        Self { name, data }
+    }
+
+    fn serde_enum_ident(&self) -> Ident {
+        Ident::new(&format!("{}Serde", self.name), self.name.span())
+    }
+
+    fn gen_serde_enum(&self) -> TokenStream {
+        let enum_name = self.serde_enum_ident();
+        let data = &self.data.variants;
+
+        quote! {
+            #[derive(serde::Serialize, serde::Deserialize)]
+            enum #enum_name {
+                #data
+            }
+
+        }
+    }
+
+    fn gen_variant_field(&self, variant: &Variant) -> (TokenStream, TokenStream) {
+        let gen_fields = |num: usize| {
+            let mut input = Vec::new();
+            let mut output = Vec::new();
+
+            for i in 0..num {
+                let arg_name = Ident::new(&format!("arg_{}", i), self.name.span());
+
+                input.push(quote! { #arg_name });
+                output.push(quote! { #arg_name.clone() });
+            }
+
+            (quote! (( #(#input),* )), quote! (( #(#output),* )))
+        };
+
+        match variant.fields.is_empty() {
+            true => (quote! {}, quote! {}),
+            false => gen_fields(variant.fields.len()),
+        }
+    }
+
+    fn gen_serialize_fn(&self) -> TokenStream {
+        let enum_name = self.serde_enum_ident();
+        let variants = self.data.variants.iter().map(|variant| {
+            let variant_name = &variant.ident;
+            let (variant_input, variant_output) = self.gen_variant_field(variant);
+
+            quote! { Self::#variant_name #variant_input => #enum_name::#variant_name #variant_output }
+        });
+        let name = &self.name;
+
+        quote! {
+            impl serde::Serialize for #name {
+                fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+                where
+                    S: serde::Serializer {
+                    let serde_state = match self {
+                        #(#variants),*
+                    };
+                    serde_state.serialize(serializer)
+                }
+            }
+
+        }
+    }
+
+    fn gen_deserialize_fn(&self) -> TokenStream {
+        let enum_name = self.serde_enum_ident();
+        let variants = self.data.variants.iter().map(|variant| {
+            let variant_name = &variant.ident;
+            let (variant_input, variant_output) = self.gen_variant_field(variant);
+
+            quote! { #enum_name::#variant_name #variant_input => Self::#variant_name #variant_output }
+        });
+        let name = &self.name;
+
+        quote! {
+            impl<'de> serde::Deserialize<'de> for #name {
+                fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
+                where
+                    D: serde::Deserializer<'de> {
+                    let serde_state = #enum_name::deserialize(deserializer)?;
+                    Ok(match serde_state {
+                        #(#variants),*
+                    })
+                }
+            }
+
+        }
+    }
+}
+
+impl ConfigAnalyzer for ConfigEnumAnalyzer {
+    fn gen_constructor_impl(&self) -> TokenStream {
+        quote! {}.into()
+    }
+
+    fn gen_builder_fn_impl(&self) -> TokenStream {
+        quote! {}.into()
+    }
+
+    fn gen_serde(&self) -> TokenStream {
+        let struct_gen = self.gen_serde_enum();
+        let serialize_gen = self.gen_serialize_fn();
+        let deserialize_gen = self.gen_deserialize_fn();
+
+        quote! {
+            #struct_gen
+            #serialize_gen
+            #deserialize_gen
+        }
+    }
+
+    fn gen_clone(&self) -> TokenStream {
+        quote! {}.into()
+    }
+
+    fn gen_display(&self) -> TokenStream {
+        quote! {}.into()
+    }
+
+    fn gen_config_impl(&self) -> TokenStream {
+        let name = &self.name;
+
+        quote! {
+            impl burn::config::Config for #name {
+            }
+        }
+    }
+}
