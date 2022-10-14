ef2020ac1241575dac5ba089fd763c9ea1c5d7ee
nathaniel <nathaniel.simard.42@gmail.com>
2022-10-13 21:22:04 -0400

    refactor: ad backend decorator

--- burn-tensor/src/tensor/backend/autodiff/ops/arg.rs
+++ burn-tensor/src/tensor/backend/autodiff/ops/arg.rs
@@ -1,31 +1,21 @@
+use crate::backend::autodiff::ADBackendDecorator;
 use crate::backend::Backend;
 use crate::tensor::ops::*;
 
-macro_rules! define_impl {
-    (
-        $backend:ty,
-        $backend_inner:ty,
-        $element:ident
-    ) => {
-        impl<E: $element, const D: usize> TensorOpsArg<$backend, D>
-            for <$backend as Backend>::TensorPrimitive<D>
-        {
-            fn argmax(
-                &self,
-                dim: usize,
-            ) -> <<$backend as Backend>::IntegerBackend as Backend>::TensorPrimitive<D> {
-                TensorOpsArg::argmax(&self.tensor(), dim)
-            }
+impl<B: Backend, const D: usize> TensorOpsArg<ADBackendDecorator<B>, D>
+    for <ADBackendDecorator<B> as Backend>::TensorPrimitive<D>
+{
+    fn argmax(
+        &self,
+        dim: usize,
+    ) -> <<ADBackendDecorator<B> as Backend>::IntegerBackend as Backend>::TensorPrimitive<D> {
+        TensorOpsArg::argmax(&self.tensor(), dim)
+    }
 
-            fn argmin(
-                &self,
-                dim: usize,
-            ) -> <<$backend as Backend>::IntegerBackend as Backend>::TensorPrimitive<D> {
-                TensorOpsArg::argmin(&self.tensor(), dim)
-            }
-        }
-    };
+    fn argmin(
+        &self,
+        dim: usize,
+    ) -> <<ADBackendDecorator<B> as Backend>::IntegerBackend as Backend>::TensorPrimitive<D> {
+        TensorOpsArg::argmin(&self.tensor(), dim)
+    }
 }
-
-crate::register_tch!();
-crate::register_ndarray!();
