7c38a980c14f55563f659d54d28a0cf1988c3ba7
Nathaniel Simard <nathaniel.simard.42@gmail.com>
2022-12-01 19:43:36 -0500

    feat: improve bool tensor (#122)

--- burn-tensor/src/tensor/ops/base.rs
+++ burn-tensor/src/tensor/ops/base.rs
@@ -20,6 +20,10 @@ pub trait TensorOps<B: Backend> {
     fn bool_shape<const D: usize>(tensor: &B::BoolTensorPrimitive<D>) -> &Shape<D>;
     fn bool_to_data<const D: usize>(tensor: &B::BoolTensorPrimitive<D>) -> Data<bool, D>;
     fn bool_into_data<const D: usize>(tensor: B::BoolTensorPrimitive<D>) -> Data<bool, D>;
+    fn bool_to_device<const D: usize>(
+        tensor: &B::BoolTensorPrimitive<D>,
+        device: B::Device,
+    ) -> B::BoolTensorPrimitive<D>;
     fn bool_reshape<const D1: usize, const D2: usize>(
         tensor: &B::BoolTensorPrimitive<D1>,
         shape: Shape<D2>,
