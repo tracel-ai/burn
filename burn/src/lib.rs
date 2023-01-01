837e492349672dc11b38d800277e95e5ba863259
nathaniel <nathaniel.simard.42@gmail.com>
2023-01-01 18:38:22 -0500

    Extract in burn-core

--- burn/src/lib.rs
+++ burn/src/lib.rs
@@ -1,15 +1,1 @@
-#[macro_use]
-extern crate derive_new;
-
-pub mod config;
-pub mod data;
-pub mod module;
-pub mod nn;
-pub mod optim;
-pub mod tensor;
-pub mod train;
-
-#[cfg(test)]
-pub type TestBackend = burn_ndarray::NdArrayBackend<f32>;
-#[cfg(test)]
-pub type TestADBackend = burn_autodiff::ADBackendDecorator<TestBackend>;
+pub use burn_core::*;
