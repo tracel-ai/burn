9f584303b7383049a91d3a03abb6b4f48e431c6f
nathaniel <nathaniel.simard.42@gmail.com>
2023-01-19 17:08:20 -0500

    First draft

--- burn-tch/src/ops/module.rs
+++ burn-tch/src/ops/module.rs
@@ -1,5 +1,8 @@
-use crate::{element::TchElement, TchBackend, TchTensor};
-use burn_tensor::{ops::ModuleOps, Shape};
+use crate::{element::TchElement, TchBackend, TchKind, TchTensor};
+use burn_tensor::{
+    ops::{MaxPooling2dBackward, MaxPooling2dWithIndices, ModuleOps},
+    Shape,
+};
 
 impl<E: TchElement> ModuleOps<TchBackend<E>> for TchBackend<E> {
     fn embedding(weights: &TchTensor<E, 2>, indexes: &TchTensor<i64, 2>) -> TchTensor<E, 3> {
@@ -84,5 +87,88 @@ impl<E: TchElement> ModuleOps<TchBackend<E>> for TchBackend<E> {
             tensor,
             shape,
         }
+    }
+
+    fn max_pooling2d(
+        x: &TchTensor<E, 4>,
+        kernel_size: [usize; 2],
+        stride: [usize; 2],
+        padding: [usize; 2],
+    ) -> TchTensor<E, 4> {
+        let tensor = tch::Tensor::max_pool2d(
+            &x.tensor,
+            &[kernel_size[0] as i64, kernel_size[1] as i64],
+            &[stride[0] as i64, stride[1] as i64],
+            &[padding[0] as i64, padding[1] as i64],
+            &[1, 1],
+            false,
+        );
+        let shape = Shape::from(tensor.size());
+
+        TchTensor {
+            kind: x.kind,
+            tensor,
+            shape,
+        }
+    }
+
+    fn max_pooling2d_with_indices(
+        x: &TchTensor<E, 4>,
+        kernel_size: [usize; 2],
+        stride: [usize; 2],
+        padding: [usize; 2],
+    ) -> MaxPooling2dWithIndices<TchBackend<E>> {
+        let (tensor, indices) = tch::Tensor::max_pool2d_with_indices(
+            &x.tensor,
+            &[kernel_size[0] as i64, kernel_size[1] as i64],
+            &[stride[0] as i64, stride[1] as i64],
+            &[padding[0] as i64, padding[1] as i64],
+            &[1, 1],
+            false,
+        );
+        let shape = Shape::from(tensor.size());
+
+        let output = TchTensor {
+            kind: x.kind,
+            tensor,
+            shape,
+        };
+        let shape = Shape::from(indices.size());
+        let indices = TchTensor {
+            kind: TchKind::<i64>::new(),
+            tensor: indices,
+            shape,
+        };
+
+        MaxPooling2dWithIndices::new(output, indices)
+    }
+
+    fn max_pooling2d_backward(
+        x: &TchTensor<E, 4>,
+        kernel_size: [usize; 2],
+        stride: [usize; 2],
+        padding: [usize; 2],
+        output_grad: &TchTensor<E, 4>,
+        indices: &TchTensor<i64, 4>,
+    ) -> MaxPooling2dBackward<TchBackend<E>> {
+        let grad = tch::Tensor::max_pool2d_with_indices_backward(
+            &x.tensor,
+            &output_grad.tensor,
+            &[kernel_size[0] as i64, kernel_size[1] as i64],
+            &[stride[0] as i64, stride[1] as i64],
+            &[padding[0] as i64, padding[1] as i64],
+            &[1, 1],
+            false,
+            &indices.tensor,
+        );
+
+        let shape = Shape::from(grad.size());
+        let tensor = TchTensor {
+            kind: x.kind,
+            tensor: grad,
+            shape,
+        };
+
+        MaxPooling2dBackward::new(tensor)
     }
 }
--- burn-tensor/src/tensor/ops/modules/base.rs
+++ burn-tensor/src/tensor/ops/modules/base.rs
@@ -9,6 +9,19 @@ pub struct Conv2dBackward<B: Backend> {
     pub bias_grad: Option<B::TensorPrimitive<1>>,
 }
 
+/// Gradient computed during the backward pass for each tensor used by [max_pooling2d](ModuleOps::max_pooling2d).
+#[derive(new)]
+pub struct MaxPooling2dBackward<B: Backend> {
+    pub x_grad: B::TensorPrimitive<4>,
+}
+
+/// Gradient computed during the backward pass for each tensor used by [max_pooling2d](ModuleOps::max_pooling2d).
+#[derive(new)]
+pub struct MaxPooling2dWithIndices<B: Backend> {
+    pub output: B::TensorPrimitive<4>,
+    pub indices: <B::IntegerBackend as Backend>::TensorPrimitive<4>,
+}
+
 /// Gradient computed during the backward pass for each tensor used by [conv1d](ModuleOps::conv1d).
 #[derive(new)]
 pub struct Conv1dBackward<B: Backend> {
@@ -77,4 +90,35 @@ pub trait ModuleOps<B: Backend> {
     ) -> Conv1dBackward<B> {
         conv::conv1d_backward(x, weight, bias, stride, output_grad)
     }
+    /// Two dimensional max pooling.
+    ///
+    /// # Shapes
+    ///
+    /// x: [batch_size, channels_in, height, width],
+    fn max_pooling2d(
+        x: &B::TensorPrimitive<4>,
+        kernel_size: [usize; 2],
+        stride: [usize; 2],
+        padding: [usize; 2],
+    ) -> B::TensorPrimitive<4>;
+    /// Two dimensional max pooling with indices.
+    ///
+    /// # Shapes
+    ///
+    /// x: [batch_size, channels_in, height, width],
+    fn max_pooling2d_with_indices(
+        x: &B::TensorPrimitive<4>,
+        kernel_size: [usize; 2],
+        stride: [usize; 2],
+        padding: [usize; 2],
+    ) -> MaxPooling2dWithIndices<B>;
+    /// Backward pass for the [maxpooling2d](ModuleOps::maxpooling2d) operation.
+    fn max_pooling2d_backward(
+        x: &B::TensorPrimitive<4>,
+        kernel_size: [usize; 2],
+        stride: [usize; 2],
+        padding: [usize; 2],
+        output_grad: &B::TensorPrimitive<4>,
+        indices: &<B::IntegerBackend as Backend>::TensorPrimitive<4>,
+    ) -> MaxPooling2dBackward<B>;
 }
