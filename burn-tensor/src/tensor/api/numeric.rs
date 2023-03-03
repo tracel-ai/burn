d34c0f77095e034976f180adf32ee90b401907c6
nathaniel <nathaniel.simard.42@gmail.com>
2023-03-03 10:22:33 -0500

    Basic operations

--- burn-tensor/src/tensor/api/numeric.rs
+++ burn-tensor/src/tensor/api/numeric.rs
@@ -1,4 +1,6 @@
-use crate::{backend::Backend, ops::TensorOps, Float, Int, TensorKind, TensorNew};
+use crate::{
+    backend::Backend, ops::TensorOps, ElementConversion, Float, Int, TensorKind, TensorNew,
+};
 
 impl<B, const D: usize, K> TensorNew<B, D, K>
 where
@@ -12,6 +14,65 @@ where
     pub fn add(self, other: Self) -> Self {
         Self::new(K::add(self.primitive, other.primitive))
     }
+
+    /// Applies element wise addition operation with a scalar.
+    ///
+    /// `y = x + s`
+    pub fn add_scalar<E: ElementConversion>(self, other: E) -> Self {
+        Self::new(K::add_scalar(self.primitive, other))
+    }
+
+    /// Applies element wise substraction operation.
+    ///
+    /// `y = x2 - x1`
+    #[allow(clippy::should_implement_trait)]
+    pub fn sub(self, other: Self) -> Self {
+        Self::new(K::sub(self.primitive, other.primitive))
+    }
+
+    /// Applies element wise substraction operation with a scalar.
+    ///
+    /// `y = x - s`
+    pub fn sub_scalar<E: ElementConversion>(self, other: E) -> Self {
+        Self::new(K::sub_scalar(self.primitive, other))
+    }
+
+    /// Applies element wise division operation.
+    ///
+    /// `y = x2 / x1`
+    #[allow(clippy::should_implement_trait)]
+    pub fn div(self, other: Self) -> Self {
+        Self::new(K::div(self.primitive, other.primitive))
+    }
+
+    /// Applies element wise division operation with a scalar.
+    ///
+    /// `y = x / s`
+    pub fn div_scalar<E: ElementConversion>(self, other: E) -> Self {
+        Self::new(K::div_scalar(self.primitive, other))
+    }
+    ///
+    /// Applies element wise multiplication operation.
+    ///
+    /// `y = x2 * x1`
+    #[allow(clippy::should_implement_trait)]
+    pub fn mul(self, other: Self) -> Self {
+        Self::new(K::mul(self.primitive, other.primitive))
+    }
+
+    /// Applies element wise multiplication operation with a scalar.
+    ///
+    /// `y = x * s`
+    pub fn mul_scalar<E: ElementConversion>(self, other: E) -> Self {
+        Self::new(K::mul_scalar(self.primitive, other))
+    }
+
+    /// Switch sign of each element in the tensor.
+    ///
+    /// `y = -x`
+    pub fn neg(self) -> Self {
+        Self::new(K::neg(self.primitive))
+    }
 }
 
 /// Trait that list all operations that can be applied on all numerical tensors.
@@ -21,6 +82,26 @@ where
 /// This is an internal trait, use the public API provided by [tensor struct](TensorNew).
 pub trait Numeric<B: Backend>: TensorKind<B> {
     fn add<const D: usize>(lhs: Self::Primitive<D>, rhs: Self::Primitive<D>) -> Self::Primitive<D>;
+    fn add_scalar<const D: usize, E: ElementConversion>(
+        lhs: Self::Primitive<D>,
+        rhs: E,
+    ) -> Self::Primitive<D>;
+    fn sub<const D: usize>(lhs: Self::Primitive<D>, rhs: Self::Primitive<D>) -> Self::Primitive<D>;
+    fn sub_scalar<const D: usize, E: ElementConversion>(
+        lhs: Self::Primitive<D>,
+        rhs: E,
+    ) -> Self::Primitive<D>;
+    fn div<const D: usize>(lhs: Self::Primitive<D>, rhs: Self::Primitive<D>) -> Self::Primitive<D>;
+    fn div_scalar<const D: usize, E: ElementConversion>(
+        lhs: Self::Primitive<D>,
+        rhs: E,
+    ) -> Self::Primitive<D>;
+    fn mul<const D: usize>(lhs: Self::Primitive<D>, rhs: Self::Primitive<D>) -> Self::Primitive<D>;
+    fn mul_scalar<const D: usize, E: ElementConversion>(
+        lhs: Self::Primitive<D>,
+        rhs: E,
+    ) -> Self::Primitive<D>;
+    fn neg<const D: usize>(tensor: Self::Primitive<D>) -> Self::Primitive<D>;
 }
 
 impl<B: Backend> Numeric<B> for Int {
@@ -29,6 +110,51 @@ impl<B: Backend> Numeric<B> for Int {
         rhs: Self::Primitive<D>,
     ) -> <Int as TensorKind<B>>::Primitive<D> {
         B::IntegerBackend::add(lhs, rhs)
+    }
+    fn add_scalar<const D: usize, E: ElementConversion>(
+        lhs: Self::Primitive<D>,
+        rhs: E,
+    ) -> Self::Primitive<D> {
+        B::IntegerBackend::add_scalar(lhs, rhs.to_elem())
+    }
+    fn sub<const D: usize>(
+        lhs: Self::Primitive<D>,
+        rhs: Self::Primitive<D>,
+    ) -> <Int as TensorKind<B>>::Primitive<D> {
+        B::IntegerBackend::sub(lhs, rhs)
+    }
+    fn sub_scalar<const D: usize, E: ElementConversion>(
+        lhs: Self::Primitive<D>,
+        rhs: E,
+    ) -> Self::Primitive<D> {
+        B::IntegerBackend::sub_scalar(lhs, rhs.to_elem())
+    }
+    fn div<const D: usize>(
+        lhs: Self::Primitive<D>,
+        rhs: Self::Primitive<D>,
+    ) -> <Int as TensorKind<B>>::Primitive<D> {
+        B::IntegerBackend::div(lhs, rhs)
+    }
+    fn div_scalar<const D: usize, E: ElementConversion>(
+        lhs: Self::Primitive<D>,
+        rhs: E,
+    ) -> Self::Primitive<D> {
+        B::IntegerBackend::div_scalar(lhs, rhs.to_elem())
+    }
+    fn mul<const D: usize>(
+        lhs: Self::Primitive<D>,
+        rhs: Self::Primitive<D>,
+    ) -> <Int as TensorKind<B>>::Primitive<D> {
+        B::IntegerBackend::mul(lhs, rhs)
+    }
+    fn mul_scalar<const D: usize, E: ElementConversion>(
+        lhs: Self::Primitive<D>,
+        rhs: E,
+    ) -> Self::Primitive<D> {
+        B::IntegerBackend::mul_scalar(lhs, rhs.to_elem())
+    }
+    fn neg<const D: usize>(tensor: Self::Primitive<D>) -> Self::Primitive<D> {
+        B::IntegerBackend::neg(tensor)
     }
 }
 
@@ -39,16 +165,161 @@ impl<B: Backend> Numeric<B> for Float {
     ) -> <Float as TensorKind<B>>::Primitive<D> {
         B::add(lhs, rhs)
     }
+    fn add_scalar<const D: usize, E: ElementConversion>(
+        lhs: Self::Primitive<D>,
+        rhs: E,
+    ) -> Self::Primitive<D> {
+        B::add_scalar(lhs, rhs.to_elem())
+    }
+    fn sub<const D: usize>(
+        lhs: Self::Primitive<D>,
+        rhs: Self::Primitive<D>,
+    ) -> <Float as TensorKind<B>>::Primitive<D> {
+        B::sub(lhs, rhs)
+    }
+    fn sub_scalar<const D: usize, E: ElementConversion>(
+        lhs: Self::Primitive<D>,
+        rhs: E,
+    ) -> Self::Primitive<D> {
+        B::sub_scalar(lhs, rhs.to_elem())
+    }
+    fn div<const D: usize>(
+        lhs: Self::Primitive<D>,
+        rhs: Self::Primitive<D>,
+    ) -> <Float as TensorKind<B>>::Primitive<D> {
+        B::div(lhs, rhs)
+    }
+    fn div_scalar<const D: usize, E: ElementConversion>(
+        lhs: Self::Primitive<D>,
+        rhs: E,
+    ) -> Self::Primitive<D> {
+        B::div_scalar(lhs, rhs.to_elem())
+    }
+    fn mul<const D: usize>(
+        lhs: Self::Primitive<D>,
+        rhs: Self::Primitive<D>,
+    ) -> <Float as TensorKind<B>>::Primitive<D> {
+        B::mul(lhs, rhs)
+    }
+    fn mul_scalar<const D: usize, E: ElementConversion>(
+        lhs: Self::Primitive<D>,
+        rhs: E,
+    ) -> Self::Primitive<D> {
+        B::mul_scalar(lhs, rhs.to_elem())
+    }
+    fn neg<const D: usize>(tensor: Self::Primitive<D>) -> Self::Primitive<D> {
+        B::neg(tensor)
+    }
 }
 
-impl<B, const D: usize, K> core::ops::Add<TensorNew<B, D, K>> for TensorNew<B, D, K>
+impl<B, const D: usize, K> core::ops::Add<Self> for TensorNew<B, D, K>
 where
     B: Backend,
     K: Numeric<B>,
 {
     type Output = Self;
 
-    fn add(self, rhs: TensorNew<B, D, K>) -> Self::Output {
+    fn add(self, rhs: TensorNew<B, D, K>) -> Self {
         Self::add(self, rhs)
+    }
+}
+
+impl<E, const D: usize, B, K> core::ops::Add<E> for TensorNew<B, D, K>
+where
+    E: ElementConversion,
+    B: Backend,
+    K: Numeric<B>,
+{
+    type Output = Self;
+
+    fn add(self, other: E) -> Self {
+        TensorNew::add_scalar(self, other)
+    }
+}
+
+impl<B, const D: usize, K> core::ops::Sub<TensorNew<B, D, K>> for TensorNew<B, D, K>
+where
+    B: Backend,
+    K: Numeric<B>,
+{
+    type Output = Self;
+
+    fn sub(self, rhs: TensorNew<B, D, K>) -> Self {
+        TensorNew::sub(self, rhs)
+    }
+}
+
+impl<E, const D: usize, B, K> core::ops::Sub<E> for TensorNew<B, D, K>
+where
+    E: ElementConversion,
+    B: Backend,
+    K: Numeric<B>,
+{
+    type Output = Self;
+
+    fn sub(self, other: E) -> Self {
+        TensorNew::sub_scalar(self, other)
+    }
+}
+
+impl<B, const D: usize, K> core::ops::Div<TensorNew<B, D, K>> for TensorNew<B, D, K>
+where
+    B: Backend,
+    K: Numeric<B>,
+{
+    type Output = Self;
+
+    fn div(self, rhs: TensorNew<B, D, K>) -> Self {
+        TensorNew::div(self, rhs)
+    }
+}
+
+impl<E, const D: usize, B, K> core::ops::Div<E> for TensorNew<B, D, K>
+where
+    E: ElementConversion,
+    B: Backend,
+    K: Numeric<B>,
+{
+    type Output = Self;
+
+    fn div(self, other: E) -> Self {
+        TensorNew::div_scalar(self, other)
+    }
+}
+
+impl<B, const D: usize, K> core::ops::Mul<TensorNew<B, D, K>> for TensorNew<B, D, K>
+where
+    B: Backend,
+    K: Numeric<B>,
+{
+    type Output = Self;
+
+    fn mul(self, rhs: TensorNew<B, D, K>) -> Self {
+        TensorNew::mul(self, rhs)
+    }
+}
+
+impl<E, const D: usize, B, K> core::ops::Mul<E> for TensorNew<B, D, K>
+where
+    E: ElementConversion,
+    B: Backend,
+    K: Numeric<B>,
+{
+    type Output = Self;
+
+    fn mul(self, other: E) -> Self {
+        TensorNew::mul_scalar(self, other)
+    }
+}
+
+impl<B, const D: usize, K> core::ops::Neg for TensorNew<B, D, K>
+where
+    B: Backend,
+    K: Numeric<B>,
+{
+    type Output = Self;
+
+    fn neg(self) -> Self {
+        TensorNew::neg(self)
     }
 }
