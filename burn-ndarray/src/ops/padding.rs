c1a27203c8602852a34fd23a663d3c54c13d0702
nathaniel <nathaniel.simard.42@gmail.com>
2023-03-10 19:04:17 -0500

    improve ndarray maxpool

--- burn-ndarray/src/ops/padding.rs
+++ burn-ndarray/src/ops/padding.rs
@@ -1,41 +1,23 @@
-use crate::{element::FloatNdArrayElement, tensor::NdArrayTensor, NdArrayBackend, NdArrayDevice};
-use burn_tensor::{ops::TensorOps, Shape};
-
-pub(crate) fn apply_padding2d<E: FloatNdArrayElement>(
-    x: NdArrayTensor<E, 2>,
-    padding: [usize; 2],
-) -> NdArrayTensor<E, 2> {
-    let [heigth, width] = x.shape().dims;
-
-    let heigth_new = heigth + (2 * padding[0]);
-    let width_new = width + (2 * padding[1]);
-
-    let mut x_new = NdArrayBackend::zeros(Shape::new([heigth_new, width_new]), &NdArrayDevice::Cpu);
-    x_new = NdArrayBackend::index_assign(
-        x_new,
-        [
-            padding[0]..heigth + padding[0],
-            padding[1]..width + padding[1],
-        ],
-        x,
-    );
-
-    x_new
-}
+use crate::{element::FloatNdArrayElement, tensor::NdArrayTensor, NdArrayBackend};
+use burn_tensor::ops::TensorOps;
+use ndarray::Array4;
 
 pub(crate) fn apply_padding_4d<E: FloatNdArrayElement>(
     x: NdArrayTensor<E, 4>,
     padding: [usize; 2],
+    elem: E,
 ) -> NdArrayTensor<E, 4> {
     let [batch_size, input_channels, height, width] = x.shape().dims;
     let [padding_height, padding_width] = padding;
     let padded_height = height + 2 * padding_height;
     let padded_width = width + 2 * padding_width;
 
-    let mut x_new = NdArrayBackend::zeros(
-        Shape::new([batch_size, input_channels, padded_height, padded_width]),
-        &NdArrayDevice::Cpu,
+    let x_new = Array4::from_elem(
+        (batch_size, input_channels, padded_height, padded_width),
+        elem,
     );
+    let mut x_new = NdArrayTensor::new(x_new.into_shared().into_dyn());
+
     x_new = NdArrayBackend::index_assign(
         x_new,
         [
