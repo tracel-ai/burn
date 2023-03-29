e18b84586a14edbb4404e342d3ce278bc4b8e3aa
Mathias <mathiaswi177@gmail.com>
2023-03-29 00:52:39 -0700

    Work on test

--- burn-tensor/src/tests/stats/basic.rs
+++ burn-tensor/src/tests/stats/basic.rs
@@ -16,8 +16,8 @@ mod tests {
 
     #[test]
     fn test_display_2d_tensor() {
-        let data = Data::from([[1, 2, 3], [4, 5, 6], [7, 8, 9]]);
-        let tensor = Tensor::<TestBackend, 2>::from_data(data);
+        let data = Data::from([[1.0, 2.0, 3.0], [4.0, 5.0, 6.0], [7.0, 8.0, 9.0]]); // Is there any sort of narrowing conversion to Int?
+        let tensor = Tensor::<burn_tensor::tensor<Int>, 2, Int>::from_data(data);
 
         let output = format!("{}", tensor);
         let expected = "Tensor {\n  data: [[1, 2, 3], [4, 5, 6], [7, 8, 9]],\n  shape:   [3, 3],\n  device:  Cpu,\n  backend: ndarray,\n  dtype:   int,\n}";
