11b278a65230b1106fd1d1457afa56e248eb50d4
Mathias <mathiaswi177@gmail.com>
2023-03-29 22:52:24 -0700

    Attempt 2 at figuring out imports

--- burn-tensor/src/tests/stats/basic.rs
+++ burn-tensor/src/tests/stats/basic.rs
@@ -2,6 +2,7 @@
 mod tests {
     use super::*;
     use burn_tensor::{Data, Tensor};
+    use burn_ndarray::NdArrayBackend; // Well this doesn't work
 
     #[test]
     fn test_var() {
@@ -17,7 +18,7 @@ mod tests {
     #[test]
     fn test_display_2d_tensor() {
         let data = Data::from([[1.0, 2.0, 3.0], [4.0, 5.0, 6.0], [7.0, 8.0, 9.0]]); // Is there any sort of narrowing conversion to Int?
-        let tensor = Tensor::<burn_tensor::tensor<Int>, 2, Int>::from_data(data);
+        let tensor: burn_tensor::Tensor<burn_ndarray::NdArrayBackend<f32>, 2, burn_tensor::Int> = Tensor::from_data(data);
 
         let output = format!("{}", tensor);
         let expected = "Tensor {\n  data: [[1, 2, 3], [4, 5, 6], [7, 8, 9]],\n  shape:   [3, 3],\n  device:  Cpu,\n  backend: ndarray,\n  dtype:   int,\n}";
