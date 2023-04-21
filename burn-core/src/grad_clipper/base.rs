6ee07934b7dcb0c3a3ccc39b1be8cb8b9adf66f0
Mathias <mathiaswi177@gmail.com>
2023-04-20 22:21:52 -0700

    clip_by_value first implementation, have type restrictions to worry about still

--- burn-core/src/grad_clipper/base.rs
+++ burn-core/src/grad_clipper/base.rs
@@ -1,22 +1,21 @@
-use burn_tensor::{backend::Backend, Tensor};
-
+use burn_tensor::{backend::Backend, Tensor, Numeric, Element, TensorKind, Float, BasicOps};
 /// Gradient Clipper provides a way to mitigate the exploding gradients
 /// problem by clipping every component of the gradient to a value during 
 /// backpropagation.
 pub struct GradientClipper {
-    clip_value: Option<f64>,
-    clip_norm: Option<f64>,
+    clip_value: Option<f32>,
+    clip_norm: Option<f32>,
 }
 
 impl GradientClipper {
-    pub fn by_value(threshold: f64) -> Self {
+    pub fn by_value(threshold: f32) -> Self {
         Self {
             clip_value: Some(threshold),
             clip_norm: None,
         }
     }
 
-    pub fn by_norm(threshold: f64) -> Self {
+    pub fn by_norm(threshold: f32) -> Self {
         Self {
             clip_value: None,
             clip_norm: Some(threshold),
@@ -31,12 +30,23 @@ impl GradientClipper {
         }
     }
 
-    fn clip_by_value<B: Backend, const D: usize>(&self, grad: Tensor<B, D>, threshold: f64) -> Tensor<B, D> {
-        todo!()
-        // Need something like a clamp() function
+    fn clip_by_value<B: Backend, const D: usize>(&self, grad: Tensor<B, D>, threshold: f32) -> Tensor<B, D> 
+    where
+        B::FloatElem: core::cmp::PartialOrd<f32> + From<f32>,
+    {
+        let mut grad_data = grad.to_data();
+        grad_data.value.iter_mut().for_each(|val| {
+            if *val > threshold {
+                *val = threshold.into();
+            } else if *val < -threshold {
+                *val = (-threshold).into();
+            }
+        });
+
+        Tensor::from_data_device(grad_data, &grad.device())
     }
 
-    fn clip_by_norm<B: Backend, const D: usize>(&self, grad: Tensor<B, D>, threshold: f64) -> Tensor<B, D> {
+    fn clip_by_norm<B: Backend, const D: usize>(&self, grad: Tensor<B, D>, threshold: f32) -> Tensor<B, D> {
         todo!()
         // Does Tensor have a norm() function?
     }
