03f318d054a0a354c980dc7928f19d6477771b28
Mathias <mathiaswi177@gmail.com>
2023-04-23 13:48:35 -0700

    Get right types for comparisons

--- burn-core/src/grad_clipper/base.rs
+++ burn-core/src/grad_clipper/base.rs
@@ -1,46 +1,30 @@
-use burn_tensor::{backend::Backend};
+use burn_tensor::{backend::Backend, ElementConversion};
 use crate::tensor::Tensor;
 /// Gradient Clipper provides a way to mitigate the exploding gradients
 /// problem by clipping every component of the gradient to a value during 
 /// backpropagation.
-pub struct GradientClipper {
-    clip_value: Option<f32>,
-    clip_norm: Option<f32>,
+pub enum GradientClipper {
+    ClipByValue(f32),
+    ClipByNorm(f32),
 }
 
 impl GradientClipper {
-    pub fn by_value(threshold: f32) -> Self {
-        Self {
-            clip_value: Some(threshold),
-            clip_norm: None,
-        }
-    }
-
-    pub fn by_norm(threshold: f32) -> Self {
-        Self {
-            clip_value: None,
-            clip_norm: Some(threshold),
-        }
-    }
 
     pub fn clip_gradient<B: Backend, const D: usize>(&self, grad: Tensor<B, D>) -> Tensor<B, D> {
-        match (self.clip_value, self.clip_norm) {
-            (Some(clip_value), None) => self.clip_by_value(grad, clip_value),
-            (None, Some(clip_norm)) => self.clip_by_norm(grad, clip_norm),
-            _ => panic!("Gradient Clipper must have one (and only one) clip_value or clip_norm."),
+        match self {
+            GradientClipper::ClipByValue(threshold) => self.clip_by_value(grad, *threshold),
+            GradientClipper::ClipByNorm(max_norm) => self.clip_by_norm(grad, *max_norm),
         }
     }
 
-    fn clip_by_value<B: Backend, const D: usize>(&self, grad: Tensor<B, D>, threshold: f32) -> Tensor<B, D> 
-    where
-        B::FloatElem: core::cmp::PartialOrd<f32> + From<f32>, // this doesn't feel right
-    {
+    fn clip_by_value<B: Backend, const D: usize>(&self, grad: Tensor<B, D>, threshold: f32) -> Tensor<B, D> {
         let mut grad_data = grad.to_data();
         grad_data.value.iter_mut().for_each(|val| {
-            if *val > threshold {
-                *val = threshold.into();
-            } else if *val < -threshold {
-                *val = (-threshold).into();
+            let f_val: f32 = val.elem();
+            if f_val > threshold {
+                *val = threshold.elem();
+            } else if f_val < -threshold {
+                *val = (-threshold).elem();
             }
         });
 
@@ -57,8 +41,8 @@ impl GradientClipper {
 
     fn clip_by_norm<B: Backend, const D: usize>(&self, grad: Tensor<B, D>, threshold: f32) -> Tensor<B, D> {
         let norm = Self::l2_norm(&grad);
-        let norm_float = norm.into_scalar();
-        if norm_float > threshold { // FloatElem can't be compared to f32?
+        let norm_float = norm.into_scalar().elem::<f32>();
+        if norm_float > threshold {
             let scale = threshold / norm_float;
             let scaled_grad = grad.mul_scalar(scale);
             scaled_grad
