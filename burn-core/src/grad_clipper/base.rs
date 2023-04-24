4042a820f8b64d276f4f1d34879e19a66c52e89a
Mathias <mathiaswi177@gmail.com>
2023-04-23 23:14:10 -0700

    Go back to struct and derive from Config

--- burn-core/src/grad_clipper/base.rs
+++ burn-core/src/grad_clipper/base.rs
@@ -1,23 +1,53 @@
-use burn_tensor::{backend::Backend, ElementConversion};
+use crate as burn;
+use crate::config::Config;
 use crate::tensor::Tensor;
-/// Gradient Clipper provides a way to mitigate the exploding gradients
-/// problem by clipping every component of the gradient to a value during 
+use burn_tensor::{backend::Backend, ElementConversion};
+
+/// Gradient Clipper provides a way to mitigate exploding gradients
+/// by clipping every component of the gradient to a value during
 /// backpropagation.
-pub enum GradientClipper {
-    ClipByValue(f32),
-    ClipByNorm(f32),
+#[derive(Config)]
+pub struct GradientClipper {
+    #[config(default = "Some(1.0)")] // ngl this is confusing
+    pub clip_value: Option<f32>,
+    #[config(default = "Some(1.0)")]
+    pub clip_norm: Option<f32>,
 }
 
 impl GradientClipper {
-
-    pub fn clip_gradient<B: Backend, const D: usize>(&self, grad: Tensor<B, D>) -> Tensor<B, D> {
-        match self {
-            GradientClipper::ClipByValue(threshold) => self.clip_by_value(grad, *threshold),
-            GradientClipper::ClipByNorm(max_norm) => self.clip_by_norm(grad, *max_norm),
+    pub fn new() -> Self {
+        Self {
+            clip_value: None,
+            clip_norm: None,
+        }
+    }
+    pub fn by_value(threshold: f32) -> Self {
+        Self {
+            clip_value: Some(threshold),
+            clip_norm: None,
         }
     }
 
-    fn clip_by_value<B: Backend, const D: usize>(&self, grad: Tensor<B, D>, threshold: f32) -> Tensor<B, D> {
+    pub fn by_norm(threshold: f32) -> Self {
+        Self {
+            clip_value: None,
+            clip_norm: Some(threshold),
+        }
+    }
+
+    pub fn clip_gradient<B: Backend, const D: usize>(&self, grad: Tensor<B, D>) -> Tensor<B, D> {
+        match (self.clip_value, self.clip_norm) {
+            (Some(clip_value), None) => self.clip_by_value(grad, clip_value),
+            (None, Some(clip_norm)) => self.clip_by_norm(grad, clip_norm),
+            _ => panic!("Gradient Clipper must have one (and only one) clip_value or clip_norm."),
+        }
+    }
+
+    fn clip_by_value<B: Backend, const D: usize>(
+        &self,
+        grad: Tensor<B, D>,
+        threshold: f32,
+    ) -> Tensor<B, D> {
         let mut grad_data = grad.to_data();
         grad_data.value.iter_mut().for_each(|val| {
             let f_val: f32 = val.elem();
@@ -39,7 +69,11 @@ impl GradientClipper {
         norm
     }
 
-    fn clip_by_norm<B: Backend, const D: usize>(&self, grad: Tensor<B, D>, threshold: f32) -> Tensor<B, D> {
+    fn clip_by_norm<B: Backend, const D: usize>(
+        &self,
+        grad: Tensor<B, D>,
+        threshold: f32,
+    ) -> Tensor<B, D> {
         let norm = Self::l2_norm(&grad);
         let norm_float = norm.into_scalar().elem::<f32>();
         if norm_float > threshold {
