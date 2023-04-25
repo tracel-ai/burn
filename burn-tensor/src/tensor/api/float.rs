deddf743c49acbd2ad79daf3f9dc89fe9be69076
Karel Nagel <nagelkarel@gmail.com>
2023-04-25 15:40:07 +0300

    permute

--- burn-tensor/src/tensor/api/float.rs
+++ burn-tensor/src/tensor/api/float.rs
@@ -355,6 +355,13 @@ where
     pub fn flip(&self, dims: Vec<usize>) -> Self {
         Self::new(B::flip(self.primitive.clone(), dims))
     }
+    pub fn permute(&self, dims: Shape<D>) -> Self {
+        let mut tensor = self.clone();
+        for (current, new) in dims.dims.iter().enumerate() {
+            tensor = tensor.swap_dims(current, *new)
+        }
+        tensor
+    }
 }
 
 impl<const D: usize, B: ADBackend> Tensor<B, D> {
