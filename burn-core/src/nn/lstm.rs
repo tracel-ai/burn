11b270fcbbe67fd9cf4f87978d55ce9169e8a322
Mathias <mathiaswi177@gmail.com>
2023-05-22 23:33:50 -0700

    Add in struct fields for hidden and cell state, clarify this is unidirectional and stateful

--- burn-core/src/nn/lstm.rs
+++ burn-core/src/nn/lstm.rs
@@ -1,11 +1,12 @@
 use crate as burn;
 
+use crate::module::Param;
 use crate::config::Config;
 use crate::module::Module;
 use crate::tensor::backend::Backend;
 use crate::tensor::Tensor;
 
-use super::Initializer;
+use super::{Initializer, Linear, LinearConfig};
 
 #[derive(Config)]
 pub struct LSTMConfig {
@@ -18,4 +19,48 @@ pub struct LSTMConfig {
     /// The type of function used to initialize LSTM parameters
     #[config(default = "Initializer::UniformDefault")]
     pub initializer: Initializer,
+    /// The batch size
+    pub batch_size: usize,
+}
+
+/// The LSTM module. This implementation is for a unidirectional, stateful, LSTM.
+#[derive(Module, Debug)]
+pub struct LSTM<B: Backend> {
+    input_gate: Linear<B>,
+    forget_gate: Linear<B>,
+    output_gate: Linear<B>,
+    cell_gate: Linear<B>,
+    hidden_state: Option<Param<Tensor<B, 2>>>,
+    cell_state: Option<Param<Tensor<B, 2>>>,
+}
+
+impl LSTMConfig {
+    /// Initialize a new [lstm](LSTM) module
+    pub fn init<B: Backend>(&self) -> LSTM<B> {
+        let d_output = self.d_hidden;
+
+        let input_gate = LinearConfig{ d_input: self.d_input, d_output, bias: self.bias, initializer: self.initializer.clone() }.init();
+        let forget_gate = LinearConfig{ d_input: self.d_input, d_output, bias: self.bias, initializer: self.initializer.clone() }.init();
+        let output_gate = LinearConfig{ d_input: self.d_input, d_output, bias: self.bias, initializer: self.initializer.clone() }.init();
+        let cell_gate = LinearConfig{ d_input: self.d_input, d_output, bias: self.bias, initializer: self.initializer.clone() }.init();
+
+        let hidden_state = Some(Param::from(Tensor::zeros([self.batch_size, self.d_hidden])));
+        let cell_state = Some(Param::from(Tensor::zeros([self.batch_size, self.d_hidden])));
+
+        LSTM {
+            input_gate,
+            forget_gate,
+            output_gate,
+            cell_gate,
+            hidden_state,
+            cell_state,
+        }
+    }
+}
+
+impl<B: Backend> LSTM<B> {
+    /// Applies the forward pass on the input tensor
+    pub fn forward<const D: usize>(&self, input: Tensor<B, D>, state: Option<(Tensor<B, D>, Tensor<B, D>)>) -> (Tensor<B, D>, Tensor<B, D>, Tensor<B, D>) {
+
+    }
 }
