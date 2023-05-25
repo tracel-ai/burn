beaab066a9e3f31d0f1fa91d1183c872a27af363
nathaniel <nathaniel.simard.42@gmail.com>
2023-05-25 10:47:27 -0400

    feat: burn import logging

---
+++ burn-import/src/logger.rs
@@ -0,0 +1,30 @@
+use log::{LevelFilter, SetLoggerError};
+use log4rs::{
+    append::console::ConsoleAppender,
+    config::{Appender, Root},
+    Config,
+};
+
+pub fn init_log() -> Result<(), SetLoggerError> {
+    let stdout = ConsoleAppender::builder().build();
+    let appender = Appender::builder().build("stdout", Box::new(stdout));
+
+    log4rs::init_config(
+        Config::builder()
+            .appender(appender)
+            .build(Root::builder().appender("stdout").build(LevelFilter::Debug))
+            .unwrap(),
+    )?;
+    update_panic_hook();
+
+    Ok(())
+}
+
+fn update_panic_hook() {
+    let hook = std::panic::take_hook();
+
+    std::panic::set_hook(Box::new(move |info| {
+        log::error!("PANIC => {}", info.to_string());
+        hook(info);
+    }));
+}
