3140bb12229d87629e68ada2b7f87facc301413f
nathaniel <nathaniel.simard.42@gmail.com>
2023-05-25 10:52:39 -0400

    Don't show date

--- burn-import/src/logger.rs
+++ burn-import/src/logger.rs
@@ -2,11 +2,14 @@ use log::{LevelFilter, SetLoggerError};
 use log4rs::{
     append::console::ConsoleAppender,
     config::{Appender, Root},
+    encode::pattern::PatternEncoder,
     Config,
 };
 
 pub fn init_log() -> Result<(), SetLoggerError> {
-    let stdout = ConsoleAppender::builder().build();
+    let stdout = ConsoleAppender::builder()
+        .encoder(Box::new(PatternEncoder::new("[{h({l})} - {f}:{L}] {m}{n}")))
+        .build();
     let appender = Appender::builder().build("stdout", Box::new(stdout));
 
     log4rs::init_config(
