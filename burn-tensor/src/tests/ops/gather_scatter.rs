02056f749fe903ad6bbc178004681426656b182c
nathaniel <nathaniel.simard.42@gmail.com>
2023-05-27 09:52:15 -0400

    Add more tests

--- burn-tensor/src/tests/ops/gather_scatter.rs
+++ burn-tensor/src/tests/ops/gather_scatter.rs
@@ -40,6 +40,25 @@ mod tests {
     }
 
     #[test]
+    fn should_gather_3d_dim1() {
+        let tensor = TestTensor::from_floats([
+            [[0.0, 1.0, 2.0], [3.0, 4.0, 5.0]],
+            [[6.0, 7.0, 8.0], [9.0, 10.0, 11.0]],
+        ]);
+        let indexes = TestTensorInt::from_ints([[[1, 0, 0], [0, 1, 0]], [[0, 0, 1], [0, 1, 1]]]);
+
+        let output = tensor.gather(1, indexes);
+
+        assert_eq!(
+            output.into_data(),
+            Data::from([
+                [[3.0, 1.0, 2.0], [0.0, 4.0, 2.0]],
+                [[6.0, 7.0, 11.0], [6.0, 10.0, 11.0]]
+            ])
+        );
+    }
+
+    #[test]
     fn should_gather_2d_only_1dim() {
         let tensor = TestTensor::from_floats([[0.0, 1.0, 2.0], [3.0, 4.0, 5.0]]);
         let indexes = TestTensorInt::from_ints([[1, 2]]).reshape([2, 1]);
@@ -85,6 +104,29 @@ mod tests {
         assert_eq!(
             output.into_data(),
             Data::from([[2.0, 1.0, 3.0], [6.0, 4.0, 5.0]])
+        );
+    }
+
+    #[test]
+    fn should_scatter_3d_dim1() {
+        let tensor = TestTensor::from_floats([
+            [[0.0, 1.0, 2.0], [3.0, 4.0, 5.0]],
+            [[6.0, 7.0, 8.0], [9.0, 10.0, 11.0]],
+        ]);
+        let values = TestTensor::from_floats([
+            [[12.0, 13.0, 14.0], [15.0, 16.0, 17.0]],
+            [[18.0, 19.0, 20.0], [21.0, 22.0, 23.0]],
+        ]);
+        let indexes = TestTensorInt::from_ints([[[1, 0, 0], [0, 1, 0]], [[0, 0, 1], [0, 1, 1]]]);
+
+        let output = tensor.scatter(1, indexes, values);
+
+        assert_eq!(
+            output.into_data(),
+            Data::from([
+                [[15.0, 14.0, 33.0], [15.0, 20.0, 5.0]],
+                [[45.0, 26.0, 8.0], [9.0, 32.0, 54.0]]
+            ])
         );
     }
 }
