e1458ef91066716741514d00c2b5a2a676a100bb
nathaniel <nathaniel.simard.42@gmail.com>
2023-05-27 11:13:40 -0400

    Add from ints doc

--- burn-tensor/src/tensor/api/int.rs
+++ burn-tensor/src/tensor/api/int.rs
@@ -19,6 +19,19 @@ impl<const D: usize, B> Tensor<B, D, Int>
 where
     B: Backend,
 {
+    /// Create a tensor from integers (i32).
+    ///
+    /// # Example
+    ///
+    /// ```rust
+    /// use burn_tensor::backend::Backend;
+    /// use burn_tensor::{Tensor, Int};
+    ///
+    /// fn example<B: Backend>() {
+    ///     let _x: Tensor<B, 1, Int> = Tensor::from_ints([1, 2]);
+    ///     let _y: Tensor<B, 2, Int> = Tensor::from_ints([[1, 2], [3, 4]]);
+    /// }
+    /// ```
     pub fn from_ints<A: Into<Data<i32, D>>>(ints: A) -> Self {
         Self::from_data(ints.into().convert())
     }
