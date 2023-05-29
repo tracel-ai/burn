68d7cc78e014ebe7089bf459cb36aded8e0a4059
Mathias <mathiaswi177@gmail.com>
2023-05-29 16:15:20 -0700

    Fmt and clarify what create_with_weights() is for

--- burn-core/src/nn/lstm/gate_controller.rs
+++ burn-core/src/nn/lstm/gate_controller.rs
@@ -1,12 +1,12 @@
 use crate as burn;
 use crate::nn::LinearRecord;
 
-use burn_tensor::Tensor;
-use burn_tensor::backend::Backend;
 use crate::module::Module;
+use crate::nn::Initializer;
 use crate::nn::Linear;
 use crate::nn::LinearConfig;
-use crate::nn::Initializer;
+use burn_tensor::backend::Backend;
+use burn_tensor::Tensor;
 
 #[derive(Module, Debug)]
 pub struct GateController<B: Backend> {
@@ -17,27 +17,35 @@ pub struct GateController<B: Backend> {
 }
 
 impl<B: Backend> GateController<B> {
-    pub fn new(d_input: usize, d_output: usize, bias: bool, initializer: Initializer) -> GateController<B> {
-        GateController { 
-            input_transform: LinearConfig{ d_input: d_input, d_output: d_output, bias: bias, initializer: initializer.clone() }.init(), 
-            hidden_transform: LinearConfig{ d_input: d_output, d_output: d_output, bias: bias, initializer: initializer.clone() }.init(), 
-        }
-    }
-
-    pub fn create_with_weights(d_input: usize, d_output: usize, bias: bool, initializer: Initializer, input_record: LinearRecord<B>, hidden_record: LinearRecord<B>) -> GateController<B> {
-        let l1 = LinearConfig{ d_input: d_input, d_output: d_output, bias: bias, initializer: initializer.clone() }.init_with(input_record);
-        let l2 = LinearConfig{ d_input: d_input, d_output: d_output, bias: bias, initializer: initializer.clone() }.init_with(hidden_record);
+    pub fn new(
+        d_input: usize,
+        d_output: usize,
+        bias: bool,
+        initializer: Initializer,
+    ) -> GateController<B> {
         GateController {
-            input_transform:  l1,
-            hidden_transform: l2
-        }   
+            input_transform: LinearConfig {
+                d_input: d_input,
+                d_output: d_output,
+                bias: bias,
+                initializer: initializer.clone(),
+            }
+            .init(),
+            hidden_transform: LinearConfig {
+                d_input: d_output,
+                d_output: d_output,
+                bias: bias,
+                initializer: initializer.clone(),
+            }
+            .init(),
+        }
     }
 
     pub fn get_input_weight(&self) -> Tensor<B, 2> {
         self.input_transform.get_weight()
     }
 
-    pub fn get_hidden_weight(&self) -> Tensor<B, 2>{
+    pub fn get_hidden_weight(&self) -> Tensor<B, 2> {
         self.hidden_transform.get_weight()
     }
 
@@ -48,5 +56,33 @@ impl<B: Backend> GateController<B> {
     pub fn get_hidden_bias(&self) -> Option<Tensor<B, 1>> {
         self.hidden_transform.get_bias()
     }
-}
 
+    /// Only used for testing in lstm
+    pub fn create_with_weights(
+        d_input: usize,
+        d_output: usize,
+        bias: bool,
+        initializer: Initializer,
+        input_record: LinearRecord<B>,
+        hidden_record: LinearRecord<B>,
+    ) -> GateController<B> {
+        let l1 = LinearConfig {
+            d_input: d_input,
+            d_output: d_output,
+            bias: bias,
+            initializer: initializer.clone(),
+        }
+        .init_with(input_record);
+        let l2 = LinearConfig {
+            d_input: d_input,
+            d_output: d_output,
+            bias: bias,
+            initializer: initializer.clone(),
+        }
+        .init_with(hidden_record);
+        GateController {
+            input_transform: l1,
+            hidden_transform: l2,
+        }
+    }
+}
