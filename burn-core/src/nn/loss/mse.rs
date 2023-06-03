c09a28cde14452aa59aa204005e17fd9d99fe3a8
Yu Sun <ysun@sctmes.com>
2023-06-03 07:59:07 +0000

    fix(mse-loss): modify the mse loss to make it adaptable to tensors of any dimension

--- burn-core/src/nn/loss/mse.rs
+++ burn-core/src/nn/loss/mse.rs
@@ -8,6 +8,11 @@ pub struct MSELoss<B: Backend> {
     backend: PhantomData<B>,
 }
 
+pub enum Reduction {
+    Mean,
+    Sum,
+}
+
 impl<B: Backend> Default for MSELoss<B> {
     fn default() -> Self {
         Self::new()
@@ -28,16 +33,25 @@ impl<B: Backend> MSELoss<B> {
     ///
     /// - logits: [batch_size, num_targets]
     /// - targets: [batch_size, num_targets]
-    pub fn forward(&self, logits: Tensor<B, 2>, targets: Tensor<B, 2>) -> Tensor<B, 1> {
-        self.forward_none(logits, targets).mean()
+    pub fn forward<const D: usize>(
+        &self,
+        logits: Tensor<B, D>,
+        targets: Tensor<B, D>,
+        reduction: Reduction,
+    ) -> Tensor<B, 1> {
+        let tensor = self.forward_no_reduction(logits, targets);
+        match reduction {
+            Reduction::Mean => tensor.mean(),
+            Reduction::Sum => tensor.sum(),
+        }
     }
 
-    pub fn forward_none(&self, logits: Tensor<B, 2>, targets: Tensor<B, 2>) -> Tensor<B, 2> {
+    pub fn forward_no_reduction<const D: usize>(
+        &self,
+        logits: Tensor<B, D>,
+        targets: Tensor<B, D>,
+    ) -> Tensor<B, D> {
         logits.sub(targets).powf(2.0)
-    }
-
-    pub fn forward_sum(&self, logits: Tensor<B, 2>, targets: Tensor<B, 2>) -> Tensor<B, 1> {
-        self.forward_none(logits, targets).sum()
     }
 }
 
@@ -45,37 +59,24 @@ impl<B: Backend> MSELoss<B> {
 mod tests {
     use super::*;
     use crate::TestBackend;
-    use burn_tensor::{loss::mse, Data, Distribution};
+    use burn_tensor::Data;
 
     #[test]
     fn test_mse_loss() {
-        let [batch_size, num_targets] = [4, 5];
+        let logits = Tensor::<TestBackend, 2>::from_data(Data::from([[1.0, 2.0], [3.0, 4.0]]));
 
-        let logits = Tensor::<TestBackend, 2>::random(
-            [batch_size, num_targets],
-            Distribution::Normal(0., 1.0),
+        let targets = Tensor::<TestBackend, 2>::from_data(Data::from([[2.0, 1.0], [3.0, 2.0]]));
+
+        let mse = MSELoss::new();
+        let loss_no_reduction = mse.forward_no_reduction(logits.clone(), targets.clone());
+        let loss = mse.forward(logits.clone(), targets.clone(), Reduction::Mean);
+        let loss_sum = mse.forward(logits, targets, Reduction::Sum);
+
+        assert_eq!(
+            loss_no_reduction.into_data(),
+            Data::from([[1.0, 1.0], [0.0, 4.0]])
         );
-
-        let targets = Tensor::<TestBackend, 2>::random(
-            [batch_size, num_targets],
-            Distribution::Normal(0., 1.0),
-        );
-
-        let loss_1 = MSELoss::new().forward(logits.clone(), targets.clone());
-        let loss_2 = mse(logits, targets);
-
-        loss_1.into_data().assert_approx_eq(&loss_2.into_data(), 3);
-    }
-
-    #[test]
-    fn test_mse_loss_sum_reduction() {
-        let logits = Tensor::<TestBackend, 2>::from_data(Data::from([[1.8, 51.8], [6.66, 8.8]]));
-
-        let targets = Tensor::<TestBackend, 2>::from_data(Data::from([[0.18, 5.18], [66.6, 0.88]]));
-
-        let loss = MSELoss::new().forward_sum(logits, targets);
-
-        loss.into_data()
-            .assert_approx_eq(&Data::from([5831.579_f32]), 3);
+        assert_eq!(loss.into_data(), Data::from([1.5]));
+        assert_eq!(loss_sum.into_data(), Data::from([6.0]));
     }
 }
