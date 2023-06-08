154b4b3dbf0262b7e03616ad88e0fd4135691f8b
maekawatoshiki <maekawatoshiki1017@gmail.com>
2023-06-08 15:05:00 +0900

    Not support serialization for bool

--- burn-import/src/onnx/to_burn.rs
+++ burn-import/src/onnx/to_burn.rs
@@ -286,11 +286,8 @@ impl Tensor {
             TensorData::Float64(val) => DataSerialize::new(val, self.shape.unwrap()).convert(),
             TensorData::Int32(val) => DataSerialize::new(val, self.shape.unwrap()).convert(),
             TensorData::Int64(val) => DataSerialize::new(val, self.shape.unwrap()).convert(),
-            TensorData::String(_) => panic!("String tensor unsuported"),
-            TensorData::Bool(val) => {
-                DataSerialize::new(val.into_iter().map(u8::from).collect(), self.shape.unwrap())
-                    .convert()
-            }
+            TensorData::String(_) => panic!("String tensor unsupported"),
+            TensorData::Bool(_) => panic!("Bool tensor unsupported"),
         }
     }
 }
