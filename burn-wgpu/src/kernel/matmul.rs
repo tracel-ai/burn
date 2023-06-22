081dfc9c8da33a7be77f46bbc6bcf525ba12d57e
louisfd <louisfd94@gmail.com>
2023-06-22 16:24:59 -0400

    tiling1D marche? lol

--- burn-wgpu/src/kernel/matmul.rs
+++ burn-wgpu/src/kernel/matmul.rs
@@ -2,36 +2,36 @@ use super::{build_info, DynamicKernelSettings, KernelSettings, SourceTemplate, S
 use crate::{context::WorkGroup, element::WgpuElement, kernel_wgsl, tensor::WgpuTensor};
 use burn_tensor::Shape;
 
-const BLOCK_SIZE: usize = 2;
-const BLOCK_K: usize = 2;
-const TILE_M: usize = 4;
+const M: usize = 20;
+const N: usize = 20;
+const K: usize = 100;
 
-kernel_wgsl!(MatmulNaiveRaw, "../template/lfd_matmul_naive.wgsl");
+const B_M: usize = 32;
+const B_N: usize = 32;
+const B_K: usize = 32;
+const T_M: usize = 16;
+
+// ça chie si K > B_M / T_M
+// B_M / T_M == WORKGROUP_SIZE_X
+// == combien de threads en x par workgroup
+
 kernel_wgsl!(
-    MatmulCoalescing,
-    "../template/lfd_matmul_mem_coalescing.wgsl"
+    MatmulTiling1DRaw,
+    "../template/lfd_matmul_blocktiling_1d.wgsl"
 );
-kernel_wgsl!(MatmulCachingRaw, "../template/lfd_matmul_caching.wgsl");
-kernel_wgsl!(MatmulTiling1DRaw, "../template/lfd_matmul_blocktiling_1d.wgsl");
 
 struct MatmulTiling1D;
-struct MatmulCaching;
+// struct MatmulCaching;
 
 impl StaticKernel for MatmulTiling1D {
     fn source_template() -> SourceTemplate {
         MatmulTiling1DRaw::source_template()
-            .register("block_size", BLOCK_SIZE.to_string())
-            .register("block_size_x_block_k", (BLOCK_SIZE * BLOCK_K).to_string())
-            .register("block_k", BLOCK_K.to_string())
-            .register("tile_m", TILE_M.to_string())
-    }
-}
-
-impl StaticKernel for MatmulCaching {
-    fn source_template() -> SourceTemplate {
-        MatmulCachingRaw::source_template()
-            .register("block_size", BLOCK_SIZE.to_string())
-            .register("block_size_2x", (BLOCK_SIZE * BLOCK_SIZE).to_string())
+            .register("b_m", B_M.to_string())
+            .register("b_n", B_N.to_string())
+            .register("b_k", B_K.to_string())
+            .register("bm_x_bk", (B_M * B_K).to_string())
+            .register("bk_x_bn", (B_K * B_N).to_string())
+            .register("t_m", T_M.to_string())
     }
 }
 
@@ -68,10 +68,13 @@ pub fn matmul_tiling_1d<E: WgpuElement, const D: usize>(
         .create_buffer(shape_out.num_elements() * core::mem::size_of::<E>());
     let output = WgpuTensor::new(lhs.context.clone(), shape_out, buffer);
 
-    const WORKGROUP_SIZE_X: usize = 2;
-    const WORKGROUP_SIZE_Y: usize = 2;
+    const WORKGROUP_SIZE_X: usize = B_M;
+    let blocks_needed_in_x = f32::ceil(num_rows as f32 / B_M as f32) as u32;
 
-    assert_eq!(WORKGROUP_SIZE_X * WORKGROUP_SIZE_Y, BLOCK_SIZE * BLOCK_K);
+    const WORKGROUP_SIZE_Y: usize = B_N;
+    let blocks_needed_in_y = f32::ceil(num_cols as f32 / B_N as f32) as u32;
+
+    // assert_eq!(WORKGROUP_SIZE_X * WORKGROUP_SIZE_Y, BLOCK_SIZE * BLOCK_K);
 
     let kernel =
         DynamicKernelSettings::<MatmulTiling1D, E, i32>::new(WORKGROUP_SIZE_X, WORKGROUP_SIZE_Y, 1);
@@ -87,177 +90,9 @@ pub fn matmul_tiling_1d<E: WgpuElement, const D: usize>(
         num_iter *= output.shape.dims[i];
     }
 
-    let workgroup_x = 4 as u32;//f32::ceil(num_rows as f32 / (BLOCK_SIZE as f32 / TILE_M as f32) as f32) as u32;
-    let workgroup_y = 4 as u32;//f32::ceil(num_cols as f32 / (2.0 / TILE_M as f32) as f32) as u32;
-    let workgroup = WorkGroup::new(workgroup_x, workgroup_y, num_iter as u32);
+    let workgroup = WorkGroup::new(blocks_needed_in_x, blocks_needed_in_y, num_iter as u32);
 
     println!("WorkGroup {:?}", workgroup);
-
-    lhs.context.execute(
-        workgroup,
-        kernel,
-        &[&lhs.buffer, &rhs.buffer, &output.buffer, &info_buffers],
-    );
-
-    output
-}
-
-pub fn matmul_coalescing<E: WgpuElement, const D: usize>(
-    lhs: WgpuTensor<E, D>,
-    rhs: WgpuTensor<E, D>,
-) -> WgpuTensor<E, D> {
-    lhs.assert_is_on_save_device(&rhs);
-    let mut shape_out = [0; D];
-    lhs.shape
-        .dims
-        .iter()
-        .zip(rhs.shape.dims.iter())
-        .enumerate()
-        .for_each(|(index, (dim_lhs, dim_rhs))| {
-            shape_out[index] = usize::max(*dim_lhs, *dim_rhs);
-        });
-
-    shape_out[D - 2] = lhs.shape.dims[D - 2];
-    shape_out[D - 1] = rhs.shape.dims[D - 1];
-    let shape_out = Shape::new(shape_out);
-
-    let buffer = lhs
-        .context
-        .create_buffer(shape_out.num_elements() * core::mem::size_of::<E>());
-    let output = WgpuTensor::new(lhs.context.clone(), shape_out, buffer);
-    let num_rows = lhs.shape.dims[D - 2];
-    let num_cols = rhs.shape.dims[D - 1];
-
-    let kernel = lhs
-        .context
-        .compile_static::<KernelSettings<MatmulCoalescing, E, i32, BLOCK_SIZE, BLOCK_SIZE, 1>>();
-
-    let info = build_info(&[&lhs, &rhs, &output]);
-    let info_buffers = lhs
-        .context
-        .create_buffer_with_data(bytemuck::cast_slice(&info));
-
-    let mut num_iter = 1;
-    for i in 0..D - 2 {
-        num_iter *= output.shape.dims[i];
-    }
-
-    let workgroup_x = f32::ceil(num_rows as f32 / BLOCK_SIZE as f32) as u32;
-    let workgroup_y = f32::ceil(num_cols as f32 / BLOCK_SIZE as f32) as u32;
-    let workgroup = WorkGroup::new(workgroup_x, workgroup_y, num_iter as u32);
-
-    lhs.context.execute(
-        workgroup,
-        kernel,
-        &[&lhs.buffer, &rhs.buffer, &output.buffer, &info_buffers],
-    );
-
-    output
-}
-
-pub fn matmul_caching<E: WgpuElement, const D: usize>(
-    lhs: WgpuTensor<E, D>,
-    rhs: WgpuTensor<E, D>,
-) -> WgpuTensor<E, D> {
-    lhs.assert_is_on_save_device(&rhs);
-    let mut shape_out = [0; D];
-    lhs.shape
-        .dims
-        .iter()
-        .zip(rhs.shape.dims.iter())
-        .enumerate()
-        .for_each(|(index, (dim_lhs, dim_rhs))| {
-            shape_out[index] = usize::max(*dim_lhs, *dim_rhs);
-        });
-
-    shape_out[D - 2] = lhs.shape.dims[D - 2];
-    shape_out[D - 1] = rhs.shape.dims[D - 1];
-    let shape_out = Shape::new(shape_out);
-
-    let buffer = lhs
-        .context
-        .create_buffer(shape_out.num_elements() * core::mem::size_of::<E>());
-    let output = WgpuTensor::new(lhs.context.clone(), shape_out, buffer);
-    let num_rows = lhs.shape.dims[D - 2];
-    let num_cols = rhs.shape.dims[D - 1];
-
-    let kernel = lhs
-        .context
-        .compile_static::<KernelSettings<MatmulCaching, E, i32, BLOCK_SIZE, BLOCK_SIZE, 1>>();
-
-    let info = build_info(&[&lhs, &rhs, &output]);
-    let info_buffers = lhs
-        .context
-        .create_buffer_with_data(bytemuck::cast_slice(&info));
-
-    let mut num_iter = 1;
-    for i in 0..D - 2 {
-        num_iter *= output.shape.dims[i];
-    }
-
-    let workgroup_x = f32::ceil(num_rows as f32 / BLOCK_SIZE as f32) as u32;
-    let workgroup_y = f32::ceil(num_cols as f32 / BLOCK_SIZE as f32) as u32;
-    let workgroup = WorkGroup::new(workgroup_x, workgroup_y, num_iter as u32);
-
-    lhs.context.execute(
-        workgroup,
-        kernel,
-        &[&lhs.buffer, &rhs.buffer, &output.buffer, &info_buffers],
-    );
-
-    output
-}
-
-pub fn matmul_naive<E: WgpuElement, const D: usize>(
-    lhs: WgpuTensor<E, D>,
-    rhs: WgpuTensor<E, D>,
-) -> WgpuTensor<E, D> {
-    lhs.assert_is_on_save_device(&rhs);
-    const WORKGROUP_SIZE_X: usize = 16;
-    const WORKGROUP_SIZE_Y: usize = 16;
-
-    let mut shape_out = [0; D];
-    lhs.shape
-        .dims
-        .iter()
-        .zip(rhs.shape.dims.iter())
-        .enumerate()
-        .for_each(|(index, (dim_lhs, dim_rhs))| {
-            shape_out[index] = usize::max(*dim_lhs, *dim_rhs);
-        });
-
-    let num_rows = lhs.shape.dims[D - 2];
-    let num_cols = rhs.shape.dims[D - 1];
-    shape_out[D - 2] = num_rows;
-    shape_out[D - 1] = num_cols;
-    let shape_out = Shape::new(shape_out);
-
-    let buffer = lhs
-        .context
-        .create_buffer(shape_out.num_elements() * core::mem::size_of::<E>());
-    let output = WgpuTensor::new(lhs.context.clone(), shape_out, buffer);
-
-    let kernel = lhs.context.compile_static::<KernelSettings<
-        MatmulNaiveRaw,
-        E,
-        i32,
-        WORKGROUP_SIZE_X,
-        WORKGROUP_SIZE_Y,
-        1,
-    >>();
-
-    let info = build_info(&[&lhs, &rhs, &output]);
-    let info_buffers = lhs
-        .context
-        .create_buffer_with_data(bytemuck::cast_slice(&info));
-
-    let mut num_iter = 1;
-    for i in 0..D - 2 {
-        num_iter *= output.shape.dims[i];
-    }
-    let workgroup_x = f32::ceil(num_rows as f32 / WORKGROUP_SIZE_X as f32) as u32; //nombre d'instanciations. on le voit pas dans le kernel
-    let workgroup_y = f32::ceil(num_cols as f32 / WORKGROUP_SIZE_Y as f32) as u32;
-    let workgroup = WorkGroup::new(workgroup_x, workgroup_y, num_iter as u32);
 
     lhs.context.execute(
         workgroup,
@@ -276,27 +111,10 @@ mod tests {
     pub type ReferenceTensor<const D: usize> =
         burn_tensor::Tensor<burn_ndarray::NdArrayBackend<f32>, D>;
 
-    // #[test]
-    // pub fn test_naive() {
-    // same_as_reference(matmul_naive, [4, 25, 13], [4, 13, 77]);
-    // }
-
-    // #[test]
-    // pub fn test_coalesing() {
-    //     same_as_reference(matmul_coalescing, [4, 25, 13], [4, 13, 77]);
-    // }
-
     #[test]
-    pub fn test_caching() {
-        same_as_reference(matmul_caching, [4, 25, 13], [4, 13, 77]);
-        same_as_reference(matmul_caching, [4, 12, 9], [4, 9, 13]);
-        same_as_reference(matmul_caching, [4, 5], [5, 6]);
+    pub fn test_tiling_1d() {
+        same_as_reference(matmul_tiling_1d, [M, K], [K, N]);
     }
-
-    // #[test]
-    // pub fn test_tiling_1d() {
-    //     same_as_reference(matmul_tiling_1d, [8, 8], [8, 8]);
-    // }
 
     fn same_as_reference<F, const D: usize, S>(func: F, shape_lhs: S, shape_rhs: S)
     where
@@ -305,6 +123,8 @@ mod tests {
     {
         let x = ReferenceTensor::random(shape_lhs, burn_tensor::Distribution::Uniform(-1.0, 1.0));
         let y = ReferenceTensor::random(shape_rhs, burn_tensor::Distribution::Uniform(-1.0, 1.0));
+        // let x = ReferenceTensor::ones(shape_lhs);
+        // let y = ReferenceTensor::ones(shape_rhs);
 
         let x_wgpu = TestTensor::from_data(x.to_data());
         let y_wgpu = TestTensor::from_data(y.to_data());
@@ -314,6 +134,7 @@ mod tests {
         let z = func(x_wgpu.into_primitive(), y_wgpu.into_primitive());
         let z = TestTensor::from_primitive(z);
 
+        println!("{z_reference}");
         println!("{z}");
         z_reference.into_data().assert_approx_eq(&z.into_data(), 3);
     }
--- burn-wgpu/src/template/lfd_matmul_blocktiling_1d.wgsl
+++ burn-wgpu/src/template/lfd_matmul_blocktiling_1d.wgsl
@@ -14,17 +14,18 @@ var<storage, read_write> output: array<{{ elem }}>;
 @binding(3)
 var<storage, read> info: array<u32>;
 
-const B_M = {{block_size}}u;
-const B_N = {{block_size}}u;
-const B_K = {{block_k}}u;
-const BLOCK_SIZE_X_BLOCK_K = {{block_size_x_block_k}}u;
-const TILE_M = {{tile_m}}u;
+const B_M = {{b_m}}u;
+const B_N = {{b_n}}u;
+const B_K = {{b_k}}u;
+const B_M_X_B_K = {{bm_x_bk}}u;
+const B_K_X_B_N = {{bk_x_bn}}u;
+const T_M = {{t_m}}u;
 
-var<workgroup> shared_lhs: array<{{ elem }}, BLOCK_SIZE_X_BLOCK_K>; // in fact: B_M * B_K
-var<workgroup> shared_rhs: array<{{ elem }}, BLOCK_SIZE_X_BLOCK_K>; // in fact: B_K * B_N
+var<workgroup> shared_lhs: array<{{ elem }}, B_M_X_B_K>; 
+var<workgroup> shared_rhs: array<{{ elem }}, B_K_X_B_N>;
 
 @compute
-@workgroup_size({{ workgroup_size_x }}, {{ workgroup_size_y }}, 1)
+@workgroup_size({{ workgroup_size_x }}, {{ workgroup_size_y }}, {{ workgroup_size_z }})
 fn main(
     @builtin(global_invocation_id) global_id: vec3<u32>,
     @builtin(local_invocation_index) local_idx: u32,
@@ -35,24 +36,14 @@ fn main(
 
     // localIdx goes up to B_M * B_N / T_M
 
-    // still 1 thread per output elem, but only for loading into sm
-    let thread_row = local_idx / B_N;
-    let thread_col = local_idx % B_N;
-
-    // will get corresponding entries
-    // but if B_K > B_N last values will be ignored
-    // if B_K < B_N we will read outside block
     let rhs_thread_row = local_idx / B_N;
     let rhs_thread_col = local_idx % B_N;
 
-    // will get offsetted entries
-    // and if B_K > B_N last values will be ignored
-    // if B_K < B_N we will read outside block
     let lhs_thread_row = local_idx / B_K;
     let lhs_thread_col = local_idx % B_K;
     
-    let row = skip_row + thread_row;
-    let col = skip_col + thread_col;
+    let row = skip_row + lhs_thread_row;
+    let col = skip_col + rhs_thread_col;
     let batch = global_id.z;
 
 
@@ -62,11 +53,21 @@ fn main(
     let n_cols = info[6u * dim];
     let K = info[5u * dim - 1u];
 
+    let lhs_column_stride = 1u;
+    let lhs_row_stride = K;
+    let rhs_column_stride = 1u;
+    let rhs_row_stride = n_cols;
+    let output_column_stride = 1u;
+    let output_row_stride = n_cols;
+    let lhs_block_column_stride = 1u;
+    let lhs_block_row_stride = B_K;
+    let rhs_block_column_stride = 1u;
+    let rhs_block_row_stride = B_N;
 
     // Calculate the corresponding offsets with support for broadcasting.
     let offset_output = batch * n_rows * n_cols;
-    var offset_lhs: u32 = skip_row * K;
-    var offset_rhs: u32 = skip_col * 1u;
+    var offset_lhs: u32 = skip_row * lhs_row_stride;
+    var offset_rhs: u32 = skip_col * rhs_column_stride;
 
     let batch_dims = dim - 2u;
     for (var b: u32 = 1u; b <= batch_dims; b++) {
@@ -80,23 +81,17 @@ fn main(
         offset_rhs += offset_output / stride_output % shape_rhs * stride_rhs;
     }
 
-    let lhs_column_stride = 1u;
-    let lhs_row_stride = K;
-    let rhs_column_stride = 1u;
-    let rhs_row_stride = n_cols;
-    let output_column_stride = 1u;
-    let output_row_stride = n_cols;
-    let lhs_block_column_stride = 1u;
-    let lhs_block_row_stride = B_K;
-    let rhs_block_column_stride = 1u;
-    let rhs_block_row_stride = B_N;
-
     let south_of_lhs = row >= n_rows;
     let east_of_rhs = col >= n_cols;
 
-    var results: array<{{ elem }}, TILE_M>;
+    let relevant_thread = lhs_thread_row % T_M == 0u;
+
+    // if relevant_thread {
+    var results: array<{{ elem }}, T_M>;
+    // }
 
     for (var k: u32 = 0u; k < K; k += B_K) {
+
         let lhs_sm_position = lhs_thread_row * lhs_block_row_stride + lhs_thread_col * lhs_block_column_stride;
         let lhs_block_ptr = k * lhs_column_stride;
         let lhs_row_rel = lhs_thread_row * lhs_row_stride;
@@ -107,7 +102,7 @@ fn main(
         } else {
             let lhs_position = offset_lhs + lhs_block_ptr + lhs_row_rel + lhs_col_rel;
             shared_lhs[lhs_sm_position] = lhs[lhs_position];
-        }
+        }   
 
         let rhs_sm_position = rhs_thread_row * rhs_block_row_stride + rhs_thread_col * rhs_block_column_stride;
         let rhs_block_ptr = k * rhs_row_stride;
@@ -120,30 +115,35 @@ fn main(
             let rhs_position = offset_rhs + rhs_block_ptr + rhs_row_rel + rhs_col_rel;
             shared_rhs[rhs_sm_position] = rhs[rhs_position];
         }
+        
 
         workgroupBarrier();
 
-        if thread_row == 0u {
-            for (var dot_index: u32 = 0u; dot_index < B_K; dot_index++) {
-            let tmp_rhs = shared_rhs[dot_index * rhs_block_row_stride + rhs_thread_col * rhs_block_column_stride];
+        if relevant_thread {
+            if !south_of_lhs && !east_of_rhs {
+                for (var dot_index: u32 = 0u; dot_index < B_K; dot_index++) {
+                    let tmp_rhs = shared_rhs[dot_index * rhs_block_row_stride + rhs_thread_col * rhs_block_column_stride];
 
-            for (var tile_index = 0u; tile_index < TILE_M; tile_index++) {
-                let lhs_position = tile_index * lhs_block_row_stride + dot_index * lhs_block_column_stride;
-                results[tile_index] += shared_lhs[lhs_position] * tmp_rhs;
+                    for (var tile_index = 0u; tile_index < T_M; tile_index++) {
+                        let lhs_sm_position = (lhs_thread_row + tile_index) * lhs_block_row_stride + dot_index * lhs_block_column_stride;
+                        results[tile_index] += shared_lhs[lhs_sm_position] * tmp_rhs;
+                    }
+                }
             }
-        }
-
         }
         
         workgroupBarrier();
     }
 
-
-    if !south_of_lhs && !east_of_rhs {
-        for (var tile_index = 0u; tile_index < TILE_M; tile_index++) {
-            let row = skip_row + tile_index;
-            let output_index = row * n_cols + col;
-            output[offset_output + output_index] = results[tile_index];
+    if relevant_thread {
+        if !south_of_lhs && !east_of_rhs {
+            var tile_index = 0u;
+            for (var tile_index = 0u; tile_index < T_M; tile_index++) { 
+                if row + tile_index < n_rows {
+                    let output_index = (row + tile_index) * output_row_stride + col * output_column_stride;
+                    output[offset_output + output_index] = results[tile_index];
+                }
+            }
         }
     }
 }
