cc315810fa6e8c5a714a71b9ee0f0bebdd1b6757
nathaniel <nathaniel.simard.42@gmail.com>
2023-06-28 09:48:59 -0400

    Fix

--- burn-wgpu/src/ops/float_ops.rs
+++ burn-wgpu/src/ops/float_ops.rs
@@ -359,7 +359,18 @@ where
         struct Powf;
         struct PowfInplace;
         const POWF_TEMPLATE: &str = "fn powf(lhs: {{ elem }}, rhs: {{ elem }}) -> {{ elem }} {
-    return pow(abs(lhs), rhs);
+    let modulo = rhs % 2.0;
+
+    if (modulo == 0.0) {
+        // Even number
+        return pow(abs(lhs), rhs);
+    } else if (modulo == 1.0) {
+        // Odd number
+        return -1.0 * pow(abs(lhs), rhs);
+    } else {
+        // Float number
+        return pow(lhs, rhs);
+    }
 }";
 
         impl StaticKernel for Powf {
