4e8950b844e6e1ee778ce86c7ff95731542c6d3d
Luni-4 <luni-4@hotmail.it>
2023-07-21 10:38:50 +0200

    burn-import: Fix test

--- burn-import/src/burn/codegen.rs
+++ burn-import/src/burn/codegen.rs
@@ -1,7 +1,7 @@
 use proc_macro2::TokenStream;
 use quote::quote;
 
-use burn::nn::conv::Conv2dPaddingConfig;
+use burn::nn::pool::MaxPool2dPaddingConfig;
 
 pub trait ToTokens {
     fn to_tokens(&self) -> TokenStream;
@@ -32,16 +32,16 @@ impl ToTokens for usize {
     }
 }
 
-/// Padding config
-impl ToTokens for Conv2dPaddingConfig {
+/// Padding configuration for MaxPool2dPaddingConfig
+impl ToTokens for MaxPool2dPaddingConfig {
     fn to_tokens(&self) -> TokenStream {
         match self {
-            Self::Same => quote! { Conv2dPaddingConfig::Same },
-            Self::Valid => quote! { Conv2dPaddingConfig::Valid },
+            Self::Same => quote! { MaxPool2dPaddingConfig::Same },
+            Self::Valid => quote! { MaxPool2dPaddingConfig::Valid },
             Self::Explicit(padding1, padding2) => {
                 let padding1 = padding1.to_tokens();
                 let padding2 = padding2.to_tokens();
-                quote! { Conv2dPaddingConfig::Explicit(#padding1, #padding2) }
+                quote! { MaxPool2dPaddingConfig::Explicit(#padding1, #padding2) }
             }
         }
     }
