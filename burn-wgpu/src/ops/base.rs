7e350a27f15c4b6ceff5179dfd55feb67be8e843
nathaniel <nathaniel.simard.42@gmail.com>
2023-07-21 09:46:54 -0400

    Feat: wgpu cast tensor type

--- burn-wgpu/src/ops/base.rs
+++ burn-wgpu/src/ops/base.rs
@@ -8,6 +8,9 @@ pub type Device<B> = <B as Backend>::Device;
 
 pub type FloatTensor<B, const D: usize> = <B as Backend>::TensorPrimitive<D>;
 
+pub type FullPrecisionBackend<B> = <B as Backend>::FullPrecisionBackend;
+pub type FullPrecisionFloatElem<B> = <FullPrecisionBackend<B> as Backend>::FloatElem;
+
 pub type IntElem<B> = <B as Backend>::IntElem;
 pub type IntTensor<B, const D: usize> = <B as Backend>::IntTensorPrimitive<D>;
 pub type BoolTensor<B, const D: usize> = <B as Backend>::BoolTensorPrimitive<D>;
