6b459fde8282a46d3c8848091676bc384b07445f
Dilshod Tadjibaev <939125+antimora@users.noreply.github.com>
2023-07-28 11:52:23 -0500

    Update Conv2d source generation to support all attributes (#558)

--- burn-import/tests/onnx_tests.rs
+++ burn-import/tests/onnx_tests.rs
@@ -19,15 +19,23 @@ mod tests {
     }
 
     #[rstest]
-    #[case("model1")]
-    // #[case("model2")] <- Add more models here
+    #[case::mixed("model1")]
+    #[case::conv2d("conv2d")]
+    // #[case::description_here("model2")] <- Add more models here
     fn test_codegen(#[case] model_name: &str) {
         let input_file = format!("tests/data/{model_name}/{model_name}.onnx");
         let source_file = format!("tests/data/{model_name}/{model_name}.rs");
         let source_expected: String =
             read_to_string(source_file).expect("Expected source file is missing");
-        let code = code(input_file);
 
-        assert_eq!(source_expected, code);
+        let generated_code = code(input_file);
+
+        // Uncomment this to update the expected code
+        // println!("Generated code:\n{}", generated_code);
+
+        assert_eq!(
+            source_expected, generated_code,
+            "Expected code is left, actual code is right"
+        );
     }
 }
