273637cad9ca6c3ade65d8a9ae7d48ac67d8d296
nathaniel <nathaniel.simard.42@gmail.com>
2023-08-04 15:25:41 -0400

    Add public api

--- burn-core/src/nn/conv/mod.rs
+++ burn-core/src/nn/conv/mod.rs
@@ -1,9 +1,11 @@
 mod conv1d;
 mod conv2d;
+mod conv_transpose1d;
 mod conv_transpose2d;
 
 pub(crate) mod checks;
 
 pub use conv1d::*;
 pub use conv2d::*;
+pub use conv_transpose1d::*;
 pub use conv_transpose2d::*;
