b98befa8ee3a47ec056539c2190b231567f9db33
Caio Piccirillo <caiopiccirillo@gmail.com>
2023-08-07 19:54:23 +0200

    Add max pool 1D

--- burn-core/src/nn/pool/mod.rs
+++ burn-core/src/nn/pool/mod.rs
@@ -2,10 +2,12 @@ mod adaptive_avg_pool1d;
 mod adaptive_avg_pool2d;
 mod avg_pool1d;
 mod avg_pool2d;
+mod max_pool1d;
 mod max_pool2d;
 
 pub use adaptive_avg_pool1d::*;
 pub use adaptive_avg_pool2d::*;
 pub use avg_pool1d::*;
 pub use avg_pool2d::*;
+pub use max_pool1d::*;
 pub use max_pool2d::*;
