624597dd1b796319bf815e468d00c4c043b6b592
Luni-4 <luni-4@hotmail.it>
2023-08-14 09:20:10 +0200

    scripts: Add a shell script for Windows

---
+++ run-checks.ps1
@@ -0,0 +1,19 @@
+# This script runs all `burn` checks locally
+#
+# Run `run-checks` using this command:
+#
+# ./scripts/run-checks environment
+#
+# where `environment` can assume **ONLY** the following values:
+#
+# - `std` to perform checks using `libstd`
+# - `no_std` to perform checks on an embedded environment using `libcore`
+# - `all` to perform checks using both `libstd` and `libcore`
+
+# Compile run-checks binary
+rustc scripts/run-checks.rs --crate-type bin --out-dir scripts
+
+# Run binary passing the first input parameter, who is mandatory.
+# If the input parameter is missing or wrong, it will be the `run-checks`
+# binary which will be responsible of arising an error.
+./scripts/run-checks $args[0]
