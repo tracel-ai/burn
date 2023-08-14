aebd359e66d766913ca7f0f1791aa2996370f7dd
Louis Fortier-Dubois <louisfd94@gmail.com>
2023-08-14 11:58:27 -0400

    Book/guide (#632)

---
+++ examples/guide/src/data.rs
@@ -0,0 +1,45 @@
+use burn::{
+    data::{dataloader::batcher::Batcher, dataset::source::huggingface::MNISTItem},
+    tensor::{backend::Backend, Data, ElementConversion, Int, Tensor},
+};
+
+pub struct MNISTBatcher<B: Backend> {
+    device: B::Device,
+}
+
+impl<B: Backend> MNISTBatcher<B> {
+    pub fn new(device: B::Device) -> Self {
+        Self { device }
+    }
+}
+
+#[derive(Clone, Debug)]
+pub struct MNISTBatch<B: Backend> {
+    pub images: Tensor<B, 3>,
+    pub targets: Tensor<B, 1, Int>,
+}
+
+impl<B: Backend> Batcher<MNISTItem, MNISTBatch<B>> for MNISTBatcher<B> {
+    fn batch(&self, items: Vec<MNISTItem>) -> MNISTBatch<B> {
+        let images = items
+            .iter()
+            .map(|item| Data::<f32, 2>::from(item.image))
+            .map(|data| Tensor::<B, 2>::from_data(data.convert()))
+            .map(|tensor| tensor.reshape([1, 28, 28]))
+            // normalize: make between [0,1] and make the mean =  0 and std = 1
+            // values mean=0.1307,std=0.3081 were copied from Pytorch Mist Example
+            // https://github.com/pytorch/examples/blob/54f4572509891883a947411fd7239237dd2a39c3/mnist/main.py#L122
+            .map(|tensor| ((tensor / 255) - 0.1307) / 0.3081)
+            .collect();
+
+        let targets = items
+            .iter()
+            .map(|item| Tensor::<B, 1, Int>::from_data([(item.label as i64).elem()]))
+            .collect();
+
+        let images = Tensor::cat(images, 0).to_device(&self.device);
+        let targets = Tensor::cat(targets, 0).to_device(&self.device);
+
+        MNISTBatch { images, targets }
+    }
+}
