810f6f5fc50882e9fbc3219ba08bca8694752403
Elazrod56 <thomas.lf5629@gmail.com>
2023-08-16 12:51:07 +0200

    Added new metrics to mod.rs

--- burn-train/src/metric/mod.rs
+++ burn-train/src/metric/mod.rs
@@ -9,9 +9,17 @@ mod base;
 mod cuda;
 mod learning_rate;
 mod loss;
+mod cpu_use;
+mod memory_use;
+mod cpu_temp;
+mod gpu_temp;
 
 pub use acc::*;
 pub use base::*;
 pub use cuda::*;
 pub use learning_rate::*;
 pub use loss::*;
+pub use cpu_use::*;
+pub use memory_use::*;
+pub use cpu_temp::*;
+pub use gpu_temp::*;
