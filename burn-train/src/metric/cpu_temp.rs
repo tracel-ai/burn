2f8b2abf1bd98611b48bc1a0e00130edc4db104c
Elazrod56 <thomas.lf5629@gmail.com>
2023-08-20 13:01:26 +0200

    CPU temp metric now uses systemstat (more reliable)

--- burn-train/src/metric/cpu_temp.rs
+++ burn-train/src/metric/cpu_temp.rs
@@ -2,7 +2,7 @@
 
 use super::MetricMetadata;
 use crate::metric::{Metric, MetricEntry};
-use sysinfo::{ComponentExt, System, SystemExt, Component};
+use systemstat::{System, Platform};
 
 static NAME: &str = "CPU_TEMP";
 
@@ -30,23 +30,15 @@ impl Metric for CpuTemp {
     type Input = ();
 
     fn update(&mut self, _item: &Self::Input, _metadata: &MetricMetadata) -> MetricEntry {
-        let mut sys = System::new();
-
+        let sys = System::new();
         let mut cpu_temp = CpuTemp::new();
 
-        // refreshing components' info
-        sys.refresh_components_list();
-        sys.refresh_components();
-
-        // vec containing components of the system (CPU, disks, NIC, GPU etc...)
-        let components_list: Vec<&Component> = sys.components().iter().collect();
-
-        for component in components_list {
-            // acpi corresponds to the CPU. if we find acpi thermal zone or something like that,
-            // we know it's the CPU
-            if component.label().to_lowercase().contains("acpi") {
-                // update CPU temp
-                cpu_temp.temp_celsius = component.temperature();
+        match sys.cpu_temp() {
+            Ok(temp) => {
+                cpu_temp.temp_celsius = temp
+            },
+            Err(_) => {
+                cpu_temp.temp_celsius = f32::NAN
             }
         }
         
