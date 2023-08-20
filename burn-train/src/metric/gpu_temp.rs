9fdc5c08a0879bd8c089620423297d3646d6f3c5
Elazrod56 <thomas.lf5629@gmail.com>
2023-08-20 13:02:28 +0200

    GPU temp is now a mean if there is >1 GPU

--- burn-train/src/metric/gpu_temp.rs
+++ burn-train/src/metric/gpu_temp.rs
@@ -2,7 +2,7 @@
 
 use super::MetricMetadata;
 use crate::metric::{Metric, MetricEntry};
-use sysinfo::{ComponentExt, System, SystemExt, Component};
+use sysinfo::{ComponentExt, System, SystemExt};
 
 static NAME: &str = "GPU_TEMP";
 
@@ -33,23 +33,32 @@ impl Metric for GpuTemp {
         let mut sys = System::new();
 
         let mut gpu_temp = GpuTemp::new();
+        // Vec containing all "gpu" labeled devices' temperature
+        let mut temps_vec: Vec<f32> = Vec::new();
 
         // refreshing components' info
         sys.refresh_components_list();
         sys.refresh_components();
 
-        // vec containing components of the system (CPU, disks, NIC, GPU etc...)
-        let components_list: Vec<&Component> = sys.components().iter().collect();
-
-        for component in components_list {
-            // if the component is a gpu, its temperature is saved to the struct
+        for component in sys.components().iter() {
+            // if the component is a gpu, its temperature is added to the `temps` vec.
+            // Then, the mean of all these temps will be calculated
             if component.label().to_lowercase().contains("gpu") {
                 // saving the temperature
-                gpu_temp.temp_celsius = component.temperature();
+                temps_vec.push(component.temperature());
             }
         }
-        
-        let formatted = format!("GPU Temp: {:.2}°C", gpu_temp.temp_celsius);
+
+        gpu_temp.temp_celsius = temps_vec.iter().sum::<f32>() / temps_vec.len() as f32;
+
+        let mut formatted = String::new();
+
+        if temps_vec.len() > 1 {
+            formatted = format!("Mean of GPUs temps: {:.2}°C", gpu_temp.temp_celsius)
+        }
+        else {
+            formatted = format!("GPU Temp: {:.2}°C", gpu_temp.temp_celsius)
+        }
 
         let raw = format!("{:.2}", gpu_temp.temp_celsius);
 
@@ -58,4 +67,3 @@ impl Metric for GpuTemp {
 
     fn clear(&mut self) {}
 }
-
