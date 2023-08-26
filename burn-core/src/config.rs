fea645bdc68772f56d9ba3131c1aa3310e8ed6f3
Asuka Minato <i@asukaminato.eu.org>
2023-08-26 13:59:45 +0900

    fix type error

--- burn-core/src/config.rs
+++ burn-core/src/config.rs
@@ -44,7 +44,7 @@ pub trait Config: serde::Serialize + serde::de::DeserializeOwned {
     ///
     /// The output of the save operation.
     #[cfg(feature = "std")]
-    fn save(&self, file: AsRef<std::path::Path>) -> std::io::Result<()> {
+    fn save<P: AsRef<std::path::Path>>(&self, file: P) -> std::io::Result<()> {
         std::fs::write(file, config_to_json(self))
     }
 
@@ -58,7 +58,7 @@ pub trait Config: serde::Serialize + serde::de::DeserializeOwned {
     ///
     /// The loaded configuration.
     #[cfg(feature = "std")]
-    fn load(file: AsRef<std::path::Path>) -> Result<Self, ConfigError> {
+    fn load<P: AsRef<std::path::Path>>(file: P) -> Result<Self, ConfigError> {
         let content = std::fs::read_to_string(file)
             .map_err(|_| ConfigError::FileNotFound(file.to_string()))?;
         config_from_str(&content)
