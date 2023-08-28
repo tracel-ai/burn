084c8bb4e0ef350241042de401f6c727ff3c253c
Nathaniel Simard <nathaniel.simard.42@gmail.com>
2023-08-28 08:20:38 -0400

    Fix: autodiff backward broadcast (#702)

--- burn-autodiff/src/tests/broadcast.rs
+++ burn-autodiff/src/tests/broadcast.rs
@@ -4,69 +4,53 @@ mod tests {
     use burn_tensor::{Data, Distribution, Int, Shape, Tensor};
 
     #[test]
-    fn should_handle_broadcast_during_backward() {
-        let x: Tensor<TestADBackend, 2> = Tensor::from_data(
-            Tensor::<TestADBackend, 1, Int>::arange(0..6)
-                .into_data()
-                .convert(),
-        )
-        .reshape([2, 3])
-        .require_grad();
-        let label: Tensor<TestADBackend, 2> = Tensor::from_data(
-            Tensor::<TestADBackend, 1, Int>::arange(0..2)
-                .into_data()
-                .convert(),
-        )
-        .reshape([2, 1])
-        .require_grad();
-
-        let weights: Tensor<TestADBackend, 2> = Tensor::from_data(
-            Tensor::<TestADBackend, 1, Int>::arange(0..3)
-                .into_data()
-                .convert(),
-        )
-        .reshape([3, 1])
-        .require_grad();
-
-        let bias: Tensor<TestADBackend, 2> = Tensor::from_data(
-            Tensor::<TestADBackend, 1, Int>::arange(0..3)
-                .into_data()
-                .convert(),
-        )
-        .reshape([1, 3])
-        .require_grad();
-
-        let y = x.clone().matmul(weights.clone()).add(bias.clone());
-        let loss = y.clone().sub(label).powf(2.0).sum();
-
-        let grads = loss.backward();
-
-        let weights_grad = weights.grad(&grads).unwrap();
-        let bias_grad = bias.grad(&grads).unwrap();
-        let x_grad = x.grad(&grads).unwrap();
-
-        weights_grad
-            .into_data()
-            .assert_approx_eq(&Data::new(vec![252., 372., 492.], Shape::new([3, 1])), 3);
-        bias_grad
-            .into_data()
-            .assert_approx_eq(&Data::from([[36., 40., 44.]]), 3);
-        x_grad
-            .into_data()
-            .assert_approx_eq(&Data::from([[0., 36., 72.], [0., 84., 168.]]), 3);
+    fn mul_broadcast() {
+        test_ops_broadcast_backward(|x, y| x * y);
     }
 
     #[test]
-    fn grad_same_shape_as_forward_tensor() {
-        let x: Tensor<TestADBackend, 2> =
-            Tensor::random([2, 1], Distribution::Default).require_grad();
-        let y: Tensor<TestADBackend, 2> =
-            Tensor::random([2, 3], Distribution::Default).require_grad();
-        let z = x.clone().add(y);
+    fn div_broadcast() {
+        test_ops_broadcast_backward(|x, y| x / y);
+    }
 
-        let grads = z.backward();
+    #[test]
+    fn sub_broadcast() {
+        test_ops_broadcast_backward(|x, y| x - y);
+    }
+
+    #[test]
+    fn add_broadcast() {
+        test_ops_broadcast_backward(|x, y| x + y);
+    }
+
+    #[test]
+    fn matmul_broadcast() {
+        test_ops_broadcast_backward(|x, y| x.matmul(y));
+    }
+
+    #[test]
+    fn mask_where_broadcast() {
+        test_ops_broadcast_backward(|x, y| x.mask_where(y.clone().equal_elem(4), y));
+    }
+
+    fn test_ops_broadcast_backward<F>(func: F)
+    where
+        F: Fn(Tensor<TestADBackend, 3>, Tensor<TestADBackend, 3>) -> Tensor<TestADBackend, 3>,
+    {
+        let w = TestADTensor::zeros([16, 5, 5]).require_grad();
+        let x = TestADTensor::zeros([4, 5, 5]).require_grad();
+
+        // Slice isn't a broadcastable operation, so it will fail when the previous backward pass
+        // of an operation that support broadcast doesn't support it during the backward pass.
+        let y = func(w.clone().slice([0..1]), x.clone());
+
+        // Will panic if broadcast isn't supported!
+        let grads = y.backward();
+
+        let w_grad = w.grad(&grads).unwrap();
         let x_grad = x.grad(&grads).unwrap();
 
-        assert!(x_grad.shape() == x.shape());
+        assert_eq!(w_grad.shape(), w.shape());
+        assert_eq!(x_grad.shape(), x.shape());
     }
 }
