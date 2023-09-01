4367c45b0338522756d1114e151089166f835a33
Arvid Hammarlund <arvid.hammarlund@gmail.com>
2023-09-01 23:27:41 +0200

    first commit

--- burn-core/src/nn/loss/cross_entropy.rs
+++ burn-core/src/nn/loss/cross_entropy.rs
@@ -7,6 +7,7 @@ use burn_tensor::{activation, backend::Backend, Bool, Int, Tensor};
 pub struct CrossEntropyLoss<B: Backend> {
     pad_index: Option<usize>,
     weights: Option<Tensor<B, 1>>,
+    smoothing: Option<f32>,
     backend: PhantomData<B>,
 }
 
@@ -16,6 +17,7 @@ impl<B: Backend> CrossEntropyLoss<B> {
         Self {
             pad_index,
             backend: PhantomData,
+            smoothing: None,
             weights: None,
         }
     }
@@ -26,7 +28,37 @@ impl<B: Backend> CrossEntropyLoss<B> {
     ///
     /// - logits: `[batch_size, num_targets]`
     /// - targets: `[batch_size]`
-    pub fn forward(&self, logits: Tensor<B, 2>, targets: Tensor<B, 1, Int>) -> Tensor<B, 1> {
+    pub fn forward(&self, logists: Tensor<B, 2>, targets: Tensor<B, 1, Int>) -> Tensor<B, 1> {
+        if let Some(alpha) = self.smoothing.clone() {
+            self.forward_smoothed(logits, targets, alpha)
+        } else {
+            self.forward_default(logits, targets)
+        } 
+    }
+
+    fn forward_smoothing(&self, logits: Tensor<B, 2>, targets: Tensor<B, 1, Int>, alpha: f32) -> Tensor<B, 1> {
+        let [batch_size] = targets.dims();
+
+        let mask = self.padding_mask(&targets);
+        let tensor = activation::log_softmax(logits, 1);
+        let scatter_idx = Tensor::cat(vec![Tensor::arange(0..batch_size).unsqueeze(), targets.clone.unsqueeze()], 1);
+        let targets_matrix = tensor.zeros_like().scatter(1, scatter_idx, targets); 
+        let nr_classes = tensor.dims()[1];
+        let target_matrix = target_matrix * (1 - alpha) + alpha / nr_classes;  
+        let tensor = tensor * target_matrix;
+
+        if let Some(weights) = self.weights.clone() {
+            let tensor = tensor * weights.unsqueeze().repeat(0, batch_size);
+            let tensor = tensor.mask_fill(mask.unsqueeze().repeat(0, nr_classes), 0);
+            let weights = weights.gather(0, targets);
+            (tensor.sum_dim(1) / weights.sum()).neg()
+        } else {
+            let tensor = tensor.mask_fill(mask.unsqueeze().repeat(0, nr_classes), 0);
+            tensor.sum_dim(1).mean().neg()
+        }
+    } 
+    
+    fn forward_default(&self, logits: Tensor<B, 2>, targets: Tensor<B, 1, Int>) -> Tensor<B, 1> {
         let [batch_size] = targets.dims();
 
         let mask = self.padding_mask(&targets);
@@ -54,6 +86,19 @@ impl<B: Backend> CrossEntropyLoss<B> {
     pub fn with_weights(self, weights: Vec<f32>) -> Self {
         Self {
             weights: Some(Tensor::<B, 1, Int>::from_floats(weights.as_slice)),
+            self..
+        }
+    }
+
+    /// Create cross-entropy with label smoothing. 
+    ///
+    /// Hard labels {0, 1} will be changed to y_smoothed = y(1 - a) + a / nr_classes.
+    /// Alpah = 0 would be the same as default.
+    ///
+    pub fn with_smoothing(self, alpha: f32) -> Self {
+        assert!(a < 1 && a > 0, "Alpha most be in interval [0, 1]");
+        Self {
+            weights: Some(alpha),
             self..
         }
     }
