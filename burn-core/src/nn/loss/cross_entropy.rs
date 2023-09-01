1aed1675bdab95543ecee1a2b6a63b2e83e8b30d
Arvid Hammarlund <arvid.hammarlund@gmail.com>
2023-09-01 21:28:19 +0200

    fixed a typo

--- burn-core/src/nn/loss/cross_entropy.rs
+++ burn-core/src/nn/loss/cross_entropy.rs
@@ -44,20 +44,6 @@ impl<B: Backend> CrossEntropyLoss<B> {
         }
     }
 
-    /// Create weighted cross-entropy.
-    ///
-    /// The loss of a specific sample will simply be given by: weight[y] * p(x) * 1,
-    ///
-    /// # Pre-conditions
-    ///   - The order of the weight vector should correspond to the label integer assignment.
-    ///   - Targets assigned negative Int's will not be allowed.
-    pub fn with_weights(self, weights: Vec<f32>) -> Self {
-        Self {
-            weights: Some(Tensor::<B, 1>::from_floats(weights.as_slice())),
-            ..self
-        }
-    }
-
     /// Create weighted cross-entropy. 
     ///
     /// The loss of a specific sample will simply be given by: weight[y] * p(x) * 1, 
