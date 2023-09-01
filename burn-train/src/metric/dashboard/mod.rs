955bb1e168205c141b7d4ccef6a9227e07c7482c
Damien Elmes <gpg@ankiweb.net>
2023-09-02 08:05:13 +1000

    Add cli feature flag
    Partially implements #729:
    
    - Added a new train-cli feature to enable the metric plotting/CLI
    rendering.

--- burn-train/src/metric/dashboard/mod.rs
+++ burn-train/src/metric/dashboard/mod.rs
@@ -1,8 +1,16 @@
 /// Command line interface module for the dashboard.
-pub mod cli;
+#[cfg(feature = "cli")]
+mod cli;
+#[cfg(not(feature = "cli"))]
+mod cli_stub;
 
 mod base;
 mod plot;
 
 pub use base::*;
 pub use plot::*;
+
+#[cfg(feature = "cli")]
+pub use cli::CLIDashboardRenderer;
+#[cfg(not(feature = "cli"))]
+pub use cli_stub::CLIDashboardRenderer;
--- burn-train/src/metric/mod.rs
+++ burn-train/src/metric/mod.rs
@@ -6,20 +6,30 @@ pub mod state;
 
 mod acc;
 mod base;
+#[cfg(feature = "cli")]
 mod cpu_temp;
+#[cfg(feature = "cli")]
 mod cpu_use;
+#[cfg(feature = "cli")]
 mod cuda;
+#[cfg(feature = "cli")]
 mod gpu_temp;
 mod learning_rate;
 mod loss;
+#[cfg(feature = "cli")]
 mod memory_use;
 
 pub use acc::*;
 pub use base::*;
+#[cfg(feature = "cli")]
 pub use cpu_temp::*;
+#[cfg(feature = "cli")]
 pub use cpu_use::*;
+#[cfg(feature = "cli")]
 pub use cuda::*;
+#[cfg(feature = "cli")]
 pub use gpu_temp::*;
 pub use learning_rate::*;
 pub use loss::*;
+#[cfg(feature = "cli")]
 pub use memory_use::*;
