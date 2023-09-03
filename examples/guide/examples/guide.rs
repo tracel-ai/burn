30236c0576f27afc4fb9188bb051d1571a57382e
Damien Elmes <gpg@ankiweb.net>
2023-09-04 08:25:47 +1000

    Add generics back to guide

--- examples/guide/examples/guide.rs
+++ examples/guide/examples/guide.rs
@@ -1,11 +1,12 @@
+use burn::backend::wgpu::AutoGraphicsApi;
 use burn::backend::{WgpuAutodiffBackend, WgpuBackend};
 use burn::data::dataset::Dataset;
 use burn::optim::AdamConfig;
 use guide::{model::ModelConfig, training::TrainingConfig};
 
 fn main() {
-    type MyBackend = WgpuBackend;
-    type MyAutodiffBackend = WgpuAutodiffBackend;
+    type MyBackend = WgpuBackend<AutoGraphicsApi, f32, i32>;
+    type MyAutodiffBackend = WgpuAutodiffBackend<AutoGraphicsApi, f32, i32>;
 
     let device = burn::backend::wgpu::WgpuDevice::default();
     let artifact_dir = "/tmp/guide";
