9e3a9420c6b60babde1ecd083ec7d5b8d4e5083b
nathaniel <nathaniel.simard.42@gmail.com>
2023-09-08 21:58:35 -0400

    Fix examples

--- examples/mnist/src/training.rs
+++ examples/mnist/src/training.rs
@@ -5,13 +5,12 @@ use burn::module::Module;
 use burn::optim::decay::WeightDecayConfig;
 use burn::optim::AdamConfig;
 use burn::record::{CompactRecorder, NoStdTrainingRecorder};
-use burn::train::metric::CUDAMetric;
 use burn::{
     config::Config,
     data::{dataloader::DataLoaderBuilder, dataset::source::huggingface::MNISTDataset},
     tensor::backend::ADBackend,
     train::{
-        metric::{AccuracyMetric, CpuUse, LossMetric},
+        metric::{AccuracyMetric, LossMetric},
         LearnerBuilder,
     },
 };
@@ -62,10 +61,6 @@ pub fn run<B: ADBackend>(device: B::Device) {
         .metric_valid_plot(AccuracyMetric::new())
         .metric_train_plot(LossMetric::new())
         .metric_valid_plot(LossMetric::new())
-        .metric_train(CpuUse::new())
-        .metric_valid(CpuUse::new())
-        .metric_train(CUDAMetric::new())
-        .metric_valid(CUDAMetric::new())
         .with_file_checkpointer(1, CompactRecorder::new())
         .devices(vec![device])
         .num_epochs(config.num_epochs)
