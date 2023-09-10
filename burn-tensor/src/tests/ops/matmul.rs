1da144adc262b1ae19e4db8369e417d650fcbca2
Juliano Negri <juliano.negri@ab-inbev.com>
2023-09-09 22:23:38 -0300

    1d not supported by candle

--- burn-tensor/src/tests/ops/matmul.rs
+++ burn-tensor/src/tests/ops/matmul.rs
@@ -4,16 +4,6 @@ mod tests {
     use burn_tensor::{Data, Tensor};
 
     #[test]
-    fn test_matmul_d1() {
-        let tensor_1 = TestTensor::from_floats([1.0, 7.0, 6.0]);
-        let tensor_2 = TestTensor::from_floats([4.0, 7.0, 5.0]);
-
-        let tensor_3 = tensor_1.matmul(tensor_2);
-
-        assert_eq!(tensor_3.into_data(), Data::from([83.0]));
-    }
-
-    #[test]
     fn test_matmul_d2() {
         let tensor_1 = TestTensor::from_floats([[1.0, 7.0], [2.0, 3.0], [1.0, 5.0]]);
         let tensor_2 = TestTensor::from_floats([[4.0, 7.0, 5.0], [2.0, 3.0, 5.0]]);
