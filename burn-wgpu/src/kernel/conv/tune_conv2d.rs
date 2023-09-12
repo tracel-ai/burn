6eb130477cc19f4226be73a6e65a06ce8ef58a24
Mathias <mathiaswi177@gmail.com>
2023-09-12 15:45:48 -0700

    Create Conv2dKernel struct wrapper to implement KernelFunction for conv2d

--- burn-wgpu/src/kernel/conv/tune_conv2d.rs
+++ burn-wgpu/src/kernel/conv/tune_conv2d.rs
@@ -1,23 +1,52 @@
-use burn_tensor::{ops::ConvOptions, Distribution, Shape, Tensor};
-use num_traits::Float;
-
 use crate::{
     benchmark::Benchmark,
     element::{FloatElement, WgpuElement},
     kernel,
     tensor::{WgpuTensor, WgpuTensorDyn},
     tune::{AutoTuneFunction, AutoTuneKey, Execution, KernelFunction, Tunable},
-    GraphicsApi, WgpuBackend, WgpuDevice,
+    GraphicsApi, WgpuDevice,
 };
+use burn_tensor::ops::ConvOptions;
 use std::{marker::PhantomData, sync::Arc};
 
 const WORKGROUP_SIZES: [usize; 3] = [8, 16, 32];
 
+struct Conv2dKernel<E> {
+    workgroup_size: usize,
+    _marker: PhantomData<E>,
+}
+
+impl<E: WgpuElement + FloatElement> KernelFunction for Conv2dKernel<E> {
+    type Input = (
+        WgpuTensorDyn<E>,
+        WgpuTensorDyn<E>,
+        Option<WgpuTensorDyn<E>>,
+        ConvOptions<2>,
+    );
+    type Output = WgpuTensorDyn<E>;
+
+    fn call(&self, input: Self::Input) -> Self::Output {
+        let (input_tensor, weight_tensor, bias, options) = input;
+        kernel::conv::conv2d(
+            WgpuTensor::<E, 4>::from(input_tensor),
+            WgpuTensor::<E, 4>::from(weight_tensor),
+            bias.map(|b| b.into()),
+            options,
+            Some(self.workgroup_size),
+        )
+        .into()
+    }
+
+    fn description(&self) -> String {
+        format!("Conv2d with workgroup size {}", self.workgroup_size)
+    }
+}
+
 #[derive(new)]
 struct Conv2dBenchmark<E: WgpuElement, const D: usize> {
-    input_shape: Shape<D>,
-    weight_shape: Shape<D>,
-    bias: Option<Shape<D>>,
+    input: WgpuTensor<E, 4>,
+    weight: WgpuTensor<E, 4>,
+    bias: Option<WgpuTensor<E, 1>>,
     options: ConvOptions<2>,
     num_repeats: usize,
     conv2d: PhantomData<E>,
@@ -29,7 +58,7 @@ struct Conv2dBenchmark<E: WgpuElement, const D: usize> {
             ConvOptions<2>,
         ),
         WgpuTensorDyn<E>,
-    >, // input and weight, does bias really affect it?
+    >,
 }
 
 impl<E, const D: usize, G> Benchmark<G> for Conv2dBenchmark<E, D>
@@ -45,11 +74,11 @@ where
     );
 
     fn name(&self) -> String {
-        format!("{:?} Convolution", self.input_shape.dims) // update later
+        format!("{:?} Convolution", self.input.shape.dims) // update later
     }
 
     fn num_samples(&self) -> usize {
-        5 // where does this come from?
+        self.num_repeats
     }
 
     fn execute(&self, (input, weight, bias, conv_options): Self::Args) {
@@ -64,32 +93,10 @@ where
     }
 
     fn prepare(&self, device: &WgpuDevice) -> Self::Args {
-        let input_tensor = Tensor::<WgpuBackend<G, E, i32>, D>::random_device(
-            self.input_shape.clone(),
-            Distribution::Default,
-            device,
-        );
-
-        let weight_tensor = Tensor::<WgpuBackend<G, E, i32>, D>::random_device(
-            self.weight_shape.clone(),
-            Distribution::Default,
-            device,
-        );
-
-        let bias_tensor = self.bias.as_ref().map(|shape| {
-            Tensor::<WgpuBackend<G, E, i32>, D>::random_device(
-                shape.clone(),
-                Distribution::Default,
-                device,
-            )
-            .into_primitive()
-            .into()
-        });
-
         (
-            input_tensor.into_primitive().into(),
-            weight_tensor.into_primitive().into(),
-            bias_tensor,
+            self.input.clone().into(),
+            self.weight.clone().into(),
+            self.bias.clone().map(|b| b.into()),
             self.options.clone(),
         )
     }
@@ -113,19 +120,91 @@ where
     );
 
     let context = input.context.clone();
-    let bias_dyn: Option<WgpuTensorDyn<E>> = bias.map(|b| b.into());
-    let input: (
+    // let bias_dyn: Option<WgpuTensorDyn<E>> = bias.map(|b| b.into());
+    let execution_input: (
         WgpuTensorDyn<E>,
         WgpuTensorDyn<E>,
         Option<WgpuTensorDyn<E>>,
         ConvOptions<2>,
-    ) = (input.into(), weight.into(), bias_dyn, options);
-    let output: WgpuTensorDyn<E> = match context.tuner.execute(&id, input) {
+    ) = (
+        input.clone().into(),
+        weight.clone().into(),
+        bias.map(|b| b.into()),
+        options,
+    );
+    let output: WgpuTensorDyn<E> = match context.tuner.execute(&id, execution_input) {
         Execution::Executed(output) => output,
         Execution::NoCacheFound((input, weight, bias, options)) => {
-            todo!()
+            let tunables = conv2d_candidates::<G, E>(
+                input.into(),
+                weight.into(),
+                bias.map(|b| b.into()),
+                options,
+            );
+            context.tuner.tune(
+                id,
+                (input, weight, bias, options),
+                tunables,
+                &context.device,
+                &context,
+            )
         }
     };
 
     output.into()
+}
+
+type Conv2dTunable<G, E> = Tunable<
+    G,
+    (
+        WgpuTensorDyn<E>,
+        WgpuTensorDyn<E>,
+        Option<WgpuTensorDyn<E>>,
+        ConvOptions<2>,
+    ),
+    WgpuTensorDyn<E>,
+>;
+/// Enumerates all convolution compute pipelines that are candidates for autotuning
+fn conv2d_candidates<G: GraphicsApi, E>(
+    input: WgpuTensor<E, 4>,
+    weight: WgpuTensor<E, 4>,
+    bias: Option<WgpuTensor<E, 1>>,
+    options: ConvOptions<2>,
+) -> Vec<Conv2dTunable<G, E>>
+where
+    E: WgpuElement + FloatElement,
+{
+    let conv2d_benchmark = |func: AutoTuneFunction<
+        (
+            WgpuTensorDyn<E>,
+            WgpuTensorDyn<E>,
+            Option<WgpuTensorDyn<E>>,
+            ConvOptions<2>,
+        ),
+        WgpuTensorDyn<E>,
+    >| {
+        Tunable::<G, _, _>::new(
+            func.clone(),
+            Arc::new(Conv2dBenchmark::new(
+                input.clone(),
+                weight.clone(),
+                bias.clone(),
+                options.clone(),
+                5,
+                func.clone(),
+            )),
+        )
+    };
+
+    let mut candidates = Vec::new(); //Vec(Tunable<G, (WgputTensorDyn<...>, ...), ...>)
+    for workgroup_size in WORKGROUP_SIZES {
+        let kernel = Conv2dKernel {
+            workgroup_size,
+            _marker: PhantomData,
+        };
+        let func: AutoTuneFunction<_, _> = Arc::new(kernel);
+        candidates.push(conv2d_benchmark(func));
+    }
+
+    candidates.into()
 }
