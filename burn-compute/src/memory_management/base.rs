314cccf6215d9db3cb1ca95ba07d2a0d288201a9
louisfd <louisfd94@gmail.com>
2023-09-14 13:31:02 -0400

    document traits

--- burn-compute/src/memory_management/base.rs
+++ burn-compute/src/memory_management/base.rs
@@ -1,12 +1,27 @@
 use crate::ComputeStorage;
 
+/// The MemoryHandle trait is an abstract way to refer to some memory segment.
+/// It should not contain actual references to data.
+///
+/// It is responsible for determining if the memory segment can be mutated,
+/// for instance by keeping track of a reference count
 pub trait MemoryHandle: Clone {
+    /// Checks if the underlying memory can be safely mutated.
     fn can_mut(&self) -> bool;
 }
 
+/// The MemoryManagement trait encapsulates strategies for (de)allocating memory.
+/// It is bound to the ComputeStorage trait, which does the actual (de)allocations.
+///
+/// The MemoryManagement can only reserve memory space or get the resource located at a space.
+/// Modification of the resource data should be done directly on the resource.
 pub trait MemoryManagement<Storage: ComputeStorage> {
+    /// The associated type Handle must implement MemoryHandle
     type Handle: MemoryHandle;
 
+    /// Returns the resource from the storage at the specified handle
     fn get(&mut self, handle: &Self::Handle) -> Storage::Resource;
+
+    /// Finds a spot in memory for a resource of size `size`, and returns a handle to it
     fn reserve(&mut self, size: usize) -> Self::Handle;
 }
--- burn-compute/src/storage/base.rs
+++ burn-compute/src/storage/base.rs
@@ -1,13 +1,18 @@
 use crate::id_type;
 
+// The StorageId can be used to map a handle to its actual data.
 id_type!(StorageId);
 
+/// The StorageUtilization defines if data uses a full memory chunk or a slice of it.
 #[derive(Clone)]
 pub enum StorageUtilization {
+    /// Full memory chunk of specified size
     Full(usize),
+    /// Slice of memory chunk with start index and size.
     Slice(usize, usize),
 }
 
+/// The StorageHandle contains the storage id of a resource and the way it is stored
 #[derive(Clone)]
 pub struct StorageHandle {
     pub id: StorageId,
@@ -15,6 +20,7 @@ pub struct StorageHandle {
 }
 
 impl StorageHandle {
+    /// Returns the size of the resource of the handle
     pub fn size(&self) -> usize {
         match self.utilization {
             StorageUtilization::Full(size) => size,
@@ -23,10 +29,18 @@ impl StorageHandle {
     }
 }
 
+/// The ComputeStorage trait is responsible for allocating and deallocating memory.
 pub trait ComputeStorage {
+    /// The Resource associated type determines the way data is implemented and how
+    /// it can be accessed by kernels
     type Resource;
 
+    /// Returns the underlying resource for a specified storage handle
     fn get(&mut self, handle: &StorageHandle) -> Self::Resource;
+
+    /// Allocates `size` units of memory and returns a handle to it
     fn alloc(&mut self, size: usize) -> StorageHandle;
+
+    /// Deallocates the memory pointed by the given handle
     fn dealloc(&mut self, handle: &StorageHandle);
 }
