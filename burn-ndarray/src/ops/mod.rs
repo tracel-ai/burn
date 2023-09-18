6760775f7da54fb2c8b0c222422e8dddb4c59525
Mathias <mathiaswi177@gmail.com>
2023-09-18 00:36:09 -0700

    Add unfold mod to burn-ndarray

--- burn-ndarray/src/ops/mod.rs
+++ burn-ndarray/src/ops/mod.rs
@@ -8,6 +8,7 @@ mod tensor;
 pub(crate) mod adaptive_avgpool;
 pub(crate) mod avgpool;
 pub(crate) mod conv;
+pub(crate) mod unfold;
 pub(crate) mod macros;
 pub(crate) mod matmul;
 pub(crate) mod maxpool;
