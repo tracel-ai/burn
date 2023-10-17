13829c0daed3215796b9f0f937b8bb9787ba47d8
louisfd <louisfd94@gmail.com>
2023-10-17 10:38:38 -0400

    cleanup operation

--- burn-compute/src/tune/operation.rs
+++ burn-compute/src/tune/operation.rs
@@ -1,13 +1,12 @@
-use core::hash::Hash;
-
 /// Format of the inputs and outputs of a kernel
-pub trait HashableResources: PartialEq + Eq + Hash {
+pub trait HashableResources {
     /// Description used as an autotune key
     fn key(&self) -> String;
 }
 
 /// Type of operation for the kernel
-pub trait Operation: PartialEq + Eq + Hash {
+// pub trait Operation: PartialEq + Eq + Hash {
+pub trait Operation {
     /// Input and output format for the operation
     type Resources: HashableResources;
 }
