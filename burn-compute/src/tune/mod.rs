f80d453725314abfda6118c6cc053db3569b8f46
nathaniel <nathaniel.simard.42@gmail.com>
2023-10-23 09:25:22 -0400

    Fix CI

--- burn-compute/src/tune/mod.rs
+++ burn-compute/src/tune/mod.rs
@@ -1,9 +1,10 @@
-mod autotune_server;
 mod operation;
 mod tune_benchmark;
 mod tune_cache;
 
-pub(crate) use autotune_server::*;
 pub use operation::*;
 pub use tune_benchmark::*;
 pub use tune_cache::*;
+
+mod autotune_server;
+pub(crate) use autotune_server::*;
