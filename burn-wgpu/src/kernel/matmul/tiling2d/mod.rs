dcfa62718da592c59ce647947c53edd2a14360e5
louisfd <louisfd94@gmail.com>
2023-10-30 15:50:31 -0400

    unpadded kernel call

--- burn-wgpu/src/kernel/matmul/tiling2d/mod.rs
+++ burn-wgpu/src/kernel/matmul/tiling2d/mod.rs
@@ -5,3 +5,6 @@ mod padding;
 pub mod vec4;
 /// WGSL vec4 primitives are used on left hand tensor
 pub mod vec4_lhs;
+/// WGSL vec4 primitives are used on left and right hand tensor,
+/// padding is avoided through the use of conditions in the kernel
+pub mod vec4_unpadded;
--- burn-wgpu/src/kernel/matmul/tiling2d/vec4_unpadded.rs
+++ burn-wgpu/src/kernel/matmul/tiling2d/vec4_unpadded.rs
@@ -1,29 +1,32 @@
 use burn_tensor::Element;
 
 use crate::{
+    compute::DynamicKernel,
     element::WgpuElement,
-    kernel::{DynamicKernelSource, SourceTemplate, StaticKernelSource},
+    kernel::{into_contiguous, DynamicKernelSource, SourceTemplate, StaticKernelSource},
     tensor::WgpuTensor,
 };
 use std::marker::PhantomData;
 
 use crate::kernel_wgsl;
 
-use super::base::{matmul_tiling_2d_launch, B_K, B_M, B_N, WORKGROUP_SIZE};
+use super::base::{
+    make_info_handle, make_workgroup, matmul_tiling_2d_launch, B_K, B_M, B_N, WORKGROUP_SIZE,
+};
 
 kernel_wgsl!(
-    MatmulTiling2Dvec4Raw,
-    "../../../template/matmul/blocktiling_2d/vec4.wgsl"
+    MatmulTiling2Dvec4UnpaddedRaw,
+    "../../../template/matmul/blocktiling_2d/vec4_unpadded.wgsl"
 );
 
 #[derive(new, Debug)]
-struct MatmulTiling2Dvec4<E: WgpuElement> {
+struct MatmulTiling2Dvec4Unpadded<E: WgpuElement> {
     _elem: PhantomData<E>,
 }
 
-impl<E: WgpuElement> DynamicKernelSource for MatmulTiling2Dvec4<E> {
+impl<E: WgpuElement> DynamicKernelSource for MatmulTiling2Dvec4Unpadded<E> {
     fn source(&self) -> SourceTemplate {
-        MatmulTiling2Dvec4Raw::source()
+        MatmulTiling2Dvec4UnpaddedRaw::source()
             .register("b_m", B_M.to_string())
             .register("b_n", B_N.to_string())
             .register("b_k", B_K.to_string())
@@ -43,13 +46,34 @@ impl<E: WgpuElement> DynamicKernelSource for MatmulTiling2Dvec4<E> {
 
 /// Matrix multiplication using tiling 2d algorithm with
 /// vec4 primitive on both lhs and rhs
-pub fn matmul_tiling_2d_vec4<E: WgpuElement + Element, const D: usize>(
+pub fn matmul_tiling_2d_vec4_unpadded<E: WgpuElement + Element, const D: usize>(
     lhs: WgpuTensor<E, D>,
     rhs: WgpuTensor<E, D>,
     out: WgpuTensor<E, D>,
 ) -> WgpuTensor<E, D> {
-    let kernel = MatmulTiling2Dvec4::<E>::new();
-    matmul_tiling_2d_launch(lhs, rhs, out, kernel)
+    let lhs = if lhs.batch_swapped_with_row_col() {
+        into_contiguous(lhs)
+    } else {
+        lhs
+    };
+    let rhs = if rhs.batch_swapped_with_row_col() {
+        into_contiguous(rhs)
+    } else {
+        rhs
+    };
+
+    let workgroup = make_workgroup(&out.shape);
+    let info_handle = make_info_handle(&lhs, &rhs, &out);
+
+    lhs.client.execute(
+        Box::new(DynamicKernel::new(
+            MatmulTiling2Dvec4Unpadded::<E>::new(),
+            workgroup,
+        )),
+        &[&lhs.handle, &rhs.handle, &out.handle, &info_handle],
+    );
+
+    out
 }
 
 #[cfg(test)]
@@ -58,62 +82,62 @@ mod tests {
     use crate::kernel::matmul::utils::tests::{same_as_reference, same_as_reference_swapped_dims};
 
     #[test]
-    pub fn test_matmul_vec4_primitive_straightforward() {
+    pub fn test_matmul_vec4_unpadded_straightforward() {
         test_with_params(1, 2, 1, 1, 1);
     }
 
     #[test]
-    pub fn test_matmul_vec4_primitive_shapes_smaller_than_blocks() {
+    pub fn test_matmul_vec4_unpadded_shapes_smaller_than_blocks() {
         test_with_params(8, 8, 8, 1, 1);
     }
 
     #[test]
-    pub fn test_matmul_vec4_primitive_n_smaller_than_m() {
+    pub fn test_matmul_vec4_unpadded_n_smaller_than_m() {
         test_with_params(8, 8, 3, 1, 1);
     }
 
     #[test]
-    pub fn test_matmul_vec4_primitive_m_smaller_than_n() {
+    pub fn test_matmul_vec4_unpadded_m_smaller_than_n() {
         test_with_params(3, 8, 8, 1, 1);
     }
 
     #[test]
-    pub fn test_matmul_vec4_primitive_k_smaller_than_m_n() {
+    pub fn test_matmul_vec4_unpadded_k_smaller_than_m_n() {
         test_with_params(8, 3, 8, 1, 1);
     }
 
     #[test]
-    pub fn test_matmul_vec4_primitive_k_larger_than_m_n() {
+    pub fn test_matmul_vec4_unpadded_k_larger_than_m_n() {
         test_with_params(8, 48, 8, 1, 1);
     }
 
     #[test]
-    pub fn test_matmul_vec4_primitive_multibatch_1_dim() {
+    pub fn test_matmul_vec4_unpadded_multibatch_1_dim() {
         test_with_params(8, 8, 8, 3, 1);
     }
 
     #[test]
-    pub fn test_matmul_vec4_primitive_multibatch_2_dims() {
+    pub fn test_matmul_vec4_unpadded_multibatch_2_dims() {
         test_with_params(8, 8, 8, 3, 4);
     }
 
     #[test]
-    pub fn test_matmul_vec4_primitive_blocks_divide_shapes_unevenly() {
+    pub fn test_matmul_vec4_unpadded_blocks_divide_shapes_unevenly() {
         test_with_params(7, 7, 7, 1, 1);
     }
 
     #[test]
-    pub fn test_matmul_vec4_primitive_medium() {
+    pub fn test_matmul_vec4_unpadded_medium() {
         test_with_params(17, 16, 16, 1, 1);
     }
 
     #[test]
-    pub fn test_matmul_vec4_primitive_large() {
+    pub fn test_matmul_vec4_unpadded_large() {
         test_with_params(134, 242, 250, 1, 1);
     }
 
     fn test_with_params(m: usize, k: usize, n: usize, batch_1: usize, batch_2: usize) {
-        let func = matmul_tiling_2d_vec4;
+        let func = matmul_tiling_2d_vec4_unpadded;
         let shape_lhs = [batch_1, batch_2, m, k];
         let shape_rhs = [batch_1, batch_2, k, n];
         same_as_reference(func, shape_lhs, shape_rhs);
@@ -121,7 +145,7 @@ mod tests {
 
     #[test]
     fn test_matmul_tiling_2d_vec4_primitive_swapped_batches_no_padding() {
-        let matmul_func = matmul_tiling_2d_vec4;
+        let matmul_func = matmul_tiling_2d_vec4_unpadded;
         let swap = [0, 1];
         let shape_lhs = [3, 2, 4, 4];
         let shape_rhs = [3, 2, 4, 4];
@@ -130,7 +154,7 @@ mod tests {
 
     #[test]
     fn test_matmul_tiling_2d_vec4_primitive_swapped_row_col_no_padding() {
-        let matmul_func = matmul_tiling_2d_vec4;
+        let matmul_func = matmul_tiling_2d_vec4_unpadded;
         let swap_lhs = [0, 0];
         let swap_rhs = [2, 3];
         let shape_lhs = [3, 2, 4, 4];
@@ -140,7 +164,7 @@ mod tests {
 
     #[test]
     fn test_matmul_tiling_2d_vec4_primitive_swapped_row_with_batch_no_padding() {
-        let matmul_func = matmul_tiling_2d_vec4;
+        let matmul_func = matmul_tiling_2d_vec4_unpadded;
         let swap_lhs = [0, 3];
         let swap_rhs = [0, 2];
         let shape_lhs = [4, 4, 4, 4];
