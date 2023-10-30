e55fc114d012ff2c143f9aabfad729bcdefbe7b9
louisfd <louisfd94@gmail.com>
2023-10-30 13:00:05 -0400

    fix ag news infer

--- examples/text-classification/examples/ag-news-infer.rs
+++ examples/text-classification/examples/ag-news-infer.rs
@@ -28,7 +28,7 @@ pub fn launch<B: AutodiffBackend>(device: B::Device) {
     feature = "ndarray-blas-accelerate",
 ))]
 mod ndarray {
-    use burn::autodiff::Autodiff;
+    use burn::backend::Autodiff;
     use burn::backend::ndarray::{NdArray, NdArrayDevice};
 
     use crate::{launch, ElemType};
@@ -40,7 +40,7 @@ mod ndarray {
 
 #[cfg(feature = "tch-gpu")]
 mod tch_gpu {
-    use burn::autodiff::Autodiff;
+    use burn::backend::Autodiff;
     use burn::backend::libtorch::{LibTorch, LibTorchDevice};
 
     use crate::{launch, ElemType};
@@ -57,7 +57,7 @@ mod tch_gpu {
 
 #[cfg(feature = "tch-cpu")]
 mod tch_cpu {
-    use burn::autodiff::Autodiff;
+    use burn::backend::Autodiff;
     use burn::backend::libtorch::{LibTorch, LibTorchDevice};
 
     use crate::{launch, ElemType};
@@ -69,7 +69,7 @@ mod tch_cpu {
 
 #[cfg(feature = "wgpu")]
 mod wgpu {
-    use burn::autodiff::Autodiff;
+    use burn::backend::Autodiff;
     use burn::backend::wgpu::{AutoGraphicsApi, Wgpu, WgpuDevice};
 
     use crate::{launch, ElemType};
