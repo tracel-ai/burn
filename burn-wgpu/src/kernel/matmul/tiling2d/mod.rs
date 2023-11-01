02fa454c58baec459d1519c8421424f52e338645
louisfd <louisfd94@gmail.com>
2023-11-01 15:16:30 -0400

    fmt

--- burn-wgpu/src/kernel/matmul/tiling2d/mod.rs
+++ burn-wgpu/src/kernel/matmul/tiling2d/mod.rs
@@ -1,10 +1,10 @@
 mod base;
 mod padding;
 
+/// WGSL vec4 primitives are used on left and right hand tensor,
+/// padding is avoided through the use of conditions in the kernel
+pub mod unpadded;
 /// WGSL vec4 primitives are used on left and right hand tensor
 pub mod vec4;
 /// WGSL vec4 primitives are used on left hand tensor
 pub mod vec4_lhs;
-/// WGSL vec4 primitives are used on left and right hand tensor,
-/// padding is avoided through the use of conditions in the kernel
-pub mod unpadded;
