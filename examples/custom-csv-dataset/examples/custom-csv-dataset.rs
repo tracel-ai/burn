74a8225192f159b9e4347d1fbcfc8365f1798156
Guillaume Lagrange <lagrange.guillaume.1@gmail.com>
2024-01-10 13:49:38 -0500

    Add custom csv dataset example w/ diabetes dataset

---
+++ examples/custom-csv-dataset/examples/custom-csv-dataset.rs
@@ -0,0 +1,14 @@
+use burn::data::dataset::Dataset;
+use custom_csv_dataset::dataset::DiabetesDataset;
+
+fn main() {
+    let dataset = DiabetesDataset::new().expect("Could not load diabetes dataset");
+
+    println!("Dataset loaded with {} rows", dataset.len());
+
+    let item = dataset.get(0).unwrap();
+    println!("First item:\n{:?}", item);
+
+    let item = dataset.get(441).unwrap();
+    println!("Last item:\n{:?}", item);
+}
