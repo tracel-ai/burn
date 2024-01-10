6dfec92689acf3972036b575622c6c2de736d36f
Guillaume Lagrange <lagrange.guillaume.1@gmail.com>
2024-01-10 15:13:26 -0500

    Remove comments

--- examples/custom-csv-dataset/examples/custom-csv-dataset.rs
+++ examples/custom-csv-dataset/examples/custom-csv-dataset.rs
@@ -2,21 +2,11 @@ use burn::data::dataset::Dataset;
 use custom_csv_dataset::dataset::DiabetesDataset;
 
 fn main() {
-    // TODO:
-    // - Should the CSV reader be added to parametrize `InMemDataset::from_csv()`? Current implementation does not allow a wide configuration for the csv to read (but maybe this is better like this?)
-    // -> from_csv(path, ReaderBuilder)
-
-    // NEXT:
-    // - Huggingface source
-
-    // - Add another example for images? (similar to `torchvision.datasets.DatasetFolder`)
-    // - Add batcher to complete the example (new)
-
-    // let source = env::args().skip(1).next();
     let dataset = DiabetesDataset::new().expect("Could not load diabetes dataset");
 
     println!("Dataset loaded with {} rows", dataset.len());
 
+    // Display first and last elements
     let item = dataset.get(0).unwrap();
     println!("First item:\n{:?}", item);
 
