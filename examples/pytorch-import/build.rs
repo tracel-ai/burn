77fd43cc11563003cec0be7b3547ab4668b4a3ca
Dilshod Tadjibaev <939125+antimora@users.noreply.github.com>
2024-01-14 17:09:50 -0600

    Update build.rs to re-save record in burn format

--- examples/pytorch-import/build.rs
+++ examples/pytorch-import/build.rs
@@ -1,8 +1,33 @@
-use burn_import::pytorch::Converter;
+use std::env;
+use std::path::Path;
+
+use burn::backend::NdArray;
+use burn::record::{FullPrecisionSettings, NamedMpkFileRecorder, Recorder};
+use burn_import::pytorch::PyTorchFileRecorder;
+
+// Include the source code of the model since we cannot import from the crate directly.
+mod model {
+    #![allow(dead_code)]
+    include!("src/model/mnist.rs");
+}
+
+// Basic backend type (not used directly here).
+type B = NdArray<f32>;
 
 fn main() {
-    Converter::new()
-        .input("pytorch/mnist.pt")
-        .out_dir("model/")
-        .run_from_script();
+    // Load PyTorch weights into a model record.
+    let record: model::ModelRecord<B> = PyTorchFileRecorder::<FullPrecisionSettings>::default()
+        .load("pytorch/mnist.pt".into())
+        .expect("Failed to decode state");
+
+    // Save the model record to a file.
+    let recorder = NamedMpkFileRecorder::<FullPrecisionSettings>::default();
+
+    // Save into the OUT_DIR directory so that the model can be loaded by the
+    let out_dir = env::var_os("OUT_DIR").unwrap();
+    let file_path = Path::new(&out_dir).join("model/mnist");
+
+    recorder
+        .record(record, file_path)
+        .expect("Failed to save model record");
 }
