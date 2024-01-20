f86199e031fc316bf81a06c9a51fb1e989c2e3df
Mathias <mathiaswi177@gmail.com>
2024-01-20 02:05:33 -0800

    Macro that accomadates wgpu IntElement

--- burn-wgpu/src/kernel/reduce/tune/base.rs
+++ burn-wgpu/src/kernel/reduce/tune/base.rs
@@ -25,3 +25,31 @@ macro_rules! reduce_tune_ops {
         }
     };
 }
+
+#[macro_export]
+/// Generate an autotune operation for a reduce kernel on an int tensor
+macro_rules! reduce_tune_int_ops {
+    ($name:ident, $func:expr) => {
+        #[derive(new)]
+        pub(crate) struct $name<I: IntElement, const D: usize> {
+            input: WgpuTensor<I, D>,
+            output: WgpuTensor<I, D>,
+            reduce_dim: usize,
+        }
+
+        impl<I: IntElement, const D: usize> AutotuneOperation for $name<I, D> {
+            fn execute(self: Box<Self>) {
+                #[allow(clippy::redundant_closure_call)]
+                $func(self.input, self.output, self.reduce_dim);
+            }
+
+            fn clone(&self) -> Box<dyn AutotuneOperation> {
+                Box::new(Self {
+                    input: self.input.clone(),
+                    output: self.output.clone(),
+                    reduce_dim: self.reduce_dim.clone(),
+                })
+            }
+        }
+    };
+}
