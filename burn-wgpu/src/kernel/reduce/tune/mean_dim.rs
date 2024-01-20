c5b127983b2f39029c4105b46a212c49eb1f939d
Mathias <mathiaswi177@gmail.com>
2024-01-20 02:48:38 -0800

    Create autotunables for mean_dim

--- burn-wgpu/src/kernel/reduce/tune/mean_dim.rs
+++ burn-wgpu/src/kernel/reduce/tune/mean_dim.rs
@@ -5,12 +5,12 @@ use crate::{
     compute::WgpuAutotuneKey,
     element::WgpuElement,
     kernel::{
-        prng::random_like_uniform,
-        reduce::{init_reduce_output, mean_dim, mean_dim_shared_memory},
+        prng::{random_like_uniform, random_like_uniform_int},
+        reduce::{init_reduce_output, mean_dim, mean_dim_shared_memory, int_mean_dim, int_mean_dim_shared_memory},
     },
     ops::numeric::empty_device,
     reduce_tune_ops,
-    tensor::WgpuTensor,
+    tensor::WgpuTensor, IntElement, reduce_tune_int_ops,
 };
 
 use super::ReduceAutotuneKey;
@@ -109,8 +109,104 @@ pub fn mean_dim_autotune<E: WgpuElement + Element, const D: usize>(
     output
 }
 
+/// Replicate for int
+
+// Set of mean_dim implementations available for autotuning int tensors
+pub struct MeanDimIntAutotuneOperationSet<I: IntElement, const D: usize> {
+    key: WgpuAutotuneKey,
+    input: WgpuTensor<I, D>,
+    output: WgpuTensor<I, D>,
+    reduce_dim: usize,
+}
+impl<I: IntElement, const D: usize> MeanDimIntAutotuneOperationSet<I, D> {
+    fn new(input: WgpuTensor<I, D>, output: WgpuTensor<I, D>, reduce_dim: usize) -> Self {
+        Self {
+            key: WgpuAutotuneKey::MeanDim(ReduceAutotuneKey::new(
+                &input.shape,
+                &input.strides,
+                reduce_dim,
+            )),
+            input,
+            output,
+            reduce_dim,
+        }
+    }
+}
+
+impl<I: IntElement, const D: usize> AutotuneOperationSet<WgpuAutotuneKey>
+    for MeanDimIntAutotuneOperationSet<I, D>
+{
+    fn key(&self) -> WgpuAutotuneKey {
+        self.key.clone()
+    }
+
+    fn autotunables(&self) -> Vec<Box<dyn AutotuneOperation>> {
+        let random_bounds: (I, I) = ((-10.0).elem::<I>(), (10.0).elem::<I>());
+        let input = random_like_uniform_int(&self.input, random_bounds.0, random_bounds.1);
+
+        let output = empty_device(
+            self.output.client.clone(),
+            self.output.device.clone(),
+            self.output.shape.clone(),
+        );
+
+        vec![
+            Box::new(MeanDimAutotune::<I, D>::new(
+                input.clone(),
+                output.clone(),
+                self.reduce_dim,
+            )),
+            Box::new(MeanDimSharedMemoryAutotune::<I, D>::new(
+                input.clone(),
+                output.clone(),
+                self.reduce_dim,
+            )),
+        ]
+    }
+
+    fn fastest(self: Box<Self>, fastest_index: usize) -> Box<dyn AutotuneOperation> {
+        // Warning: since AutotuneOperationSet shares his key with SumDimAutotuneOperationSet
+        // we must make sure the order here is correlated with SumDim
+        match fastest_index {
+            0 => Box::new(MeanDimIntAutotune::<I, D>::new(
+                self.input,
+                self.output,
+                self.reduce_dim,
+            )),
+            1 => Box::new(MeanDimIntSharedMemoryAutotune::<I, D>::new(
+                self.input,
+                self.output,
+                self.reduce_dim,
+            )),
+            _ => panic!("Fastest index is out of bound"),
+        }
+    }
+}
+
+/// Executes autotune on mean_dim operation
+pub fn int_mean_dim_autotune<I: IntElement, const D: usize>(
+    input: WgpuTensor<I, D>,
+    reduce_dim: usize,
+) -> WgpuTensor<I, D> {
+    let client = input.client.clone();
+
+    let output = init_reduce_output(&input, reduce_dim);
+
+    let operation_set = Box::new(MeanDimIntAutotuneOperationSet::<I, D>::new(
+        input,
+        output.clone(),
+        reduce_dim,
+    ));
+
+    client.execute_autotune(operation_set);
+
+    output
+}
+
 // Probably better on balanced tensor shapes
 reduce_tune_ops!(MeanDimAutotune, mean_dim);
+reduce_tune_int_ops!(MeanDimIntAutotune, int_mean_dim);
 
 // Probably better on tensors large along reduce dim
 reduce_tune_ops!(MeanDimSharedMemoryAutotune, mean_dim_shared_memory);
+reduce_tune_int_ops!(MeanDimIntSharedMemoryAutotune, int_mean_dim_shared_memory);
