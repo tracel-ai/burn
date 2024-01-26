36960cdbe255bc9f13cee59af1caaad1dc7e2534
Guillaume Lagrange <lagrange.guillaume.1@gmail.com>
2024-01-26 15:45:51 -0500

    Uncomment auto device selection

--- burn-tch/examples/simple-add.rs
+++ burn-tch/examples/simple-add.rs
@@ -2,14 +2,13 @@ use burn_tch::{LibTorch, LibTorchDevice};
 use burn_tensor::Tensor;
 
 fn main() {
-    // let device = match tch::utils::has_cuda() {
-    //     true => LibTorchDevice::Cuda(0),
-    //     false => match tch::utils::has_mps() {
-    //         true => LibTorchDevice::Mps,
-    //         false => LibTorchDevice::Cpu,
-    //     },
-    // };
-    let device = LibTorchDevice::Cpu;
+    let device = match tch::utils::has_cuda() {
+        true => LibTorchDevice::Cuda(0),
+        false => match tch::utils::has_mps() {
+            true => LibTorchDevice::Mps,
+            false => LibTorchDevice::Cpu,
+        },
+    };
 
     // Creation of two tensors, the first with explicit values and the second one with ones, with the same shape as the first
     let tensor_1 = Tensor::<LibTorch<f32>, 2>::from_data([[2., 3.], [4., 5.]], &device);
