bde758833bf352c0d33cddae698f2ae41de5ecb2
Sylvain Benner <sylvain@benner.online>
2024-01-25 22:44:07 -0500

    [xtask] Add support for dependencies check in xtask
    cargo xtask dependencies --help

--- xtask/src/main.rs
+++ xtask/src/main.rs
@@ -1,5 +1,6 @@
 use clap::{Parser, Subcommand};
 
+mod dependencies;
 mod logging;
 mod publish;
 mod runchecks;
@@ -18,6 +19,11 @@ struct Args {
 
 #[derive(Subcommand)]
 enum Command {
+    /// Run the specified dependencies check locally. This command should be called with 'cargo +nightly'.
+    Dependencies {
+        /// The dependency check to run
+        dependency_check: dependencies::DependencyCheckType,
+    },
     /// Publish a crate to crates.io
     Publish {
         /// The name of the crate to publish on crates.io
@@ -30,8 +36,9 @@ enum Command {
     },
     /// Run the specified vulnerability check locally. This command should be called with 'cargo +nightly'.
     Vulnerabilities {
-        /// The vulnerability to run
-        vulnerability: vulnerabilities::VulnerabilityType,
+        /// The vulnerability check to run.
+        /// For the reference visit the page https://doc.rust-lang.org/beta/unstable-book/compiler-flags/sanitizer.html
+        vulnerability_check: vulnerabilities::VulnerabilityCheckType,
     },
 }
 
@@ -39,8 +46,11 @@ fn main() -> anyhow::Result<()> {
     let args = Args::parse();
 
     match args.command {
+        Command::Dependencies { dependency_check } => dependencies::run(dependency_check),
         Command::Publish { name } => publish::run(name),
         Command::RunChecks { env } => runchecks::run(env),
-        Command::Vulnerabilities { vulnerability } => vulnerabilities::run(vulnerability),
+        Command::Vulnerabilities {
+            vulnerability_check,
+        } => vulnerabilities::run(vulnerability_check),
     }
 }
