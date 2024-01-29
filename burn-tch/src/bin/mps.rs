99cc8178d5af45c1d2c242e9388fa9dc4dfe8a85
Guillaume Lagrange <lagrange.guillaume.1@gmail.com>
2024-01-29 08:36:54 -0500

    Move simple-add example to separate binaries for device tests

--- burn-tch/src/bin/mps.rs
+++ burn-tch/src/bin/mps.rs
@@ -2,18 +2,14 @@ use burn_tch::{LibTorch, LibTorchDevice};
 use burn_tensor::Tensor;
 
 fn main() {
-    let device = match tch::utils::has_cuda() {
-        true => LibTorchDevice::Cuda(0),
-        false => match tch::utils::has_mps() {
-            true => LibTorchDevice::Mps,
-            false => LibTorchDevice::Cpu,
-        },
-    };
+    assert!(tch::utils::has_mps(), "Could not detect MPS");
+
+    let device = LibTorchDevice::Mps;
 
     // Creation of two tensors, the first with explicit values and the second one with ones, with the same shape as the first
     let tensor_1 = Tensor::<LibTorch<f32>, 2>::from_data([[2., 3.], [4., 5.]], &device);
     let tensor_2 = Tensor::ones_like(&tensor_1);
 
-    // Print the element-wise addition (done with the WGPU backend) of the two tensors.
+    // Print the element-wise addition of the two tensors.
     println!("{}", tensor_1 + tensor_2);
 }
