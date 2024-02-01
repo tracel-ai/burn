051915a9f4c2f52f794db19a532446714e233e04
nathaniel <nathaniel.simard.42@gmail.com>
2024-02-01 09:55:25 -0500

    Fix clippy

--- burn-wgpu/src/compute/base.rs
+++ burn-wgpu/src/compute/base.rs
@@ -89,7 +89,7 @@ pub async fn select_device<G: GraphicsApi>(
             &DeviceDescriptor {
                 label: None,
                 features: wgpu::Features::empty(),
-                limits: limits,
+                limits,
             },
             None,
         )
@@ -130,7 +130,6 @@ fn select_adapter<G: GraphicsApi>(device: &WgpuDevice) -> wgpu::Adapter {
 
     instance
         .enumerate_adapters(G::backend().into())
-        .into_iter()
         .for_each(|adapter| {
             let device_type = adapter.get_info().device_type;
 
