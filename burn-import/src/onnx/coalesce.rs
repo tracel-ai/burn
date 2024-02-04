bfebc6428a6fcb227e3d2b60034c3d8ec122e78b
Joshua Ferguson <joshua.ferguson.273@gmail.com>
2024-02-04 16:06:32 -0600

    Running into issues with identity nodes

--- burn-import/src/onnx/coalesce.rs
+++ burn-import/src/onnx/coalesce.rs
@@ -1,4 +1,4 @@
-use std::{iter::Peekable, slice::IterMut};
+use std::{collections::HashSet, iter::Peekable, slice::IterMut};
 
 use super::ir::{AttributeValue, Node, NodeType};
 use crate::onnx::ir::{ArgType, Data, TensorType};
@@ -26,7 +26,7 @@ pub fn coalesce(nodes: &mut Vec<Node>) {
 /// This function converts a Gemm node into a Linear node
 ///
 /// PyTorch and other frameworks use Gemm node to represent Linear layer.
-fn convert_gemm_to_linear(node: &mut Node) {
+pub(crate) fn convert_gemm_to_linear(node: &mut Node) {
     if node.outputs.len() != 1 {
         panic!("Gemm node must have 1 output");
     }
@@ -149,6 +149,41 @@ fn convert_matmul_to_linear(
     }
 }
 
+pub(crate) fn convert_matmul_to_linear2(
+    node_vec: &mut Vec<Node>,
+    node_index: usize,
+    nodes_to_remove: &mut HashSet<usize>,
+) {
+    let mut iter_mut = node_vec.iter_mut().peekable();
+    let node = iter_mut.nth(node_index).unwrap();
+    if node.inputs.len() != 2 {
+        panic!("MatMul node must have 2 inputs");
+    }
+
+    // if the second input does not have a value, it is not a weight, then proceed to the next node
+    if node.inputs[1].value.is_none() {
+        return;
+    }
+
+    // Check if the second input is a 2D tensor
+    if let ArgType::Tensor(ref tensor_type) = node.inputs[1].ty {
+        assert_eq!(tensor_type.dim, 2, "Weight must be a 2D tensor");
+    } else {
+        panic!("Tensor input is expected");
+    }
+
+    // Convert the node to Linear
+    node.node_type = NodeType::Linear;
+
+    // Check the next node for potential conversion
+
+    if let Some(next_node) = iter_mut.peek() {
+        if is_add_node_with_bias(&next_node, node) {
+            convert_node2(next_node, nodes_to_remove, node);
+            nodes_to_remove.insert(node_index + 1);
+        }
+    }
+}
 /// Helper function to check if the peeked node is an Add node with bias
 fn is_add_node_with_bias(peek_node: &Node, current_node: &Node) -> bool {
     peek_node.node_type == NodeType::Add
@@ -179,4 +214,21 @@ fn convert_and_remove_add_node(
 
     // Remove the Add node
     nodes_to_remove.push(bias_node.name.clone());
+}
+
+/// Helper function to convert and remove the Add node
+pub(crate) fn convert_node2(
+    bias_node: &Node,
+    nodes_to_remove: &mut HashSet<usize>,
+    current_node: &mut Node,
+) {
+    let bias_input = if bias_node.inputs[0].value.is_some() {
+        bias_node.inputs[0].clone()
+    } else {
+        bias_node.inputs[1].clone()
+    };
+
+    // Push the bias input and update the output name
+    current_node.inputs.push(bias_input);
+    current_node.outputs[0].name = bias_node.outputs[0].name.clone();
 }
--- burn-import/src/onnx/from_onnx.rs
+++ burn-import/src/onnx/from_onnx.rs
@@ -9,9 +9,15 @@ use crate::onnx::{
     proto_conversion::convert_node_proto,
 };
 
-use super::dim_inference::dim_inference;
-use super::ir::{ArgType, Argument, Node, NodeType, ONNXGraph, Tensor};
-use super::protos::{ModelProto, TensorProto};
+use super::{
+    coalesce::convert_gemm_to_linear,
+    protos::{ModelProto, TensorProto},
+};
+use super::{
+    coalesce::convert_matmul_to_linear2,
+    ir::{ArgType, Argument, Node, NodeType, ONNXGraph, Tensor},
+};
+use super::{dim_inference::dim_inference, protos::ValueInfoProto};
 
 use protobuf::Message;
 
@@ -22,7 +28,212 @@ const LIFT_CONSTANTS_FOR_NODE_TYPES: [NodeType; 6] = [
     NodeType::Conv2d,
     NodeType::Dropout,
     NodeType::Reshape,
+    NodeType::Unsqueeze,
 ];
+#[derive(Default)]
+pub(crate) struct ONNXGraphBuilder {
+    nodes: Vec<Node>,
+    node_name_counter: HashMap<NodeType, usize>,
+    old_node_names: HashMap<String, String>,
+    //map of input names to a vec of indices of nodes that use it
+    input_of: HashMap<String, Vec<usize>>,
+    outputs_to_move: HashMap<String, usize>,
+    //map of output names to
+    output_of: HashMap<String, usize>,
+    //nodes to remove
+    nodes_to_remove: HashSet<usize>,
+    //constants to lift
+    constants: HashMap<String, Node>,
+    postprocess_for_constants: Vec<usize>,
+    constants_types: HashSet<NodeType>,
+    //identity_nodes
+    identity_nodes: Vec<String>,
+    //matmul nodes
+    matmul_nodes: Vec<usize>,
+}
+
+impl ONNXGraphBuilder {
+    pub(crate) fn node_gen(&mut self, model_proto: &ModelProto) {
+        self.constants_types = LIFT_CONSTANTS_FOR_NODE_TYPES.into_iter().collect();
+        // Convert initializers to hashmap for faster lookup
+        let initializers = model_proto
+            .graph
+            .initializer
+            .iter()
+            .map(|x| (x.name.clone(), x.clone()))
+            .collect::<HashMap<String, TensorProto>>();
+
+        let inputs = model_proto
+            .graph
+            .input
+            .iter()
+            .map(|x| (x.name.clone(), x.clone()))
+            .collect::<HashMap<String, ValueInfoProto>>();
+
+        for (i, node_proto) in model_proto.graph.node.iter().enumerate() {
+            let mut node = convert_node_proto(node_proto);
+            for node_input in node.inputs.iter_mut() {
+                self.input_of
+                    .entry(node_input.name.clone())
+                    .and_modify(|f| f.push(i))
+                    .or_insert(vec![i]);
+                if let Some(initializer) = initializers.get(&node_input.name) {
+                    move_initializer_data(initializer, node_input);
+                }
+            }
+            remap_node_type(&mut node);
+            for node_output in node.outputs.iter() {
+                self.output_of.insert(node_output.name.clone(), i);
+            }
+
+            let node_type = node.node_type.clone();
+            if self.handle_identity(&node_type, &node, i)
+                || self.check_constants(&node, &node_type, i)
+            {
+                self.constants.insert(node.name.clone(), node);
+            } else {
+                //name stuff
+                self.handle_node_renaming(&node_type, &mut node);
+
+                self.handle_unsqueeze(&node_type, &node, i);
+                //NOTE: still not done with this one
+
+                self.handle_coalesce(node_type, &mut node, i);
+
+                self.nodes.push(node);
+            }
+        }
+        self.postprocess_unsqueeze(inputs, model_proto.graph.output.clone());
+        self.postprocess_identity();
+        self.postprocess_constants();
+        self.postprocess_coalesce();
+    }
+
+    fn handle_node_renaming(&mut self, node_type: &NodeType, node: &mut Node) {
+        self.node_name_counter
+            .entry(node_type.clone())
+            .and_modify(|e| *e += 1)
+            .or_insert(1);
+        let old_name = node.name.clone();
+        let new_name =
+            format!("{}{}", node.node_type, self.node_name_counter[&node_type]).to_lowercase();
+        node.name = new_name.clone();
+        println!("old name {:?} new name {:?}", old_name, new_name);
+        self.old_node_names.insert(old_name, new_name);
+    }
+
+    fn postprocess_rename_inputs(&mut self, inputs: &mut Vec<Argument>) {
+        for input in inputs.iter_mut() {
+            if let Some(new_name) = self.old_node_names.get(&input.name) {
+                input.name = new_name.clone();
+            }
+        }
+    }
+
+    fn check_constants(&mut self, node: &Node, node_type: &NodeType, i: usize) -> bool {
+        if node_type == &NodeType::Constant {
+            return true;
+        } else if self.constants_types.contains(node_type) {
+            self.postprocess_for_constants.push(i);
+        }
+        false
+    }
+
+    fn postprocess_constants(&mut self) {
+        for i in self.postprocess_for_constants.iter() {
+            let mut node = &mut self.nodes[*i];
+
+            for input in node.inputs.iter_mut().skip(1) {
+                if let Some(constant) = self.constants.get(&input.name) {
+                    if !constant.inputs.is_empty() && constant.inputs[0].value.is_some() {
+                        input.value = constant.outputs[0].value.clone();
+                        input.ty = constant.outputs[0].ty.clone();
+                    } else {
+                        let arg = convert_constant_value(&constant);
+                        input.value = arg.value;
+                        input.ty = arg.ty;
+                    }
+                }
+            }
+        }
+    }
+
+    //fn get_mult_ref(&self, node_name: String, node_index, )
+
+    fn handle_unsqueeze(&mut self, node_type: &NodeType, node: &Node, i: usize) {
+        if *node_type == NodeType::Unsqueeze {
+            self.outputs_to_move.insert(node.outputs[0].name.clone(), i);
+        }
+    }
+
+    fn postprocess_unsqueeze(
+        &mut self,
+        node_inputs: HashMap<String, ValueInfoProto>,
+        graph_outputs: Vec<ValueInfoProto>,
+    ) {
+        for (output_name, i) in self.outputs_to_move.iter() {
+            let node = &mut self.nodes[*i];
+            if let Some(val_proto) = node_inputs.get(output_name) {
+                move_output_shape(node, val_proto);
+            } else {
+                for output in graph_outputs.iter() {
+                    if output.name == *output_name {
+                        move_output_shape(node, output);
+                    }
+                }
+            }
+        }
+    }
+
+    fn handle_identity(&mut self, node_type: &NodeType, node: &Node, i: usize) -> bool {
+        if node_type == &NodeType::Identity && node.inputs[0].value.is_none() {
+            self.identity_nodes.push(node.name.clone());
+            // self.nodes_to_remove.insert(i);
+            return true;
+        }
+        false
+    }
+
+    fn postprocess_identity(&mut self) {
+        for identity in self.identity_nodes.iter() {
+            let identity_node = self
+                .constants
+                .get(identity)
+                .expect(&format!("Identity node {} not found", identity));
+
+            let input_name = &identity_node.inputs[0].name;
+            let identity_output = &identity_node.outputs[0].name;
+
+            // Replace the identity node's output with its input in the connected nodes.
+            if let Some(node_index) = self.input_of.get(identity_output) {
+                for node_index in node_index {
+                    let node = &mut self.nodes[*node_index];
+                    if let Some(matched_input) =
+                        node.inputs.iter_mut().find(|x| x.name == *identity_output)
+                    {
+                        matched_input.name = input_name.clone();
+                    }
+                }
+            }
+        }
+    }
+
+    fn handle_coalesce(&mut self, node_type: NodeType, node: &mut Node, i: usize) {
+        match node_type {
+            NodeType::Gemm => convert_gemm_to_linear(node),
+            NodeType::MatMul => {
+                self.matmul_nodes.push(i);
+            }
+            _ => {}
+        }
+    }
+
+    fn postprocess_coalesce(&mut self) {
+        for matmul_index in self.matmul_nodes.clone() {
+            convert_matmul_to_linear2(&mut self.nodes, matmul_index, &mut self.nodes_to_remove);
+        }
+    }
+}
 
 /// Open an onnx file and convert it to a Graph (intermediate representation)
 ///
@@ -56,35 +267,78 @@ pub fn parse_onnx(onnx_path: &Path) -> ONNXGraph {
     );
 
     log::debug!("Number of outputs: {:?}", onnx_model.graph.output.len());
+    let mut builder = ONNXGraphBuilder::default();
+    builder.node_gen(&onnx_model);
 
+    let ONNXGraphBuilder {
+        mut nodes,
+        old_node_names,
+        nodes_to_remove,
+        ..
+    } = builder;
     // Convert the nodes
-    let mut nodes: Vec<Node> = vec![];
-    for onnx_node in onnx_model.graph.node.iter() {
-        let mut node = convert_node_proto(onnx_node);
-        remap_node_type(&mut node);
-        nodes.push(node);
-    }
+    // let mut nodes: Vec<Node> = vec![];
+    // println!("onnx_model.graph.node: {:#?}", onnx_model.graph.node);
+    // for onnx_node in onnx_model.graph.node.iter() {
+    //     let mut node = convert_node_proto(onnx_node);
+    //     if onnx_node.op_type == "Unsqueeze" {
+    //         move_output_for_unsqueeze(
+    //             &mut node,
+    //             onnx_model.graph.input.clone(),
+    //             onnx_model.graph.output.clone(),
+    //         );
+    //     }
+    //     remap_node_type(&mut node);
+    //     nodes.push(node);
+    // }
 
-    // ONNX nodes must be topologically sorted per spec:
-    // https://github.com/onnx/onnx/blob/main/docs/IR.md#graphs
-    assert!(nodes.is_top_sorted(), "Nodes are not topologically sorted");
+    // // ONNX nodes must be topologically sorted per spec:
+    // // https://github.com/onnx/onnx/blob/main/docs/IR.md#graphs
+    // assert!(nodes.is_top_sorted(), "Nodes are not topologically sorted");
 
-    // Move inputs with initializers to states
-    move_inputs_to_state(&mut nodes, &onnx_model.graph.initializer);
+    // //TMP: converts initializers to hashmap, iterates over nodes
+    // // Move inputs with initializers to states
+    // move_inputs_to_state(&mut nodes, &onnx_model.graph.initializer);
+    // //TMP: loops over all nodes, filters for identity nodes,
+    // //for each identity node, loop over all nodes and if the identity is in the inputs, replace
+    // // Handle Identity nodes (expects inputs to be moved to states)
+    //handle_identity(&mut nodes);
+    // //TMP: loops over all nodes, filters for constants, collects them into a hashmap
+    // //loop over all nodes, filter for types in constant lifting, for each input
+    // //if it's in the hashmap, move the data to appropriate input
+    // // Lift constants to initializers (expects inputs to be moved to states)
+    //lift_constants(&mut nodes);
+    // //TMP: loops over all nodes, if type matches one of the current coalesce types, call the function
+    // //loop
+    // // Coalesce and transform nodes
+    // coalesce(&mut nodes);
 
-    // Handle Identity nodes (expects inputs to be moved to states)
-    handle_identity(&mut nodes);
+    // //TMP: loop over all nodes, rename each with it's name and a counter
+    // //keep a map of old names to new names
+    // // Rename nodes and inputs, save the mapping for later
+    // let old_node_names = rename_nodes(&mut nodes);
 
-    // Lift constants to initializers (expects inputs to be moved to states)
-    lift_constants(&mut nodes);
+    // // This function collects the inputs of an ONNX model and returns them as a vector of Arguments.
+    // let mut inputs = onnx_model
+    //     .graph
+    //     .input
+    //     .iter()
+    //     .map(|x| Argument::try_from(x.clone()).unwrap())
+    //     .collect();
 
-    // Coalesce and transform nodes
-    coalesce(&mut nodes);
-
-    // Rename nodes and inputs, save the mapping for later
-    let old_node_names = rename_nodes(&mut nodes);
-
-    // This function collects the inputs of an ONNX model and returns them as a vector of Arguments.
+    // // Map each output in the model's graph to an Argument and collect them into a vector.
+    // let mut outputs = onnx_model
+    //     .graph
+    //     .output
+    //     .iter()
+    //     .map(|x| Argument::try_from(x.clone()).unwrap())
+    //     .collect();
+    let mut i = 0;
+    nodes.retain(|node| {
+        let res = !nodes_to_remove.contains(&i);
+        i += 1;
+        res
+    });
     let mut inputs = onnx_model
         .graph
         .input
@@ -92,7 +346,6 @@ pub fn parse_onnx(onnx_path: &Path) -> ONNXGraph {
         .map(|x| Argument::try_from(x.clone()).unwrap())
         .collect();
 
-    // Map each output in the model's graph to an Argument and collect them into a vector.
     let mut outputs = onnx_model
         .graph
         .output
@@ -101,7 +354,6 @@ pub fn parse_onnx(onnx_path: &Path) -> ONNXGraph {
         .collect();
 
     let old_input_names = rename_inputs(&mut nodes, &mut inputs, &mut outputs);
-
     // Infer shapes and update the inputs and outputs
     dim_inference(&mut nodes, &inputs, &mut outputs);
 
@@ -170,6 +422,67 @@ fn move_initializer_data(initializer: &TensorProto, input: &mut Argument) {
             elem_type: tensor.elem_type,
             shape: tensor.shape,
         });
+    }
+}
+
+//this is an extremely hacky temporary solution while I figure out how to properly handle this
+//situation
+fn move_output_for_unsqueeze(
+    node: &mut Node,
+    outputs: Vec<ValueInfoProto>,
+    inputs: Vec<ValueInfoProto>,
+) {
+    let output_name = node.outputs[0].name.clone();
+    // check outputs first, as it's shorter
+    for output in outputs.iter() {
+        if output.name == output_name {
+            match node.outputs[0].ty {
+                ArgType::Tensor(ref mut tensor_type) => {
+                    if let Some(shape) = output.type_.as_ref().unwrap().tensor_type().shape.as_ref()
+                    {
+                        tensor_type.shape =
+                            Some(shape.dim.iter().map(|x| x.dim_value() as usize).collect());
+                        return;
+                    }
+                }
+                _ => return,
+            }
+        }
+    }
+    for input in inputs.iter() {
+        if input.name == output_name {
+            //copy the shape
+            match node.outputs[0].ty {
+                ArgType::Tensor(ref mut tensor_type) => {
+                    if let Some(shape) = input.type_.as_ref().unwrap().tensor_type().shape.as_ref()
+                    {
+                        tensor_type.shape =
+                            Some(shape.dim.iter().map(|x| x.dim_value() as usize).collect());
+                        return;
+                    }
+                }
+                _ => return,
+            }
+        }
+    }
+}
+
+fn move_output_shape(node: &mut Node, output_tensor: &ValueInfoProto) {
+    match node.outputs[0].ty {
+        ArgType::Tensor(ref mut tensor_type) => {
+            if let Some(shape) = output_tensor
+                .type_
+                .as_ref()
+                .unwrap()
+                .tensor_type()
+                .shape
+                .as_ref()
+            {
+                tensor_type.shape =
+                    Some(shape.dim.iter().map(|x| x.dim_value() as usize).collect());
+            }
+        }
+        _ => {}
     }
 }
 
