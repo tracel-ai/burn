c75a28854a2a1c24518218d726fc47940fae207a
ronofays <ronofays@gmail.com>
2024-02-05 13:03:00 -0600

    write image to file - needs testing

--- burn-vision/src/io.rs
+++ burn-vision/src/io.rs
@@ -4,8 +4,10 @@ use std::path::Path;
 use std::fs::File;
 use std::error::Error;
 
+use burn_core::serde::de::value;
 use burn_core::tensor::{backend::Backend, Tensor, Data, Shape};
-use image::{DynamicImage, GenericImageView};
+use image::{DynamicImage, GenericImage, GenericImageView, Rgba};
+use image::ColorType::Rgba8;
 
 #[derive(Debug)]
 pub enum ImageReaderError {
@@ -58,24 +60,39 @@ impl<B: Backend> ImageReader<B> {
     }
 
     pub fn write_image<P: AsRef<Path>>(&self, tensor: &Tensor<B, 3>, path: P) -> Result<(), ImageReaderError> {
-        let data = tensor.data().to_host();
-        let shape = data.shape;
-        let width = shape[1];
-        let height = shape[0];
+    
+        // First normalize the image to fall in the range of [0, 255]
+        let tensor = tensor.clone();
+        let min = tensor.clone().min().into_scalar();
 
+        // Bring the minimum value to zero.
+        let tensor = tensor.sub_scalar(min);
+
+        // Bring the max to 255
+        // Note: there are clearly issues with this method, as it will end up stretching the brightness of the image when writing
+        //       but I am not sure how much of an issue this is for most use cases. (eg. I want to output the results of my transforms
+        //       to make sure they look sensible)
+        let max = tensor.clone().max().into_scalar();
+        let tensor = tensor.div_scalar(max).mul_scalar(255);
+        
+
+        let data = tensor.to_data().convert::<u8>();
+        let shape = data.shape;
+        let width = shape.dims[1];
+        let height = shape.dims[0];
         let mut img = DynamicImage::new_rgb8(width as u32, height as u32);
         for y in 0..height {
             for x in 0..width {
-                let r = data.value[(y * width + x) * 3 + 0] as u8;
-                let g = data.value[(y * width + x) * 3 + 1] as u8;
-                let b = data.value[(y * width + x) * 3 + 2] as u8;
-                img.put_pixel(x as u32, y as u32, image::Rgb([r, g, b]));
+                let r = data.value[(y * width + x) * 3 + 0];
+                let g = data.value[(y * width + x) * 3 + 1];
+                let b = data.value[(y * width + x) * 3 + 2];
+
+                img.put_pixel(x as u32, y as u32, image::Rgba([r, g, b, 255]));
             }
         }
 
-        let file = File::create(path).map_err(ImageReaderError::Io)?;
-        img.write_to(file, image::ImageOutputFormat::Png).map_err(ImageReaderError::Image)?;
+        let mut file = File::create(path).map_err(ImageReaderError::Io)?;
+        img.write_to(&mut file, image::ImageOutputFormat::Png).map_err(ImageReaderError::Image)?;
         Ok(())
     }
-
 }
