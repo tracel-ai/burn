5ecf27f3aaf4656a5afb52b507d491c38f193a04
nathaniel <nathaniel.simard.42@gmail.com>
2024-02-10 13:36:50 -0500

    Add documentation for the runtime trait

--- burn-wgpu/src/runtime.rs
+++ burn-wgpu/src/runtime.rs
@@ -1,16 +1,21 @@
+use crate::{codegen::Compiler, compute::JitAutotuneKey};
 use burn_compute::{channel::ComputeChannel, client::ComputeClient, server::ComputeServer};
 use burn_fusion::FusionDevice;
 
-use crate::{codegen::Compiler, compute::JitAutotuneKey};
-
-/// Just-In-Time runtime.
+/// Runtime for the [just-in-time backend](crate::JitBackend).
 pub trait Runtime: Send + Sync + 'static {
+    /// The compiler used to compile the
+    /// [inner representation](crate::codegen::dialect::gpu::ComputeShader).
+    /// into tokens.
     type Compiler: Compiler;
+    /// The compute server used to run kernels and perform autotuning.
     type Server: ComputeServer<
         Kernel = Box<dyn crate::compute::Kernel>,
         AutotuneKey = JitAutotuneKey,
     >;
+    /// The channel used to communicate with the compute server.
     type Channel: ComputeChannel<Self::Server>;
+    /// The device used to retrieve the compute client.
     type Device: FusionDevice
         + Default
         + core::hash::Hash
@@ -21,6 +26,10 @@ pub trait Runtime: Send + Sync + 'static {
         + Sync
         + Send;
 
+    /// A version of the runtime that supports full precision.
+    ///
+    /// Note that the runtime should share all other runtime components.
+    /// This way, it's possible to share the same handles for both runtimes and reduce data copies to a minimum.
     type FullPrecisionRuntime: Runtime<
         Compiler = <Self::Compiler as Compiler>::FullPrecisionCompiler,
         Device = Self::Device,
@@ -28,5 +37,6 @@ pub trait Runtime: Send + Sync + 'static {
         Channel = Self::Channel,
     >;
 
+    /// Retrieve the compute client from the runtime device.
     fn client(device: &Self::Device) -> ComputeClient<Self::Server, Self::Channel>;
 }
