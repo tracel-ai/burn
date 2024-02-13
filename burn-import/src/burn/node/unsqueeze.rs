98f56f230fd5a77aef3f3f859e7872d9f796cc40
Joshua Ferguson <joshua.ferguson.273@gmail.com>
2024-02-13 13:45:37 -0600

    added test for unsqueeze codegen

--- burn-import/src/burn/node/unsqueeze.rs
+++ burn-import/src/burn/node/unsqueeze.rs
@@ -35,3 +35,56 @@ impl<PS: PrecisionSettings> NodeCodegen<PS> for UnsqueezeNode {
         Node::Unsqueeze(self)
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use burn::record::FullPrecisionSettings;
+
+    use super::*;
+    use crate::burn::{
+        graph::BurnGraph,
+        node::{test::assert_tokens, unsqueeze::UnsqueezeNode},
+        TensorType,
+    };
+
+    #[test]
+    fn test_codegen_nodes() {
+        let mut graph = BurnGraph::<FullPrecisionSettings>::default();
+
+        graph.register(UnsqueezeNode::new(
+            TensorType::new_float("tensor1", 3),
+            TensorType::new_float("tensor2", 5),
+            [0, 4].into(),
+        ));
+
+        graph.register_input_output(vec!["tensor1".to_string()], vec!["tensor2".to_string()]);
+
+        let expected = quote! {
+            use burn::{
+                module::Module,
+                tensor::{backend::Backend, Tensor},
+            };
+
+            #[derive(Module, Debug)]
+            pub struct Model<B: Backend> {
+                phantom: core::marker::PhantomData<B>,
+            }
+
+            impl<B: Backend> Model <B> {
+                #[allow(unused_variables)]
+                pub fn new_with(record: ModelRecord<B>) -> Self {
+                    Self {
+                        phantom: core::marker::PhantomData,
+                    }
+                }
+                #[allow(clippy::let_and_return, clippy::approx_constant)]
+                pub fn forward(&self, tensor1: Tensor<B, 3>) -> Tensor<B, 5> {
+                    let tensor2: Tensor<B, 5> = tensor1.unsqueeze_dims(&[0,4]);
+                    tensor2
+                }
+            }
+        };
+
+        assert_tokens(graph.codegen(), expected);
+    }
+}
