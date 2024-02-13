577b052f13056402f296d0aa11e8c24c7e83528e
jcmullwh <78822128+jcmullwh@users.noreply.github.com>
2024-02-12 21:05:35 -0500

    Pad Fixes
    Moved pad to numeric
    
    Simplified PadMode Element
    
    updated tensor creations
    
    fixed doc example

--- burn-tensor/src/tests/api/padding.rs
+++ burn-tensor/src/tests/api/padding.rs
@@ -1,12 +1,12 @@
 #[burn_tensor_testgen::testgen(padding)]
 mod tests {
     use super::*;
-    use burn_tensor::{Data, Int, PadMode, PadSize, Shape, Tensor};
+    use burn_tensor::{Data, Int, Numeric, PadMode, PadSize, Shape, Tensor};
 
     #[test]
     fn padding_2d_test() {
         let unpadded_floats: [[f32; 3]; 2] = [[0.0, 1.0, 2.0], [3.0, 4.0, 5.0]];
-        let tensor = Tensor::<TestBackend, 2>::from_floats_devauto(unpadded_floats);
+        let tensor = TestTensor::from(unpadded_floats);
         let insert_num = 1.1;
 
         let padded_tensor = tensor.pad(PadSize::uniform(2), PadMode::Constant(insert_num));
@@ -27,33 +27,21 @@ mod tests {
 
     #[test]
     fn padding_4d_test() {
-        let unpadded_floats = [
-            [
-                [
-                    [0.0, 1.0],
-                    [2.0, 3.0],
-                    [4.0, 5.0],
-                ],
-            ],
-        ];
-        let tensor = Tensor::<TestBackend, 4>::from_floats_devauto(unpadded_floats);
+        let unpadded_floats = [[[[0.0, 1.0], [2.0, 3.0], [4.0, 5.0]]]];
+        let tensor = TestTensor::from(unpadded_floats);
         let insert_num = 1.1;
 
         let padded_tensor = tensor.pad(PadSize::uniform(2), PadMode::Constant(insert_num));
 
-        let padded_primitive_data_expected = [
-            [
-                [
-                    [1.1, 1.1, 1.1, 1.1, 1.1, 1.1],
-                    [1.1, 1.1, 1.1, 1.1, 1.1, 1.1],
-                    [1.1, 1.1, 0.0, 1.0, 1.1, 1.1],
-                    [1.1, 1.1, 2.0, 3.0, 1.1, 1.1],
-                    [1.1, 1.1, 4.0, 5.0, 1.1, 1.1],
-                    [1.1, 1.1, 1.1, 1.1, 1.1, 1.1],
-                    [1.1, 1.1, 1.1, 1.1, 1.1, 1.1],
-                ],
-            ],
-        ];
+        let padded_primitive_data_expected = [[[
+            [1.1, 1.1, 1.1, 1.1, 1.1, 1.1],
+            [1.1, 1.1, 1.1, 1.1, 1.1, 1.1],
+            [1.1, 1.1, 0.0, 1.0, 1.1, 1.1],
+            [1.1, 1.1, 2.0, 3.0, 1.1, 1.1],
+            [1.1, 1.1, 4.0, 5.0, 1.1, 1.1],
+            [1.1, 1.1, 1.1, 1.1, 1.1, 1.1],
+            [1.1, 1.1, 1.1, 1.1, 1.1, 1.1],
+        ]]];
 
         let padded_data_expected = Data::from(padded_primitive_data_expected);
         let padded_data_actual = padded_tensor.into_data();
@@ -62,74 +50,49 @@ mod tests {
 
     #[test]
     fn padding_asymmetric_test() {
-        let unpadded_floats = [
-            [
-                [
-                    [0.0, 1.0],
-                    [2.0, 3.0],
-                    [4.0, 5.0],
-                ],
-            ],
-        ];
-        let tensor = Tensor::<TestBackend, 4>::from_floats_devauto(unpadded_floats);
+        let unpadded_floats = [[[[0.0, 1.0], [2.0, 3.0], [4.0, 5.0]]]];
+        let tensor = TestTensor::from(unpadded_floats);
         let insert_num = 1.1;
 
         let padding = [4, 3, 2, 1];
         let padded_tensor = tensor.pad(PadSize::asymmetric(padding), PadMode::Constant(insert_num));
 
-        let padded_primitive_data_expected = [
-            [
-                [
-                    [1.1, 1.1, 1.1, 1.1, 1.1],
-                    [1.1, 1.1, 1.1, 1.1, 1.1],
-                    [1.1, 1.1, 1.1, 1.1, 1.1],
-                    [1.1, 1.1, 1.1, 1.1, 1.1],
-                    [1.1, 1.1, 0.0, 1.0, 1.1],
-                    [1.1, 1.1, 2.0, 3.0, 1.1],
-                    [1.1, 1.1, 4.0, 5.0, 1.1],
-                    [1.1, 1.1, 1.1, 1.1, 1.1],
-                    [1.1, 1.1, 1.1, 1.1, 1.1],
-                    [1.1, 1.1, 1.1, 1.1, 1.1],
-                ],
-            ],
-        ];
+        let padded_primitive_data_expected = [[[
+            [1.1, 1.1, 1.1, 1.1, 1.1],
+            [1.1, 1.1, 1.1, 1.1, 1.1],
+            [1.1, 1.1, 1.1, 1.1, 1.1],
+            [1.1, 1.1, 1.1, 1.1, 1.1],
+            [1.1, 1.1, 0.0, 1.0, 1.1],
+            [1.1, 1.1, 2.0, 3.0, 1.1],
+            [1.1, 1.1, 4.0, 5.0, 1.1],
+            [1.1, 1.1, 1.1, 1.1, 1.1],
+            [1.1, 1.1, 1.1, 1.1, 1.1],
+            [1.1, 1.1, 1.1, 1.1, 1.1],
+        ]]];
     }
 
     #[test]
     fn padding_asymmetric_integer_test() {
-        let unpadded_ints = [
-            [
-                [
-                    [0, 1],
-                    [2, 3],
-                    [4, 5],
-                ],
-            ],
-        ];
+        let unpadded_ints = [[[[0, 1], [2, 3], [4, 5]]]];
 
-        let tensor = Tensor::<TestBackend, 4, Int>::from_ints_devauto(unpadded_ints);
+        let tensor = TestTensorInt::from(unpadded_ints);
         let insert_num = 6;
-
 
         let padding = [4, 3, 2, 1];
         let padded_tensor = tensor.pad(PadSize::asymmetric(padding), PadMode::Constant(insert_num));
 
-        let padded_primitive_data_expected = [
-            [
-                [
-                    [6, 6, 6, 6, 6],
-                    [6, 6, 6, 6, 6],
-                    [6, 6, 6, 6, 6],
-                    [6, 6, 6, 6, 6],
-                    [6, 6, 0, 1, 6],
-                    [6, 6, 2, 3, 6],
-                    [6, 6, 4, 5, 6],
-                    [6, 6, 6, 6, 6],
-                    [6, 6, 6, 6, 6],
-                    [6, 6, 6, 6, 6],
-                ],
-            ],
-        ];
+        let padded_primitive_data_expected = [[[
+            [6, 6, 6, 6, 6],
+            [6, 6, 6, 6, 6],
+            [6, 6, 6, 6, 6],
+            [6, 6, 6, 6, 6],
+            [6, 6, 0, 1, 6],
+            [6, 6, 2, 3, 6],
+            [6, 6, 4, 5, 6],
+            [6, 6, 6, 6, 6],
+            [6, 6, 6, 6, 6],
+            [6, 6, 6, 6, 6],
+        ]]];
 
         let padded_data_expected = Data::from(padded_primitive_data_expected);
         let padded_data_actual = padded_tensor.into_data();
