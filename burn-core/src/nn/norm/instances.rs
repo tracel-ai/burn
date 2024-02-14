686e16e48720ece8d2b7ad9318e4b305fc641fe5
tushushu <xmaseve1984@gmail.com>
2024-02-14 09:54:08 +0800

    refactor

--- burn-core/src/nn/norm/instances.rs
+++ burn-core/src/nn/norm/instances.rs
@@ -6,81 +6,28 @@ use crate::module::Param;
 use crate::tensor::backend::Backend;
 use crate::tensor::Tensor;
 
-/// Configuration to create a [InstanceNorm1d](InstanceNorm1d) layer.
-#[derive(Config)]
-pub struct InstanceNorm1dConfig {
-    /// Number of features in the input tensor.
+#[derive(Config, Debug)]
+pub struct InstanceNormConfig {
+    /// The number of features.
     pub num_features: usize,
-    /// Epsilon value to avoid division by zero.
+    /// A value required for numerical stability. Default: 1e-5
+    #[config(default = 1e-5)]
     pub eps: f64,
-    /// Momentum value to update running statistics.
+    /// Momentum used to update the metrics. Default: 0.1
+    #[config(default = 0.1)]
     pub momentum: f64,
-    /// Whether to use the input tensor's mean and variance to normalize the tensor.
+    /// A boolean value that when set to `true`, this module has learnable
+    /// affine parameters. Default: `false`
+    #[config(default = false)]
     pub affine: bool,
-    /// Whether to use the input tensor's mean and variance to normalize the tensor.
-    pub track_running_stats: bool,
 }
 
-/// Applies Instance Normalization over a mini-batch of inputs.
 #[derive(Module, Debug)]
-pub struct InstanceNorm1d<B: Backend> {
+pub struct InstanceNorm<B: Backend> {
     num_features: usize,
     eps: f64,
     momentum: f64,
     affine: bool,
-    track_running_stats: bool,
-    weight: Option<Param<Tensor<B, 1>>>,
-    bias: Option<Param<Tensor<B, 1>>>,
-    running_mean: Option<Tensor<B, 1>>,
-    running_var: Option<Tensor<B, 1>>,
-}
-
-impl InstanceNorm1dConfig {
-    /// Initialize a new [instance norm](InstanceNorm1d) module.
-    pub fn init<B: Backend>(&self, device: &B::Device) -> InstanceNorm1d<B> {
-        let (weight, bias, running_mean, running_var) = if self.affine {
-            let weight = Tensor::ones([self.num_features], device).into();
-            let bias = Tensor::zeros([self.num_features], device).into();
-            let running_mean = Tensor::zeros([self.num_features], device);
-            let running_var = Tensor::ones([self.num_features], device);
-
-            (
-                Some(weight),
-                Some(bias),
-                Some(running_mean),
-                Some(running_var),
-            )
-        } else {
-            (None, None, None, None)
-        };
-
-        InstanceNorm1d {
-            num_features: self.num_features,
-            eps: self.eps,
-            momentum: self.momentum,
-            affine: self.affine,
-            track_running_stats: self.track_running_stats,
-            weight,
-            bias,
-            running_mean,
-            running_var,
-        }
-    }
-}
-
-impl<B: Backend> InstanceNorm1d<B> {
-    /// Applies the forward pass on the input tensor.
-    ///
-    /// `Y = (X - μ) / σ * γ + β`
-    pub fn forward(&self, input: &Tensor<B, 3>) -> Tensor<B, 3> {
-        let mean = input.mean_axis(&[2], false);
-        let variance = input.var_axis(&[2], false);
-
-        let mut output = input - mean;
-        output /= variance.sqrt();
-        output *= self.weight.as_ref().unwrap();
-        output += self.bias.as_ref().unwrap();
-
-        output
-    }
+    gamma: Option<Param<Tensor<B, 1>>>,
+    beta: Option<Param<Tensor<B, 1>>>,
 }
