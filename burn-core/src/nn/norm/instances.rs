cebdab500b1668bbe64dcbd21a3fa2b1964f0f58
tushushu <xmaseve1984@gmail.com>
2024-02-14 10:51:16 +0800

    InstanceNormConfig

--- burn-core/src/nn/norm/instances.rs
+++ burn-core/src/nn/norm/instances.rs
@@ -12,7 +12,7 @@ pub struct InstanceNormConfig {
     pub num_features: usize,
     /// A value required for numerical stability. Default: 1e-5
     #[config(default = 1e-5)]
-    pub eps: f64,
+    pub epsilon: f64,
     /// Momentum used to update the metrics. Default: 0.1
     #[config(default = 0.1)]
     pub momentum: f64,
@@ -23,11 +23,46 @@ pub struct InstanceNormConfig {
 }
 
 #[derive(Module, Debug)]
-pub struct InstanceNorm<B: Backend> {
+pub struct InstanceNorm<B: Backend, const D: usize> {
     num_features: usize,
-    eps: f64,
+    epsilon: f64,
     momentum: f64,
     affine: bool,
     gamma: Option<Param<Tensor<B, 1>>>,
     beta: Option<Param<Tensor<B, 1>>>,
+}
+
+impl InstanceNormConfig {
+    pub fn init<B: Backend, const D: usize>(&self, device: &B::Device) -> InstanceNorm<B, D> {
+        let (gamma, beta) = if self.affine {
+            let gamma = Tensor::ones([self.num_features], device).into();
+            let beta = Tensor::zeros([self.num_features], device).into();
+            (Some(gamma), Some(beta))
+        } else {
+            (None, None)
+        };
+
+        InstanceNorm {
+            num_features: self.num_features,
+            epsilon: self.epsilon,
+            momentum: self.momentum,
+            affine: self.affine,
+            gamma,
+            beta,
+        }
+    }
+
+    pub fn init_with<B: Backend, const D: usize>(
+        &self,
+        record: InstanceNormRecord<B, D>,
+    ) -> InstanceNorm<B, D> {
+        InstanceNorm {
+            num_features: self.num_features,
+            epsilon: self.epsilon,
+            momentum: self.momentum,
+            affine: self.affine,
+            gamma: record.gamma,
+            beta: record.beta,
+        }
+    }
 }
