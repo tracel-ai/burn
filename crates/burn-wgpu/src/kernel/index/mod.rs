a16199870ab0baa339c46d5cd4a156f5528f172d
nathaniel <nathaniel.simard.42@gmail.com>
2024-02-28 11:05:10 -0500

    Add slice

--- crates/burn-wgpu/src/kernel/index/mod.rs
+++ crates/burn-wgpu/src/kernel/index/mod.rs
@@ -4,11 +4,13 @@ mod scatter;
 mod select;
 mod select_assign;
 mod slice;
+mod slice_new;
 
 pub use repeat::*;
 pub use select::*;
 pub use select_assign::*;
 pub use slice::*;
+pub use slice_new::*;
 
 pub(crate) use gather::*;
 pub(crate) use scatter::*;
