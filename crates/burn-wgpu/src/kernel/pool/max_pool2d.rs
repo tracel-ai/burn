604e40982193f96483d70f0747280328fed4734d
nathaniel <nathaniel.simard.42@gmail.com>
2024-02-29 09:59:26 -0500

    Max pool2d backward wip

--- crates/burn-wgpu/src/kernel/pool/max_pool2d.rs
+++ crates/burn-wgpu/src/kernel/pool/max_pool2d.rs
@@ -1,73 +1,123 @@
 use crate::{
+    codegen::dialect::gpu::{gpu, Elem, Scope, Variable},
     compute::StaticKernel,
     element::JitElement,
     kernel::{
-        self, elemwise_workgroup,
-        pool::{build_output_and_info_pool2d, build_pool2d_info},
-        KernelSettings, WORKGROUP_DEFAULT,
+        self, elemwise_workgroup, pool::build_pool2d_info, KernelSettings, WORKGROUP_DEFAULT,
     },
     kernel_wgsl,
-    ops::numeric::empty_device,
     tensor::JitTensor,
     Runtime,
 };
+use std::marker::PhantomData;
 
-kernel_wgsl!(MaxPool2d, "../../template/pool/max_pool2d.wgsl");
 kernel_wgsl!(
     MaxPool2dWithIndicesBackward,
     "../../template/pool/max_pool2d_with_indices_backward.wgsl"
 );
-kernel_wgsl!(
-    MaxPool2dWithIndices,
-    "../../template/pool/max_pool2d_with_indices.wgsl"
-);
 
-pub(crate) fn max_pool2d_old<R: Runtime, E: JitElement>(
-    x: JitTensor<R, E, 4>,
+#[derive(new)]
+pub struct MaxPool2dWithIndicesBackwardEagerKernel<R: Runtime, E: JitElement> {
     kernel_size: [usize; 2],
-    stride: [usize; 2],
-    padding: [usize; 2],
-    dilation: [usize; 2],
-) -> JitTensor<R, E, 4> {
-    let (info_handle, output) =
-        build_output_and_info_pool2d(&x, kernel_size, stride, padding, dilation);
-    let kernel = StaticKernel::<
-        KernelSettings<MaxPool2d, E, i32, WORKGROUP_DEFAULT, WORKGROUP_DEFAULT, 1>,
-    >::new(elemwise_workgroup(
-        output.shape.num_elements(),
-        WORKGROUP_DEFAULT,
-    ));
-
-    x.client
-        .execute(Box::new(kernel), &[&x.handle, &output.handle, &info_handle]);
-
-    output
+    _runtime: PhantomData<R>,
+    _elem: PhantomData<E>,
 }
 
-pub(crate) fn max_pool2d_with_indices_old<R: Runtime, E: JitElement, I: JitElement>(
-    x: JitTensor<R, E, 4>,
+pub struct MaxPool2dBackwardComputeShader {
+    indices: Variable,
+    grad: Variable,
+    output: Variable,
     kernel_size: [usize; 2],
-    stride: [usize; 2],
-    padding: [usize; 2],
-    dilation: [usize; 2],
-) -> (JitTensor<R, E, 4>, JitTensor<R, I, 4>) {
-    let (info_handle, output) =
-        build_output_and_info_pool2d(&x, kernel_size, stride, padding, dilation);
-    let indices = empty_device(x.client.clone(), x.device, output.shape.clone());
+}
 
-    let kernel = StaticKernel::<
-        KernelSettings<MaxPool2dWithIndices, E, i32, WORKGROUP_DEFAULT, WORKGROUP_DEFAULT, 1>,
-    >::new(elemwise_workgroup(
-        output.shape.num_elements(),
-        WORKGROUP_DEFAULT,
-    ));
+impl MaxPool2dBackwardComputeShader {
+    pub fn expand(self, scope: &mut Scope) {
+        let grad = self.grad;
+        let output = self.output;
+        let id = Variable::Id;
 
-    x.client.execute(
-        Box::new(kernel),
-        &[&x.handle, &output.handle, &indices.handle, &info_handle],
-    );
+        let grad_stride_0 = scope.create_local(Elem::UInt);
+        let grad_stride_1 = scope.create_local(Elem::UInt);
+        let grad_stride_2 = scope.create_local(Elem::UInt);
+        let grad_stride_3 = scope.create_local(Elem::UInt);
 
-    (output, indices)
+        let grad_shape_2 = scope.create_local(Elem::UInt);
+        let grad_shape_3 = scope.create_local(Elem::UInt);
+
+        let output_stride_0 = scope.create_local(Elem::UInt);
+        let output_stride_1 = scope.create_local(Elem::UInt);
+        let output_stride_2 = scope.create_local(Elem::UInt);
+        let output_stride_3 = scope.create_local(Elem::UInt);
+
+        let output_shape_0 = scope.create_local(Elem::UInt);
+        let output_shape_1 = scope.create_local(Elem::UInt);
+        let output_shape_2 = scope.create_local(Elem::UInt);
+        let output_shape_3 = scope.create_local(Elem::UInt);
+
+        gpu!(scope, grad_stride_0 = stride(grad, 0u32));
+        gpu!(scope, grad_stride_1 = stride(grad, 1u32));
+        gpu!(scope, grad_stride_2 = stride(grad, 2u32));
+        gpu!(scope, grad_stride_3 = stride(grad, 3u32));
+
+        gpu!(scope, grad_shape_2 = shape(grad, 2u32));
+        gpu!(scope, grad_shape_3 = shape(grad, 3u32));
+
+        gpu!(scope, output_stride_0 = stride(output, 0u32));
+        gpu!(scope, output_stride_1 = stride(output, 1u32));
+        gpu!(scope, output_stride_2 = stride(output, 2u32));
+        gpu!(scope, output_stride_3 = stride(output, 3u32));
+
+        gpu!(scope, output_shape_0 = shape(output, 0u32));
+        gpu!(scope, output_shape_1 = shape(output, 1u32));
+        gpu!(scope, output_shape_2 = shape(output, 2u32));
+        gpu!(scope, output_shape_3 = shape(output, 3u32));
+
+        let pool_stride_0 = Variable::GlobalScalar(0, Elem::UInt);
+        let pool_stride_1 = Variable::GlobalScalar(1, Elem::UInt);
+        let dilation_0 = Variable::GlobalScalar(2, Elem::UInt);
+        let dilation_1 = Variable::GlobalScalar(3, Elem::UInt);
+        let padding_0 = Variable::GlobalScalar(4, Elem::UInt);
+        let padding_1 = Variable::GlobalScalar(5, Elem::UInt);
+
+        let [kernel_size_0, kernel_size_1] = self.kernel_size;
+
+        let b = scope.create_local(Elem::UInt);
+        let c = scope.create_local(Elem::UInt);
+        let ih = scope.create_local(Elem::UInt);
+        let iw = scope.create_local(Elem::UInt);
+
+        gpu!(scope, b = id / output_stride_0);
+        gpu!(scope, b = b % output_shape_0);
+
+        gpu!(scope, c = id / output_stride_1);
+        gpu!(scope, c = c % output_shape_1);
+
+        gpu!(scope, ih = id / output_stride_2);
+        gpu!(scope, ih = ih % output_shape_2);
+
+        gpu!(scope, iw = id / output_stride_3);
+        gpu!(scope, iw = iw % output_shape_3);
+
+        let kms_0 = scope.create_local(Elem::Int);
+        let kms_1 = scope.create_local(Elem::Int);
+
+        gpu!(scope, kms_0 = dilation_0 * kernel_size_0);
+        gpu!(scope, kms_0 = kms_0 - pool_stride_0);
+
+        gpu!(scope, kms_1 = dilation_1 * kernel_size_1);
+        gpu!(scope, kms_1 = kms_1 + pool_stride_1);
+
+        let oh_start_tmp = scope.create_local(Elem::UInt);
+        let ow_start_tmp = scope.create_local(Elem::UInt);
+
+        gpu!(scope, oh_start_tmp = ih + padding_0);
+        gpu!(scope, oh_start_tmp = oh_start_tmp - kms_0);
+        gpu!(scope, oh_start_tmp = oh_start_tmp / pool_stride_0);
+
+        gpu!(scope, ow_start_tmp = iw + padding_1);
+        gpu!(scope, ow_start_tmp = ow_start_tmp - kms_1);
+        gpu!(scope, ow_start_tmp = ow_start_tmp / pool_stride_1);
+    }
 }
 
 pub(crate) fn max_pool2d_with_indices_backward<R: Runtime, E: JitElement, I: JitElement>(
