93f3470cda7740208edd60c83cd9436383d50d30
jackdarlison <jdarlison7@gmail.com>
2024-03-04 20:20:56 +0000

    Switched arguments in `reshape_args_usize` check

--- crates/burn-tensor/src/tensor/api/base.rs
+++ crates/burn-tensor/src/tensor/api/base.rs
@@ -1748,7 +1748,7 @@ impl<const D2: usize> ReshapeArgs<D2> for Shape<D2> {
         self,
         tensor: &Tensor<B, D, K>,
     ) -> Shape<D2> {
-        check!(TensorCheck::reshape_args_usize(&self, &tensor.shape()));
+        check!(TensorCheck::reshape_args_usize(&tensor.shape(), &self));
 
         self
     }
@@ -1760,7 +1760,7 @@ impl<const D2: usize> ReshapeArgs<D2> for [usize; D2] {
     ) -> Shape<D2> {
         let shape = Shape::from(self);
 
-        check!(TensorCheck::reshape_args_usize(&shape, &tensor.shape()));
+        check!(TensorCheck::reshape_args_usize(&tensor.shape(), &shape));
 
         shape
     }
