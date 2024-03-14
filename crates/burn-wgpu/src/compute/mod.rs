9a624cec7daf212dd9c63fe1e8952636991de910
Piotr Stankiewicz <piotr.stankiewicz@docker.com>
2024-03-14 11:34:20 +0100

    burn-wgpu: Move any use of wgpu into a separate module
    Currently the burn-wgpu crate is hardcoded to use the wgpu WebGPU
    implementaion. It would be nice to be able to use other WebGPU
    implementations as new features may land at different times, and there
    may be potential performance gains to unlock. So, separate any use of
    the wgpu crate into a separate module, in preparation for adding the
    ability to target other WebGPU implementaions.
    
    Signed-off-by: Piotr Stankiewicz <piotr.stankiewicz@docker.com>

--- crates/burn-wgpu/src/compute/mod.rs
+++ crates/burn-wgpu/src/compute/mod.rs
@@ -1,5 +1,7 @@
 mod server;
 mod storage;
+mod wgpu_api_shim;
 
 pub use server::*;
 pub use storage::*;
+pub use wgpu_api_shim::*;
--- crates/burn-wgpu/src/compute/storage.rs
+++ crates/burn-wgpu/src/compute/storage.rs
@@ -1,12 +1,16 @@
+use crate::compute::{
+    WebGPUBindingResource, WebGPUBuffer, WebGPUBufferAddress, WebGPUBufferBinding,
+    WebGPUBufferDescriptor, WebGPUBufferSize, WebGPUDevice, COPY_DST, COPY_SRC, STORAGE,
+};
 use burn_compute::storage::{ComputeStorage, StorageHandle, StorageId, StorageUtilization};
 use hashbrown::HashMap;
 use std::{num::NonZeroU64, sync::Arc};
 
 /// Buffer storage for wgpu.
 pub struct WgpuStorage {
-    memory: HashMap<StorageId, Arc<wgpu::Buffer>>,
+    memory: HashMap<StorageId, Arc<WebGPUBuffer>>,
     deallocations: Vec<StorageId>,
-    device: Arc<wgpu::Device>,
+    device: Arc<WebGPUDevice>,
 }
 
 impl core::fmt::Debug for WgpuStorage {
@@ -19,23 +23,23 @@ impl core::fmt::Debug for WgpuStorage {
 #[derive(new, Debug)]
 pub struct WgpuResource {
     /// The wgpu buffer.
-    pub buffer: Arc<wgpu::Buffer>,
+    pub buffer: Arc<WebGPUBuffer>,
     /// How the resource is used.
     pub kind: WgpuResourceKind,
 }
 
 impl WgpuResource {
     /// Return the binding view of the buffer.
-    pub fn as_binding(&self) -> wgpu::BindingResource {
+    pub fn as_binding(&self) -> WebGPUBindingResource {
         let binding = match &self.kind {
             WgpuResourceKind::Full => self.buffer.as_entire_buffer_binding(),
-            WgpuResourceKind::Slice(offs, size) => wgpu::BufferBinding {
+            WgpuResourceKind::Slice(offs, size) => WebGPUBufferBinding {
                 buffer: &self.buffer,
                 offset: *offs,
                 size: Some(*size),
             },
         };
-        wgpu::BindingResource::Buffer(binding)
+        WebGPUBindingResource::Buffer(binding)
     }
 
     /// Return the buffer size.
@@ -61,13 +65,13 @@ pub enum WgpuResourceKind {
     /// Represents an entire buffer.
     Full,
     /// A slice over a buffer.
-    Slice(wgpu::BufferAddress, wgpu::BufferSize),
+    Slice(WebGPUBufferAddress, WebGPUBufferSize),
 }
 
 /// Keeps actual wgpu buffer references in a hashmap with ids as key.
 impl WgpuStorage {
-    /// Create a new storage on the given [device](wgpu::Device).
-    pub fn new(device: Arc<wgpu::Device>) -> Self {
+    /// Create a new storage on the given [device](WebGPUDevice).
+    pub fn new(device: Arc<WebGPUDevice>) -> Self {
         Self {
             memory: HashMap::new(),
             deallocations: Vec::new(),
@@ -104,12 +108,11 @@ impl ComputeStorage for WgpuStorage {
 
     fn alloc(&mut self, size: usize) -> StorageHandle {
         let id = StorageId::new();
-        let buffer = Arc::new(self.device.create_buffer(&wgpu::BufferDescriptor {
+
+        let buffer = Arc::new(self.device.create_buffer(&WebGPUBufferDescriptor {
             label: None,
             size: size as u64,
-            usage: wgpu::BufferUsages::COPY_DST
-                | wgpu::BufferUsages::STORAGE
-                | wgpu::BufferUsages::COPY_SRC,
+            usage: COPY_DST | STORAGE | COPY_SRC,
             mapped_at_creation: false,
         }));
 
