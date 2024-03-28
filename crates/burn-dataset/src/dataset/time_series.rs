c0a8b87fadb7409be6d6be24fc8eeeb65aab3395
Nico Zweifel <34443492+NicoZweifel@users.noreply.github.com>
2024-03-29 00:47:27 +0100

    format

--- crates/burn-dataset/src/dataset/time_series.rs
+++ crates/burn-dataset/src/dataset/time_series.rs
@@ -5,15 +5,15 @@ use crate::Dataset;
 #[derive(new)]
 pub struct TimeSeriesDataset<D> {
     dataset: D,
-    window_size: usize
+    window_size: usize,
 }
-
 
 impl<D, I> Dataset<Vec<I>> for TimeSeriesDataset<D>
 where
     D: Dataset<I>,
     I: Clone + Send + Sync,
-{    fn get(&self, index: usize) -> Option<Vec<I>> {
+{
+    fn get(&self, index: usize) -> Option<Vec<I>> {
         (index..index + self.window_size)
             .map(|x| self.dataset.get(x))
             .collect()
