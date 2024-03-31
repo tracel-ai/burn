30fea1a87529c3c1f548705ac9e9a5e362bcf690
Sylvain Benner <sylvain@benner.online>
2024-03-30 23:39:10 -0400

    [backend-comparison] Remove tui

--- backend-comparison/src/burnbenchapp/mod.rs
+++ backend-comparison/src/burnbenchapp/mod.rs
@@ -3,14 +3,7 @@ mod base;
 
 pub use base::*;
 
-#[cfg(feature = "tui")]
-mod tui;
-#[cfg(feature = "tui")]
-use tui::TuiApplication as App;
-
-#[cfg(not(feature = "tui"))]
 mod term;
-#[cfg(not(feature = "tui"))]
 use term::TermApplication as App;
 
 const BENCHMARKS_TARGET_DIR: &str = "target/benchmarks";
