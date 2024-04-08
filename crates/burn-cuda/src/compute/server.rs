8f7a6a143b5b9d9723f0f0090829da1c28dc5170
nathaniel <nathaniel.simard.42@gmail.com>
2024-04-07 20:04:28 -0400

    Updagr grid :

--- crates/burn-cuda/src/compute/server.rs
+++ crates/burn-cuda/src/compute/server.rs
@@ -5,6 +5,7 @@ use burn_compute::{
     server::{self, ComputeServer},
 };
 use burn_jit::compute::{JitAutotuneKey, Kernel, WorkGroup};
+use burn_jit::gpu::WorkgroupSize;
 use cudarc::driver::{LaunchAsync, LaunchConfig};
 use cudarc::{driver::CudaDevice, nvrtc::compile_ptx};
 use std::{collections::HashMap, sync::Arc};
@@ -15,11 +16,11 @@ pub struct CudaServer<MM: MemoryManagement<CudaStorage>> {
     device: Arc<CudaDevice>,
     manual_available: HashMap<usize, Vec<server::Handle<Self>>>,
     manual_taken: Vec<(usize, server::Handle<Self>)>,
-    module_names: HashMap<String, String>,
+    module_names: HashMap<String, (String, WorkgroupSize)>,
 }
 
 impl<MM: MemoryManagement<CudaStorage>> ComputeServer for CudaServer<MM> {
-    type Kernel = Box<dyn Kernel>;
+    type Kernel = Kernel;
     type Storage = CudaStorage;
     type MemoryManagement = MM;
     type AutotuneKey = JitAutotuneKey;
@@ -51,23 +52,22 @@ impl<MM: MemoryManagement<CudaStorage>> ComputeServer for CudaServer<MM> {
 
         if !self.module_names.contains_key(&kernel_id) {
             let name = format!("m{}", self.module_names.len());
-            let source = kernel.source().complete();
-            println!("compiled {name}:");
-            println!("{source}");
-            let kernel = compile_ptx(source).unwrap();
+            let kernel = kernel.compile();
+            let workgroup_siz = kernel.workgroup_size;
+            let kernel = compile_ptx(kernel.source).unwrap();
 
             self.device.load_ptx(kernel, &name, &["kernel"]).unwrap();
-            self.module_names.insert(kernel_id.clone(), name);
+            self.module_names
+                .insert(kernel_id.clone(), (name, workgroup_siz));
         }
 
         let task = ComputeTask::new(
             kernel_id,
-            kernel.workgroup(),
+            kernel.launch_settings().workgroup,
             handles
                 .iter()
                 .map(|h| self.memory_management.get(&h.memory).as_binding())
                 .collect(),
-            (32, 32, 1),
         );
 
         self.execute_task(task);
@@ -85,7 +85,6 @@ struct ComputeTask {
     kernel_id: String,
     workgroup: WorkGroup,
     bindings: Vec<Binding>,
-    grid: (u32, u32, u32),
 }
 
 impl<MM: MemoryManagement<CudaStorage>> CudaServer<MM> {
@@ -137,12 +136,12 @@ impl<MM: MemoryManagement<CudaStorage>> CudaServer<MM> {
 
     fn execute_task(&mut self, task: ComputeTask) {
         let workgroup = task.workgroup;
+        let (module_name, grid) = self.module_names.get(&task.kernel_id).unwrap();
         let cfg = LaunchConfig {
             grid_dim: (workgroup.x, workgroup.y, workgroup.z),
-            block_dim: task.grid,
+            block_dim: (grid.x, grid.y, grid.z),
             shared_mem_bytes: 0,
         };
-        let module_name = self.module_names.get(&task.kernel_id).unwrap();
 
         unsafe {
             let func = self.device.get_func(module_name, "kernel").unwrap();
