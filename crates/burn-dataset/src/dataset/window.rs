afda3c10c722aaea654b543959ef2c9d6bbda709
Nico Zweifel <34443492+NicoZweifel@users.noreply.github.com>
2024-04-11 01:47:08 +0200

    chore: format

--- crates/burn-dataset/src/dataset/window.rs
+++ crates/burn-dataset/src/dataset/window.rs
@@ -50,10 +50,8 @@ impl<'a, I> Dataset<Vec<I>> for WindowDataset<'a, I> {
     ///
     /// A size representing the number of windows.
     fn len(&self) -> usize {
-        max(
-            self.dataset.len() as isize - self.size.get() as isize + 1,
-            0,
-        ) as usize
+        let len = self.dataset.len() as isize - self.size.get() as isize + 1;
+        max(len, 0) as usize
     }
 }
 
