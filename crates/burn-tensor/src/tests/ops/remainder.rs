c90f352afc7196497f154956d25c8746143fc740
Mathias <mathiaswi177@gmail.com>
2024-04-10 20:47:30 -0700

    Add more unit tests in to cover bases

--- crates/burn-tensor/src/tests/ops/remainder.rs
+++ crates/burn-tensor/src/tests/ops/remainder.rs
@@ -28,4 +28,69 @@ mod tests {
         let data_expected = Data::from([-0.5, -1.0, 0.0, -0.5, -1.0]);
         data_expected.assert_approx_eq(&data_actual, 3);
     }
+
+    #[test]
+    fn should_be_zero() {
+        let data = Data::from([0.0, 0.0, 0.0]);
+        let device = Default::default();
+        let tensor = Tensor::<TestBackend, 1>::from_data(data, &device);
+
+        let output = tensor.remainder_scalar(3.5);
+
+        let data_actual = output.into_data();
+        let data_expected = Data::from([0.0, 0.0, 0.0]);
+        data_expected.assert_approx_eq(&data_actual, 3);
+    }
+
+    #[test]
+    fn should_have_no_remainder() {
+        let data = Data::from([-4.0, 4.0]);
+        let device = Default::default();
+        let tensor = Tensor::<TestBackend, 1>::from_data(data, &device);
+
+        let output = tensor.remainder_scalar(4.0);
+
+        let data_actual = output.into_data();
+        let data_expected = Data::from([-0.0, 0.0]);
+        data_expected.assert_approx_eq(&data_actual, 3);
+    }
+
+    #[test]
+    fn should_be_negative() {
+        let data = Data::from([-7.0, -3.0, 2.0, 6.0]);
+        let device = Default::default();
+        let tensor = Tensor::<TestBackend, 1>::from_data(data, &device);
+
+        let output = tensor.remainder_scalar(-2.5);
+
+        let data_actual = output.into_data();
+        let data_expected = Data::from([-2.0, -0.50, -0.50, -1.5]);
+        data_expected.assert_approx_eq(&data_actual, 3);
+    }
+
+    #[test]
+    fn should_support_fp_dividends() {
+        let data = Data::from([-7.5, -2.5, 2.5, 7.5]);
+        let device = Default::default();
+        let tensor = Tensor::<TestBackend, 1>::from_data(data, &device);
+
+        let output = tensor.remainder_scalar(3.0);
+
+        let data_actual = output.into_data();
+        let data_expected = Data::from([1.5, 0.5, 2.5, 1.5]);
+        data_expected.assert_approx_eq(&data_actual, 3);
+    }
+
+    #[test]
+    fn should_support_large_divisor() {
+        let data = Data::from([-1.0, 1.0]);
+        let device = Default::default();
+        let tensor = Tensor::<TestBackend, 1>::from_data(data, &device);
+
+        let output = tensor.remainder_scalar(10.0);
+
+        let data_actual = output.into_data();
+        let data_expected = Data::from([9.0, 1.0]);
+        data_expected.assert_approx_eq(&data_actual, 3);
+    }
 }
