6805081c657a39612699e8b456a8d81dbda538ad
Guillaume Lagrange <lagrange.guillaume.1@gmail.com>
2024-04-15 11:59:59 -0400

    Fix comment

--- crates/burn-import/src/onnx/dim_inference.rs
+++ crates/burn-import/src/onnx/dim_inference.rs
@@ -228,7 +228,7 @@ fn reduce_mean_update_outputs(node: &mut Node) {
     if dim_only {
         node.outputs[0].ty = ArgType::Tensor(tensor);
     } else {
-        // NOTE: ReduceMax w/o keepdims reduces to a scalar value, but Burn doesn't have
+        // NOTE: ReduceMean w/o keepdims reduces to a scalar value, but Burn doesn't have
         // 0-dim tensor so we can't track or perform other ops on that value
         // node.outputs[0].ty = ArgType::Scalar(tensor.elem_type);
         node.outputs[0].ty = ArgType::Tensor(TensorType { dim: 1, ..tensor });
