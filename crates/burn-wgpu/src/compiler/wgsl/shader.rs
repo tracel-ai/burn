f81a17248540a2040c4ae82957468628c3897aba
nathaniel <nathaniel.simard.42@gmail.com>
2024-04-25 20:50:26 -0400

    Real fix

--- crates/burn-wgpu/src/compiler/wgsl/shader.rs
+++ crates/burn-wgpu/src/compiler/wgsl/shader.rs
@@ -213,13 +213,7 @@ impl Display for Location {
 impl Display for Visibility {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         match self {
-            // We have to set the visibility of all buffers to `read_write`, otherwise we may
-            // have the error: 'Attempted to use buffer with conflicting usages'
-            //
-            // This is because with lazy execution AND buffer reuse, the same buffer can be bound to
-            // two different variables with different visibility. This is especially true with
-            // operation fusion.
-            Visibility::Read => f.write_str("read_write"),
+            Visibility::Read => f.write_str("read"),
             Visibility::ReadWrite => f.write_str("read_write"),
         }
     }
