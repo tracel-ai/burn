669cb1a574fee9ccc0d6a6097117fe073b4c481c
Sylvain Benner <sylvain@benner.online>
2024-04-26 15:42:02 -0400

    [guide] Remove ambiguity lib vs. executable (#1649)

--- examples/guide/src/lib.rs
+++ examples/guide/src/lib.rs
@@ -1,3 +1,9 @@
+//
+// Note: If you are following the Burn Book guide this file can be ignored.
+//
+// This lib.rs file is added only for convenience so that the code in this
+// guide can be reused.
+//
 pub mod data;
 pub mod inference;
 pub mod model;
