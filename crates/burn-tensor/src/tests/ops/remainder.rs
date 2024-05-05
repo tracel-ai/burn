f7959361b755a905bb54fa2aca54fa5373e7308d
Jonas Kantic <jk.mail@posteo.net>
2024-05-05 16:56:39 +0200

    Adds test for % operator.

--- crates/burn-tensor/src/tests/ops/remainder.rs
+++ crates/burn-tensor/src/tests/ops/remainder.rs
@@ -95,4 +95,17 @@ mod tests {
         let data_expected = Data::from([9.0, 1.0]);
         data_expected.assert_approx_eq(&data_actual, 3);
     }
+
+    #[test]
+    fn should_support_remainder_op() {
+        let data = Data::from([-3.0, -2.0, -1.0, 1.0, 2.0, 3.0]);
+        let device = Default::default();
+        let tensor = Tensor::<TestBackend, 1>::from_data(data, &device);
+
+        let output = tensor % 2.0;
+
+        let data_actual = output.into_data();
+        let data_expected = Data::from([1.0, 0.0, 1.0, 1.0, 0.0, 1.0]);
+        data_expected.assert_approx_eq(&data_actual, 3);
+    }
 }
