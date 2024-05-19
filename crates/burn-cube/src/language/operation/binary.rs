a14cb5c4be23d40591bf7f937039260370ae83de
louisfd <louisfd94@gmail.com>
2024-05-19 11:55:31 -0400

    remove assert in add

--- crates/burn-cube/src/language/operation/binary.rs
+++ crates/burn-cube/src/language/operation/binary.rs
@@ -23,7 +23,6 @@ pub mod add {
                 type Output = Self;
 
                 fn add(self, rhs: Self) -> Self::Output {
-                    assert!(self.vectorization == rhs.vectorization);
                     <$type as $trait>::new(self.val + rhs.val)
                 }
             }
