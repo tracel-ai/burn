1fe8f719869315d7ee6688bb2c72aacc194d5d84
nathaniel <nathaniel.simard.42@gmail.com>
2024-05-29 12:38:13 -0400

    WIP

--- crates/burn-cube/src/language/mod.rs
+++ crates/burn-cube/src/language/mod.rs
@@ -6,10 +6,12 @@ mod context;
 mod element;
 mod indexation;
 mod operation;
+mod subgroup;
 mod topology;
 
 pub use comptime::*;
 pub use context::*;
 pub use element::*;
 pub use operation::*;
+pub use subgroup::*;
 pub use topology::*;
