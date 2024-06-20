2db79dfaa66976cddb0d1809a947ef45864d4e4a
Dilshod Tadjibaev <939125+antimora@users.noreply.github.com>
2024-06-20 16:54:17 -0500

    Update display documentation

--- crates/burn-core/src/module/display.rs
+++ crates/burn-core/src/module/display.rs
@@ -10,14 +10,32 @@ use core::fmt::{Display, Write};
 /// Default display settings for a module.
 pub trait ModuleDisplayDefault {
     /// Attributes of the module used for display purposes.
+    ///
+    /// # Arguments
+    ///
+    /// * `_content` - The content object that contains display settings and attributes.
+    ///
+    /// # Returns
+    ///
+    /// An optional content object containing the display attributes.
     fn content(&self, _content: Content) -> Option<Content>;
 }
 
 /// Trait to implement custom display settings for a module.
+///
+/// In order to implement custom display settings for a module,
+/// 1. Add #[custom_display] attribute to the module struct after #[derive(Module)]
+/// 2. Implement ModuleDisplay trait for the module
 pub trait ModuleDisplay: ModuleDisplayDefault {
-    //TODO mention that attribute needs to be passed
-
     /// Formats the module with provided display settings.
+    ///
+    /// # Arguments
+    ///
+    /// * `passed_settings` - Display settings passed to the module.
+    ///
+    /// # Returns
+    ///
+    /// A string representation of the formatted module.
     fn format(&self, passed_settings: DisplaySettings) -> String {
         let settings = if let Some(custom_settings) = self.custom_settings() {
             custom_settings.inherit(passed_settings)
@@ -85,11 +103,23 @@ pub trait ModuleDisplay: ModuleDisplayDefault {
     }
 
     /// Custom display settings for the module.
+    ///
+    /// # Returns
+    ///
+    /// An optional display settings object.
     fn custom_settings(&self) -> Option<DisplaySettings> {
         None
     }
 
     /// Custom attributes for the module.
+    ///
+    /// # Arguments
+    ///
+    /// * `_content` - The content object that contains display settings and attributes.
+    ///
+    /// # Returns
+    ///
+    /// An optional content object containing the custom attributes.
     fn custom_content(&self, _content: Content) -> Option<Content> {
         None
     }
@@ -121,7 +151,7 @@ impl Default for DisplaySettings {
     fn default() -> Self {
         DisplaySettings {
             show_param_id: None,
-            show_all_attributes: None, // by default, show only the attributes that are not default
+            show_all_attributes: None,
             show_num_parameters: None,
             new_line_after_attribute: None,
             indentation_size: None,
@@ -132,35 +162,79 @@ impl Default for DisplaySettings {
 
 impl DisplaySettings {
     /// Create a new format settings.
+    ///
+    /// # Returns
+    ///
+    /// A new instance of `DisplaySettings`.
     pub fn new() -> Self {
         Default::default()
     }
 
-    /// Set a flag to show module parameters.
+    /// Sets a flag to show module parameters.
+    ///
+    /// # Arguments
+    ///
+    /// * `flag` - Boolean flag to show module parameters.
+    ///
+    /// # Returns
+    ///
+    /// Updated `DisplaySettings` instance.
     pub fn with_show_param_id(mut self, flag: bool) -> Self {
         self.show_param_id = Some(flag);
         self
     }
 
-    /// Set a flag to show module attributes.
+    /// Sets a flag to show module attributes.
+    ///
+    /// # Arguments
+    ///
+    /// * `flag` - Boolean flag to show all module attributes.
+    ///
+    /// # Returns
+    ///
+    /// Updated `DisplaySettings` instance.
     pub fn with_show_all_attributes(mut self, flag: bool) -> Self {
         self.show_all_attributes = Some(flag);
         self
     }
 
-    /// Set a flag to show the number of module parameters.
+    /// Sets a flag to show the number of module parameters.
+    ///
+    /// # Arguments
+    ///
+    /// * `flag` - Boolean flag to show the number of module parameters.
+    ///
+    /// # Returns
+    ///
+    /// Updated `DisplaySettings` instance.
     pub fn with_show_num_parameters(mut self, flag: bool) -> Self {
         self.show_num_parameters = Some(flag);
         self
     }
 
-    /// Set a flag to print a new line after an attribute.
+    /// Sets a flag to print a new line after an attribute.
+    ///
+    /// # Arguments
+    ///
+    /// * `flag` - Boolean flag to print a new line after an attribute.
+    ///
+    /// # Returns
+    ///
+    /// Updated `DisplaySettings` instance.
     pub fn with_new_line_after_attribute(mut self, flag: bool) -> Self {
         self.new_line_after_attribute = Some(flag);
         self
     }
 
-    /// Inherit settings from the provided settings and return a new settings object.
+    /// Inherits settings from the provided settings and return a new settings object.
+    ///
+    /// # Arguments
+    ///
+    /// * `top` - The top level `DisplaySettings` to inherit from.
+    ///
+    /// # Returns
+    ///
+    /// Updated `DisplaySettings` instance.
     pub fn inherit(self, top: Self) -> Self {
         let mut updated = self.clone();
 
@@ -187,51 +261,82 @@ impl DisplaySettings {
         updated
     }
 
-    /// A convenience method to wrap the DisplaySettings struct in an option
+    /// A convenience method to wrap the DisplaySettings struct in an option.
+    ///
+    /// # Returns
+    ///
+    /// An optional `DisplaySettings`.
     pub fn optional(self) -> Option<Self> {
         Some(self)
     }
 
-    /// Increase the level of indentation
+    /// Increases the level of indentation.
+    ///
+    /// # Returns
+    ///
+    /// Updated `DisplaySettings` instance with increased indentation level.
     pub fn level_up(mut self) -> Self {
         self.level += 1;
         self
     }
 
-    /// Gets show_param_id flag, substitutes false if not set.
+    /// Gets `show_param_id` flag, substitutes false if not set.
     ///
     /// This flag is used to print the module parameter ids.
+    ///
+    /// # Returns
+    ///
+    /// A boolean value indicating whether to show parameter ids.
     pub fn show_param_id(&self) -> bool {
         self.show_param_id.unwrap_or(false)
     }
 
-    /// Gets show_all_attributes, substitutes false if not set.
+    /// Gets `show_all_attributes`, substitutes false if not set.
     ///
     /// This flag is used to force to print all module attributes, overriding custom attributes.
+    ///
+    /// # Returns
+    ///
+    /// A boolean value indicating whether to show all attributes.
     pub fn show_all_attributes(&self) -> bool {
         self.show_all_attributes.unwrap_or(false)
     }
 
-    /// Gets show_num_parameters, substitutes false if not set.
+    /// Gets `show_num_parameters`, substitutes false if not set.
     ///
     /// This flag is used to print the number of module parameters.
+    ///
+    /// # Returns
+    ///
+    /// A boolean value indicating whether to show the number of parameters.
     pub fn show_num_parameters(&self) -> bool {
         self.show_num_parameters.unwrap_or(false)
     }
 
-    /// Gets new_line_after_attribute, substitutes true if not set.
+    /// Gets `new_line_after_attribute`, substitutes true if not set.
     ///
     /// This flag is used to print a new line after an attribute.
+    ///
+    /// # Returns
+    ///
+    /// A boolean value indicating whether to print a new line after an attribute.
     pub fn new_line_after_attribute(&self) -> bool {
         self.new_line_after_attribute.unwrap_or(true)
     }
 
-    /// Gets indentation_size, substitutes 2 if not set.
+    /// Gets `indentation_size`, substitutes 2 if not set.
+    ///
+    /// This flag is used to set the size of indentation.
+    ///
+    /// # Returns
+    ///
+    /// An integer value indicating the size of indentation.
     pub fn indentation_size(&self) -> usize {
         self.indentation_size.unwrap_or(2)
     }
 }
-/// Struct to store the attributes of a module.
+
+/// Struct to store the attributes of a module for formatting.
 #[derive(Clone, Debug)]
 pub struct Content {
     /// List of attributes.
@@ -248,7 +353,15 @@ pub struct Content {
 }
 
 impl Content {
-    /// Create a new attributes struct.
+    /// Creates a new attributes struct.
+    ///
+    /// # Arguments
+    ///
+    /// * `display_settings` - Display settings for the content.
+    ///
+    /// # Returns
+    ///
+    /// A new instance of `Content`.
     pub fn new(display_settings: DisplaySettings) -> Self {
         Content {
             attributes: Vec::new(),
@@ -258,12 +371,16 @@ impl Content {
         }
     }
 
-    /// Add an attribute to the format settings. The value will be formatted and stored as a string.
+    /// Adds an attribute to the format settings. The value will be formatted and stored as a string.
     ///
     /// # Arguments
     ///
     /// * `name` - Name of the attribute.
     /// * `value` - Value of the attribute.
+    ///
+    /// # Returns
+    ///
+    /// Updated `Content` instance with the new attribute added.
     pub fn add<T: ModuleDisplay + ?Sized>(mut self, name: &str, value: &T) -> Self {
         if self.single_item.is_some() {
             panic!("Cannot add multiple attributes when single item is set.");
@@ -278,11 +395,15 @@ impl Content {
         self
     }
 
-    /// Add a single item.
+    /// Adds a single item.
     ///
     /// # Arguments
     ///
     /// * `value` - Rendered string of the single item.
+    ///
+    /// # Returns
+    ///
+    /// Updated `Content` instance with the single item added.
     pub fn add_single<T: ModuleDisplay>(mut self, value: &T) -> Self {
         if !self.attributes.is_empty() {
             panic!("Cannot add single item when attributes are set.");
@@ -293,7 +414,15 @@ impl Content {
         self
     }
 
-    /// Add a single item.
+    /// Adds a single item.
+    ///
+    /// # Arguments
+    ///
+    /// * `value` - Formatted display value.
+    ///
+    /// # Returns
+    ///
+    /// Updated `Content` instance with the formatted single item added.
     pub fn add_formatted<T: Display>(mut self, value: &T) -> Self {
         if !self.attributes.is_empty() {
             panic!("Cannot add single item when attributes are set.");
@@ -305,6 +434,10 @@ impl Content {
 
     /// A convenience method to wrap the Attributes struct in an option
     /// because it is often used as an optional field.
+    ///
+    /// # Returns
+    ///
+    /// An optional `Content`.
     pub fn optional(self) -> Option<Self> {
         if self.attributes.is_empty() && self.single_item.is_none() && self.top_level_type.is_none()
         {
@@ -314,7 +447,15 @@ impl Content {
         }
     }
 
-    /// Set the top level type name.
+    /// Sets the top level type name.
+    ///
+    /// # Arguments
+    ///
+    /// * `ty` - The type name to set.
+    ///
+    /// # Returns
+    ///
+    /// Updated `Content` instance with the top level type name set.
     pub fn set_top_level_type(mut self, ty: &str) -> Self {
         self.top_level_type = Some(ty.to_owned());
         self
@@ -335,6 +476,10 @@ pub struct Attribute {
 }
 
 /// Extracts the short name of a type T
+///
+/// # Returns
+///
+/// A string slice representing the short name of the type.
 pub fn extract_type_name<T: ?Sized>() -> &'static str {
     // Get the full type name of T, including module path and generic parameters
     let ty = any::type_name::<T>();
@@ -364,40 +509,3 @@ pub fn extract_type_name<T: ?Sized>() -> &'static str {
         ty
     }
 }
-
-// #[cfg(test)]
-// mod tests {
-//     use super::*;
-
-//     #[test]
-//     fn test_extract_type_name_usize() {
-//         let x: usize = 42;
-//         assert_eq!(extract_type_name(x), "usize");
-//     }
-
-//     #[test]
-//     fn test_extract_type_name_vec() {
-//         let x = vec![1, 2, 3];
-//         assert_eq!(extract_type_name(x), "Vec");
-//     }
-
-//     #[test]
-//     fn test_extract_type_name_option() {
-//         let x = Some(42);
-//         assert_eq!(extract_type_name(x), "Option");
-//     }
-
-//     #[test]
-//     fn test_extract_type_name_result() {
-//         let x: Result<i32, String> = Ok(42);
-//         assert_eq!(extract_type_name(x), "Result");
-//     }
-
-//     #[test]
-//     fn test_extract_type_name_hashmap() {
-//         use std::collections::HashMap;
-//         let mut x = HashMap::new();
-//         x.insert("key", "value");
-//         assert_eq!(extract_type_name(x), "HashMap");
-//     }
-// }
