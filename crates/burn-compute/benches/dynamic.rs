4c9097030f07c582206c9c42886dcfadc62a825f
mepatrick73 <114622680+mepatrick73@users.noreply.github.com>
2024-06-25 18:16:46 -0400

    Perf/dynamic mm slice adressing (#1917)
    * basic implementation of virtual memory addressing for fast index + merging (there is a bug with slice padding

---
+++ crates/burn-compute/benches/dynamic.rs
@@ -0,0 +1,22 @@
+use std::collections::LinkedList;
+
+use burn_compute::{
+    memory_management::{dynamic::DynamicMemoryManagement, MemoryManagement},
+    storage::BytesStorage,
+};
+
+const MB: usize = 1024 * 1024;
+fn main() {
+    let start = std::time::Instant::now();
+    let storage = BytesStorage::default();
+    let mut mm = DynamicMemoryManagement::new(storage);
+    let mut handles = LinkedList::new();
+    for _ in 0..100 * 2048 {
+        if handles.len() >= 4000 {
+            handles.pop_front();
+        }
+        let handle = mm.reserve(MB, || {});
+        handles.push_back(handle);
+    }
+    println!("{:?}", start.elapsed());
+}
