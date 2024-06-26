a762ede4ab0738aabe47ab4df6a0784609f0a667
Dilshod Tadjibaev <939125+antimora@users.noreply.github.com>
2024-06-26 09:49:31 -0500

    Fix conv transpose

--- crates/burn-import/src/burn/node/conv_transpose_2d.rs
+++ crates/burn-import/src/burn/node/conv_transpose_2d.rs
@@ -98,6 +98,7 @@ impl<PS: PrecisionSettings> NodeCodegen<PS> for ConvTranspose2dNode<PS> {
             groups: ConstantRecord::new(),
             padding: [ConstantRecord::new(); 2],
             padding_out: [ConstantRecord::new(); 2],
+            channels: [ConstantRecord::new(); 2],
         };
 
         let item = Record::into_item::<PS>(record);
