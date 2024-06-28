b61be9c54c15207754e8b39460e8fc46bb7aaf16
ArthurBrussee <arthur.brussee@gmail.com>
2024-06-28 16:43:42 +0100

    Use async channel in mspc throughout.

--- crates/burn-compute/src/channel/mpsc.rs
+++ crates/burn-compute/src/channel/mpsc.rs
@@ -1,9 +1,5 @@
-use std::{
-    sync::{mpsc, Arc},
-    thread,
-};
-
-use burn_common::sync_type::SyncType;
+use burn_common::{reader::Reader, sync_type::SyncType};
+use std::{sync::Arc, thread};
 
 use super::ComputeChannel;
 use crate::{
@@ -30,7 +26,7 @@ where
     sender: async_channel::Sender<Message<Server>>,
 }
 
-type Callback<Response> = mpsc::Sender<Response>;
+type Callback<Response> = async_channel::Sender<Response>;
 
 enum Message<Server>
 where
@@ -56,33 +52,33 @@ where
         let (sender, receiver) = async_channel::unbounded();
 
         let _handle = thread::spawn(move || {
-            // Run the whole procedure as one blocking future. This is much simpler than trying to use some multithreaded
-            // executor.
+            // Run the whole procedure as one blocking future. This is much simpler than trying
+            // to use some multithreaded executor.
             pollster::block_on(async {
                 while let Ok(message) = receiver.recv().await {
                     match message {
                         Message::Read(binding, callback) => {
                             let data = server.read(binding).await;
-                            callback.send(data).unwrap();
+                            callback.send(data).await.unwrap();
                         }
                         Message::GetResource(binding, callback) => {
                             let data = server.get_resource(binding);
-                            callback.send(data).unwrap();
+                            callback.send(data).await.unwrap();
                         }
                         Message::Create(data, callback) => {
                             let handle = server.create(&data);
-                            callback.send(handle).unwrap();
+                            callback.send(handle).await.unwrap();
                         }
                         Message::Empty(size, callback) => {
                             let handle = server.empty(size);
-                            callback.send(handle).unwrap();
+                            callback.send(handle).await.unwrap();
                         }
                         Message::ExecuteKernel(kernel, bindings) => {
                             server.execute(kernel, bindings);
                         }
                         Message::Sync(sync_type, callback) => {
                             server.sync(sync_type);
-                            callback.send(()).unwrap();
+                            callback.send(()).await.unwrap();
                         }
                     };
                 }
@@ -107,52 +103,49 @@ impl<Server> ComputeChannel<Server> for MpscComputeChannel<Server>
 where
     Server: ComputeServer + 'static,
 {
-    async fn read(&self, binding: Binding<Server>) -> Vec<u8> {
-        let (callback, response) = mpsc::channel();
+    fn read(&self, binding: Binding<Server>) -> Reader {
+        let sender = self.state.sender.clone();
 
-        self.state
-            .sender
-            .send(Message::Read(binding, callback))
-            .await
-            .unwrap();
-
-        self.response(response)
+        Box::pin(async move {
+            let (callback, response) = async_channel::unbounded();
+            sender.send(Message::Read(binding, callback)).await.unwrap();
+            handle_response(response.recv().await)
+        })
     }
 
     fn get_resource(
         &self,
         binding: Binding<Server>,
     ) -> <Server::Storage as ComputeStorage>::Resource {
-        let (callback, response) = mpsc::channel();
+        let (callback, response) = async_channel::unbounded();
 
         self.state
             .sender
             .send_blocking(Message::GetResource(binding, callback))
             .unwrap();
 
-        self.response(response)
+        handle_response(response.recv_blocking())
     }
 
     fn create(&self, data: &[u8]) -> Handle<Server> {
-        let (callback, response) = mpsc::channel();
+        let (callback, response) = async_channel::unbounded();
 
         self.state
             .sender
             .send_blocking(Message::Create(data.to_vec(), callback))
             .unwrap();
 
-        self.response(response)
+        handle_response(response.recv_blocking())
     }
 
     fn empty(&self, size: usize) -> Handle<Server> {
-        let (callback, response) = mpsc::channel();
-
+        let (callback, response) = async_channel::unbounded();
         self.state
             .sender
             .send_blocking(Message::Empty(size, callback))
             .unwrap();
 
-        self.response(response)
+        handle_response(response.recv_blocking())
     }
 
     fn execute(&self, kernel: Server::Kernel, bindings: Vec<Binding<Server>>) {
@@ -163,20 +156,18 @@ where
     }
 
     fn sync(&self, sync_type: SyncType) {
-        let (callback, response) = mpsc::channel();
+        let (callback, response) = async_channel::unbounded();
         self.state
             .sender
             .send_blocking(Message::Sync(sync_type, callback))
             .unwrap();
-        self.response(response)
+        handle_response(response.recv_blocking())
     }
 }
 
-impl<Server: ComputeServer> MpscComputeChannel<Server> {
-    fn response<Response>(&self, response: mpsc::Receiver<Response>) -> Response {
-        match response.recv() {
-            Ok(val) => val,
-            Err(err) => panic!("Can't connect to the server correctly {err:?}"),
-        }
+fn handle_response<Response, Err: core::fmt::Debug>(response: Result<Response, Err>) -> Response {
+    match response {
+        Ok(val) => val,
+        Err(err) => panic!("Can't connect to the server correctly {err:?}"),
     }
 }
