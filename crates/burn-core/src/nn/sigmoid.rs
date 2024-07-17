9804bf81b2e80a4b576e48cecbde126325df091a
RuelYasa <xuzijin@outlook.com>
2024-07-18 02:34:44 +0800

    Adding burn::nn::Sigmoid (#2031)

---
+++ crates/burn-core/src/nn/sigmoid.rs
@@ -0,0 +1,38 @@
+use crate as burn;
+
+use crate::module::Module;
+use crate::tensor::backend::Backend;
+use crate::tensor::Tensor;
+
+/// Applies the sigmoid function element-wise
+/// See also [sigmoid](burn::tensor::activation::sigmoid)
+#[derive(Module, Clone, Debug, Default)]
+pub struct Sigmoid;
+
+impl Sigmoid {
+    /// Create the module.
+    pub fn new() -> Self {
+        Self {}
+    }
+    /// Applies the forward pass on the input tensor.
+    ///
+    /// # Shapes
+    ///
+    /// - input: `[..., any]`
+    /// - output: `[..., any]`
+    pub fn forward<B: Backend, const D: usize>(&self, input: Tensor<B, D>) -> Tensor<B, D> {
+        crate::tensor::activation::sigmoid(input)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn display() {
+        let layer = Sigmoid::new();
+
+        assert_eq!(alloc::format!("{}", layer), "Sigmoid");
+    }
+}
