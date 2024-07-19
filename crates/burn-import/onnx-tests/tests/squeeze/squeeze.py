886902c80554c5f2af3cb35288873a033d022078
Mathias <mathiaswi177@gmail.com>
2024-07-19 12:02:52 -0700

    Add script for testing multiple axes along with new ONNX file

--- crates/burn-import/onnx-tests/tests/squeeze/squeeze.py
+++ crates/burn-import/onnx-tests/tests/squeeze/squeeze.py
@@ -3,7 +3,9 @@
 # used to generate model: squeeze.onnx
 
 import torch
+import onnx
 import torch.nn as nn
+from onnx import helper, TensorProto
 
 class Model(nn.Module):
     def __init__(self):
@@ -11,7 +13,6 @@ class Model(nn.Module):
         self.dims = 2
 
     def forward(self, x):
-        # With no second argument, squeeze removes all singleton dimensions
         x = torch.squeeze(x, self.dims)
         return x
 
@@ -42,6 +43,19 @@ def main():
     print(f"Test output data shape: {output.shape}")
     print(f"Test output: {output}")
 
+    # Test for squeezing multiple dimensions
+    test_input_ms = helper.make_tensor_value_info("input", TensorProto.FLOAT, [3, 4, 1, 5, 1])
+    output = helper.make_tensor_value_info("output", TensorProto.FLOAT, [3, 4, 5])
+    squeeze = helper.make_node(op_type="Squeeze", inputs=["input", "axes"], outputs=["output"], name="SqueezeOp")
+    axes = helper.make_tensor("axes", TensorProto.INT64, dims=[2], vals=[2, 4])
+    graph = helper.make_graph([squeeze], "SqueezeMultiple", [test_input_ms], [output], [axes])
+    opset = helper.make_opsetid("", 13)
+    m = helper.make_model(graph, opset_imports=[opset])
+
+    onnx.checker.check_model(m, full_check=True)
+    onnx.save(m, "squeeze_multiple.onnx")
+
+    print(f"Finished exporting model with multiple squeeze axes specified to 13")
 
 if __name__ == "__main__":
     main()
