2dfd870b5b8134fd9f031acb7fd2e2b2c83896fa
mepatrick73 <pameu17@ulaval.ca>
2024-08-02 14:50:58 -0400

    fixing tests

--- crates/burn-tensor/src/tests/ops/repeat_dim.rs
+++ crates/burn-tensor/src/tests/ops/repeat_dim.rs
@@ -5,15 +5,15 @@ mod tests {
 
     #[test]
     fn should_support_repeat_ops() {
-        let data = TensorData::from([[0.0, 1.0, 2.0]]);
-        let tensor = Tensor::<TestBackend, 2>::from_data(data, &Default::default());
+        let data = TensorData::from([[0.0f64, 1.0f64, 2.0f64]]);
+        let tensor = Tensor::<TestBackend, 2>::from_data(data.clone(), &Default::default());
 
         let output = tensor.repeat_dim(0, 4);
         let expected = TensorData::from([
-            [0.0, 1.0, 2.0],
-            [0.0, 1.0, 2.0],
-            [0.0, 1.0, 2.0],
-            [0.0, 1.0, 2.0],
+            [0.0f32, 1.0f32, 2.0f32],
+            [0.0f32, 1.0f32, 2.0f32],
+            [0.0f32, 1.0f32, 2.0f32],
+            [0.0f32, 1.0f32, 2.0f32],
         ]);
 
         output.into_data().assert_eq(&expected, false);
@@ -48,19 +48,31 @@ mod tests {
     #[test]
     fn should_support_float_repeat_on_dims_larger_than_1() {
         let data = TensorData::from([
-            [[1.0, 2.0], [3.0, 4.0]],
-            [[5.0, 6.0], [7.0, 8.0]],
-            [[9.0, 10.0], [11.0, 12.0]],
-            [[13.0, 14.0], [15.0, 16.0]],
+            [[1.0f32, 2.0f32], [3.0f32, 4.0f32]],
+            [[5.0f32, 6.0f32], [7.0f32, 8.0f32]],
+            [[9.0f32, 10.0f32], [11.0f32, 12.0f32]],
+            [[13.0f32, 14.0f32], [15.0f32, 16.0f32]],
         ]);
         let tensor = Tensor::<TestBackend, 3>::from_data(data, &Default::default());
 
         let output = tensor.repeat_dim(2, 2);
         let expected = TensorData::from([
-            [[1.0, 2.0, 1.0, 2.0], [3.0, 4.0, 3.0, 4.0]],
-            [[5.0, 6.0, 5.0, 6.0], [7.0, 8.0, 7.0, 8.0]],
-            [[9.0, 10.0, 9.0, 10.0], [11.0, 12.0, 11.0, 12.0]],
-            [[13.0, 14.0, 13.0, 14.0], [15.0, 16.0, 15.0, 16.0]],
+            [
+                [1.0f32, 2.0f32, 1.0f32, 2.0f32],
+                [3.0f32, 4.0f32, 3.0f32, 4.0f32],
+            ],
+            [
+                [5.0f32, 6.0f32, 5.0f32, 6.0f32],
+                [7.0f32, 8.0f32, 7.0f32, 8.0f32],
+            ],
+            [
+                [9.0f32, 10.0f32, 9.0f32, 10.0f32],
+                [11.0f32, 12.0f32, 11.0f32, 12.0f32],
+            ],
+            [
+                [13.0f32, 14.0f32, 13.0f32, 14.0f32],
+                [15.0f32, 16.0f32, 15.0f32, 16.0f32],
+            ],
         ]);
 
         output.into_data().assert_eq(&expected, false);
@@ -69,19 +81,31 @@ mod tests {
     #[test]
     fn should_support_int_repeat_on_dims_larger_than_1() {
         let data = TensorData::from([
-            [[1, 2], [3, 4]],
-            [[5, 6], [7, 8]],
-            [[9, 10], [11, 12]],
-            [[13, 14], [15, 16]],
+            [[1i32, 2i32], [3i32, 4i32]],
+            [[5i32, 6i32], [7i32, 8i32]],
+            [[9i32, 10i32], [11i32, 12i32]],
+            [[13i32, 14i32], [15i32, 16i32]],
         ]);
         let tensor = Tensor::<TestBackend, 3, Int>::from_data(data, &Default::default());
 
         let output = tensor.repeat_dim(2, 3);
         let expected = TensorData::from([
-            [[1, 2, 1, 2, 1, 2], [3, 4, 3, 4, 3, 4]],
-            [[5, 6, 5, 6, 5, 6], [7, 8, 7, 8, 7, 8]],
-            [[9, 10, 9, 10, 9, 10], [11, 12, 11, 12, 11, 12]],
-            [[13, 14, 13, 14, 13, 14], [15, 16, 15, 16, 15, 16]],
+            [
+                [1i32, 2i32, 1i32, 2i32, 1i32, 2i32],
+                [3i32, 4i32, 3i32, 4i32, 3i32, 4i32],
+            ],
+            [
+                [5i32, 6i32, 5i32, 6i32, 5i32, 6i32],
+                [7i32, 8i32, 7i32, 8i32, 7i32, 8i32],
+            ],
+            [
+                [9i32, 10i32, 9i32, 10i32, 9i32, 10i32],
+                [11i32, 12i32, 11i32, 12i32, 11i32, 12i32],
+            ],
+            [
+                [13i32, 14i32, 13i32, 14i32, 13i32, 14i32],
+                [15i32, 16i32, 15i32, 16i32, 15i32, 16i32],
+            ],
         ]);
 
         output.into_data().assert_eq(&expected, false);
