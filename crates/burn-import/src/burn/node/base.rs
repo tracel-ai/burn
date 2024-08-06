c28f578f1dcaf9e550396c7c43d7b487fece2b51
mepatrick73 <pameu17@ulaval.ca>
2024-08-06 14:24:14 -0400

    working implementation and test

--- crates/burn-import/src/burn/node/base.rs
+++ crates/burn-import/src/burn/node/base.rs
@@ -11,7 +11,7 @@ use super::{
     max_pool1d::MaxPool1dNode, max_pool2d::MaxPool2dNode, pad::PadNode, prelu::PReluNode,
     random_normal::RandomNormalNode, random_uniform::RandomUniformNode, range::RangeNode,
     reshape::ReshapeNode, resize::ResizeNode, slice::SliceNode, squeeze::SqueezeNode, sum::SumNode,
-    unary::UnaryNode, unsqueeze::UnsqueezeNode,
+    tile::TileNode, unary::UnaryNode, unsqueeze::UnsqueezeNode,
 };
 use crate::burn::{BurnImports, Scope, Type};
 use burn::backend::NdArray;
