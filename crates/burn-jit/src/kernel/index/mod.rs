a4bde762a6747634b8a845494a8df015b978d9dd
mepatrick73 <pameu17@ulaval.ca>
2024-08-14 20:36:34 -0400

    Reapply "testsetsetser"
    This reverts commit f8ada0044e6d7eb7163ec017ff75fd29d73629ac.

--- crates/burn-jit/src/kernel/index/mod.rs
+++ crates/burn-jit/src/kernel/index/mod.rs
@@ -9,7 +9,7 @@ mod slice_assign;
 
 pub use flip::*;
 pub use repeat_dim::*;
-pub use select::*;
+pub(crate) use select::*;
 pub(crate) use select_assign::*;
 pub use slice::*;
 pub use slice_assign::*;
