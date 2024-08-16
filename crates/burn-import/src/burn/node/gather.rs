1c92a3aa6078aa3e8d6b685ca3ecca99f1ac4bd0
alteredoxide <me@alteredoxide.com>
2024-08-16 13:21:50 -0700

    implemented muli-dim index for GatherNode
    The `NodeCodegen` impl for `GatherNode` now performs gather in complete
    accordance with the ONNX Gather spec.
    - a `gather` function was added to the gather.rs file
    - `gather()` is now called within the codegen instead of `tensor.select()`
    - a test with two test cases have been added
        - test axes 0 and 1
        - both use 2D index tensors

--- crates/burn-import/src/burn/node/gather.rs
+++ crates/burn-import/src/burn/node/gather.rs
@@ -1,7 +1,7 @@
 use super::{Node, NodeCodegen};
 use crate::burn::{TensorType, ToTokens, Type};
 
-use burn::record::PrecisionSettings;
+use burn::{record::PrecisionSettings, tensor::{backend::Backend, Int, Tensor}};
 use quote::quote;
 
 #[derive(Debug, Clone, new)]
@@ -37,13 +37,15 @@ impl<PS: PrecisionSettings> NodeCodegen<PS> for GatherNode {
                 // then squeeze the dimension to reduce the rank
                 let index = &idx_scalar.name;
                 quote! {
-                    let #output = #input.select(#dim, Tensor::from_data([#index], &*self.device)).squeeze(#dim);
+                    //let #output = #input.select(#dim, Tensor::from_data([#index], &*self.device)).squeeze(#dim);
+                    let #output = gather(#dim, #input, Tensor::from_data([#index], &*self.device)).squeeze(#dim);;
                 }
             }
             Type::Tensor(idx_tensor) => {
                 let index = scope.tensor_use_owned(idx_tensor, node_position);
                 quote! {
-                    let #output = #input.select(#dim, #index);
+                    //let #output = #input.select(#dim, #index);
+                    let #output = gather(#dim, #input, #index);
                 }
             }
             _ => panic!("Gather needs Scalar or Tensor index!"),
@@ -55,10 +57,35 @@ impl<PS: PrecisionSettings> NodeCodegen<PS> for GatherNode {
     }
 }
 
+
+fn gather<B, const D: usize, const X: usize, const O: usize>(
+    axis: usize,
+    input: Tensor<B, D>,
+    index: Tensor<B, X, Int>
+) -> Tensor<B, O>
+where
+    B: Backend,
+{
+    let n_dims_index = index.dims().len();
+    let mut out = vec![];
+    let index_flat = index.flatten::<2>(0, n_dims_index - 2);
+    for idxs in index_flat.iter_dim(0) {
+        let idxs = idxs.squeeze::<1>(0);
+        let subtensor = input.clone().select(axis, idxs);
+        out.push(subtensor);
+    }
+    Tensor::stack(out, axis)
+}
+
+
 #[cfg(test)]
 mod tests {
 
-    use burn::record::FullPrecisionSettings;
+    use burn::{
+        backend::ndarray::{NdArray, NdArrayDevice},
+        record::FullPrecisionSettings,
+        tensor::{Int, Tensor},
+    };
 
     use super::*;
     use crate::burn::{
@@ -111,7 +138,8 @@ mod tests {
                     tensor1: Tensor<B, 2>,
                     tensor2: Tensor<B, 1, Int>
                 ) -> Tensor<B, 2> {
-                    let tensor3 = tensor1.select(0, tensor2);
+                    //let tensor3 = tensor1.select(0, tensor2);
+                    let tensor3 = gather(0, tensor1, tensor2);
 
                     tensor3
                 }
@@ -164,7 +192,8 @@ mod tests {
                     tensor1: Tensor<B, 2>,
                     scalar1: i64
                 ) -> Tensor<B, 2> {
-                    let tensor2 = tensor1.select(0, Tensor::from_data([scalar1], &*self.device)).squeeze(0);
+                    //let tensor2 = tensor1.select(0, Tensor::from_data([scalar1], &*self.device)).squeeze(0);
+                    let tensor2 = gather(0, tensor1, Tensor::from_data([scalar1], &*self.device)).squeeze(0);
 
                     tensor2
                 }
@@ -172,5 +201,44 @@ mod tests {
         };
 
         assert_tokens(graph.codegen(), expected);
+    }
+
+    #[test]
+    fn gather() {
+        let device = NdArrayDevice::default();
+
+        let test_cases = vec![
+            (
+                0,
+                Tensor::<NdArray, 2>::from_data([[1.0, 1.2], [2.3, 3.4], [4.5, 5.7]], &device),
+                Tensor::<NdArray, 2, Int>::from_data([[0, 1], [1, 2]], &device),
+                Tensor::<NdArray, 3>::from_data(
+                    [
+                     [[1.0, 1.2],
+                      [2.3, 3.4]],
+                     [[2.3, 3.4],
+                      [4.5, 5.7]]
+                    ],
+                    &device
+                )
+            ),
+            (
+                1,
+                Tensor::<NdArray, 2>::from_data([[1.0, 1.2, 1.9], [2.3, 3.4, 3.9], [4.5, 5.7, 5.9]], &device),
+                Tensor::<NdArray, 2, Int>::from_data([[0, 2]], &device),
+                Tensor::<NdArray, 3>::from_data(
+                    [
+                     [[1.0, 1.9]],
+                     [[2.3, 3.9]],
+                     [[4.5, 5.9]]
+                    ],
+                    &device
+                )
+            )
+        ];
+        for (axis, input, index, expected) in test_cases {
+            let out = super::gather(axis, input, index);
+            assert!(out.all_close(expected, None, None));
+        }
     }
 }
