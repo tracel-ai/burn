0292967000635d9a5cbfca055dfdef3bb36692da
AlteredOxide <143226375+alteredoxide@users.noreply.github.com>
2024-08-28 04:51:19 -0700

    Feature/codegen gather indices greater than rank 1 (#2199)
    * implemented muli-dim index for GatherNode
    
    The `NodeCodegen` impl for `GatherNode` now performs gather in complete
    accordance with the ONNX Gather spec.
    - a `gather` function was added to the gather.rs file
    - `gather()` is now called within the codegen instead of `tensor.select()`
    - a test with two test cases have been added
        - test axes 0 and 1
        - both use 2D index tensors
    
    * add gather_onnx to numeric api
    
    Added int and float implementations of gather to the burn-tensor numeric
    api:
    - named the methods `gather_onnx` to not be confused with the current
      `gather`
    - these implementations follow the `Gather` ONNX spec
    
    Updated the gather*.py variants and their onnx outputs
    
    * modified files didn't end up in last commit
    
    * tests passing for onnx gather
    
    The implementation of gather for the ONNX `Gather` spec is tentatively
    complete:
    - py test models are updated
    - onnx_tests are modified and passing: `gather`, `gather_scalar`, and
      `gather_shape`
    - node/gather tests are passing
    
    NOTE: The two additional tests in crates/burn-import/src/burn/node/gather.rs that test
    the actual functionality of gather are likely to be deleted, since they
    are redundant to the tests in
    crates/burn-import/onnx-tests/tests/onnx_tests.rs.
    
    * inlined onnx gather within codegen
    
    * rm gather_onnx from public api; rm unnecessary tests
    
    * add comments to gather py models
    
    * some codegen changes; formatting to appease run-checks
    
    - Some necessary changes and improvements to the codegen inlined code
      after translating from public api (removed in previous commit).
    - Changed some formatting that run-checks complained about.
    
    * simplify gather codegen; include 1d and 2d onnx tests
    
    Modified the `Gather` codegen per requested changes:
    - combined match statements on index
    - remove use of `alloc::vec::Vec`
    - use map -> collect instead of procedural
    - include a 1d index gather onnx test
    - remove superflous tests
    
    * delete unused gather.onnx

--- crates/burn-import/onnx-tests/tests/onnx_tests.rs
+++ crates/burn-import/onnx-tests/tests/onnx_tests.rs
@@ -41,7 +41,8 @@ include_models!(
     exp,
     expand,
     flatten,
-    gather,
+    gather_1d_idx,
+    gather_2d_idx,
     gather_scalar,
     gather_shape,
     gather_elements,
@@ -451,15 +452,29 @@ mod tests {
     }
 
     #[test]
-    fn gather() {
-        let model: gather::Model<Backend> = gather::Model::default();
+    fn gather_1d_idx() {
+        let model: gather_1d_idx::Model<Backend> = gather_1d_idx::Model::default();
 
         let device = Default::default();
 
         let input = Tensor::<Backend, 2>::from_floats([[1., 2., 3.], [4., 5., 6.]], &device);
         let index = Tensor::<Backend, 1, Int>::from_ints([0, 2], &device);
-        let output = model.forward(input, index);
         let expected = TensorData::from([[1f32, 3.], [4., 6.]]);
+        let output = model.forward(input, index);
+
+        assert_eq!(output.to_data(), expected);
+    }
+
+    #[test]
+    fn gather_2d_idx() {
+        let model: gather_2d_idx::Model<Backend> = gather_2d_idx::Model::default();
+
+        let device = Default::default();
+
+        let input = Tensor::<Backend, 2>::from_data([[1.0, 1.2], [2.3, 3.4], [4.5, 5.7]], &device);
+        let index = Tensor::<Backend, 2, Int>::from_data([[0, 1], [1, 2]], &device);
+        let expected = TensorData::from([[[1f32, 1.2], [2.3, 3.4]], [[2.3, 3.4], [4.5, 5.7]]]);
+        let output = model.forward(input, index);
 
         assert_eq!(output.to_data(), expected);
     }
