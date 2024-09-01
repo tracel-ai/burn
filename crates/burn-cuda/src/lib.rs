bce58767664ce381c933d37ea5fae069e19baf6f
nathaniel <nathaniel.simard.42@gmail.com>
2024-09-01 16:30:17 -0400

    Set the rev

--- crates/burn-cuda/src/lib.rs
+++ crates/burn-cuda/src/lib.rs
@@ -3,13 +3,12 @@ extern crate alloc;
 use burn_jit::JitBackend;
 pub use cubecl::cuda::CudaDevice;
 use cubecl::cuda::CudaRuntime;
-use half::f16;
 
 #[cfg(not(feature = "fusion"))]
-pub type Cuda<F = f16, I = i32> = JitBackend<CudaRuntime, F, I>;
+pub type Cuda<F = f32, I = i32> = JitBackend<CudaRuntime, F, I>;
 
 #[cfg(feature = "fusion")]
-pub type Cuda<F = f16, I = i32> = burn_fusion::Fusion<JitBackend<CudaRuntime, F, I>>;
+pub type Cuda<F = f32, I = i32> = burn_fusion::Fusion<JitBackend<CudaRuntime, F, I>>;
 
 #[cfg(test)]
 mod tests {
