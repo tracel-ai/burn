7b4cf3a8e7da164f5bb65c45ef8ee60f188269bd
Louis <836250617@qq.com>
2024-09-09 20:23:00 +0800

    fix a mistake

--- examples/tch-ci/dist/train_macOS.sh
+++ examples/tch-ci/dist/train_macOS.sh
@@ -1,5 +1,9 @@
 #! /bin/bash
 
+source pytorch/bin/activate
+
 export LIBTORCH_USE_PYTORCH=1 \
     DYLD_LIBRARY_PATH="$(find pytorch -type d -name "lib" | grep /torch):$DYLD_LIBRARY_PATH" && \
     ./train
+
+deactivate
