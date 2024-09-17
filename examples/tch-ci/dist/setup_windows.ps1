ce08d01f000dd2edbe397b6d925efc0f7fffcb22
Louis <836250617@qq.com>
2024-09-17 15:50:19 +0800

    fix ci example windows

--- examples/tch-ci/dist/setup_windows.ps1
+++ examples/tch-ci/dist/setup_windows.ps1
@@ -1,3 +1,3 @@
-curl -L -o libtorch.zip https://download.pytorch.org/libtorch/cu121/libtorch-win-shared-with-deps-2.2.0%2Bcu121.zip
-unzip libtorch.zip
-rm libtorch.zip
+Invoke-WebRequest -Uri https://download.pytorch.org/libtorch/cu121/libtorch-win-shared-with-deps-2.2.0%2Bcu121.zip -OutFile "libtorch.zip"
+Expand-Archive -Path "libtorch.zip" -DestinationPath "."
+Remove-Item -Path "libtorch.zip"
--- examples/tch-ci/scripts/setup_windows.ps1
+++ examples/tch-ci/scripts/setup_windows.ps1
@@ -1,6 +1,6 @@
-curl -L -o libtorch.zip https://download.pytorch.org/libtorch/cu121/libtorch-win-shared-with-deps-2.2.0%2Bcu121.zip
-unzip libtorch.zip
-rm libtorch.zip
+Invoke-WebRequest -Uri https://download.pytorch.org/libtorch/cu121/libtorch-win-shared-with-deps-2.2.0%2Bcu121.zip -OutFile "libtorch.zip"
+Expand-Archive -Path "libtorch.zip" -DestinationPath "."
+Remove-Item -Path "libtorch.zip"
 
 $directory = ".cargo"
 if (-Not (Test-Path -Path $directory)) {
