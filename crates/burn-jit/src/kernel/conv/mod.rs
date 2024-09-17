4159fe54de6093f644664f3349e73bccecea8352
Genna Wingert <wingertge@gmail.com>
2024-09-17 17:19:46 +0200

    Remove leftover changes

--- crates/burn-jit/src/kernel/conv/mod.rs
+++ crates/burn-jit/src/kernel/conv/mod.rs
@@ -2,6 +2,8 @@ mod conv2d;
 mod conv3d;
 mod conv_transpose3d;
 
-pub use conv2d::*;
+pub(crate) use conv2d::*;
 pub(crate) use conv3d::*;
 pub(crate) use conv_transpose3d::*;
+
+pub use conv2d::{conv2d, conv_transpose2d, Conv2dStrategy, ConvTranspose2dStrategy};
