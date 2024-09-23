0dc4c3d093aa8c37fa5ef929b0a1bdea7d40763e
Genna Wingert <wingertge@gmail.com>
2024-09-23 10:40:50 +0200

    Resolve invalid merge

--- crates/burn-jit/src/fusion/tracing/builder.rs
+++ crates/burn-jit/src/fusion/tracing/builder.rs
@@ -519,11 +519,6 @@ impl TraceBuilder {
                     Operator::AtomicCompareAndSwap(_op) => {
                         // Nothing to do.
                     }
-                    Operator::Magnitude(op) => mark_unary(
-                        op,
-                        &mut local_tensor_ids_input,
-                        &mut local_tensor_ids_output,
-                    ),
                 },
                 Operation::Procedure(proc) => {
                     match proc {
