17dce603a7d61771229d55206212a99a39229448
Femi Oho <47154698+oojo12@users.noreply.github.com>
2024-09-25 16:32:29 -0400

    fix: only run on macos

--- crates/burn-jit/src/tests/unary.rs
+++ crates/burn-jit/src/tests/unary.rs
@@ -4,6 +4,7 @@ mod tests {
     use burn_tensor::{Distribution, Tensor};
 
     #[test]
+    #[cfg(target_os = "macos")]
     fn tanh_should_not_have_numerical_bugs_on_macos() {
         fn tanh_one_value(input: f32) -> f32 {
             let tensor = Tensor::<TestBackend, 1>::ones([1], &Default::default()) * input;
