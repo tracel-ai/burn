a341a77421f43118661aa9d16038f911041f5a83
Arthur <arthur.brussee@gmail.com>
2024-09-27 00:04:33 +0100

    Backwards compatibilty deserialization

--- crates/burn-core/src/record/primitive.rs
+++ crates/burn-core/src/record/primitive.rs
@@ -163,8 +163,45 @@ where
 /// (De)serialize parameters into a clean format.
 #[derive(new, Debug, Clone, Serialize, Deserialize)]
 pub struct ParamSerde<T> {
+    #[serde(deserialize_with = "compat_param_id")]
     id: u64,
     param: T,
+}
+
+use data_encoding::BASE32_DNSSEC;
+
+// Deserialize a param id from either a u64 or from a BASE32_DNSSEC encoded string (old format).
+fn compat_param_id<'de, D>(deserializer: D) -> Result<u64, D::Error>
+where
+    D: serde::Deserializer<'de>,
+{
+    struct ParamIdVisitor;
+
+    impl<'de> Visitor<'de> for ParamIdVisitor {
+        type Value = u64;
+
+        fn expecting(&self, formatter: &mut fmt::Formatter) -> fmt::Result {
+            formatter.write_str("a u64 or a base32 string")
+        }
+
+        fn visit_u64<E: serde::de::Error>(self, value: u64) -> Result<Self::Value, E> {
+            Ok(value)
+        }
+
+        fn visit_str<E: serde::de::Error>(self, value: &str) -> Result<Self::Value, E> {
+            // Old format encoded only 6 bytes.
+            BASE32_DNSSEC
+                .decode(value.as_bytes())
+                .map_err(serde::de::Error::custom)
+                .map(|bytes| {
+                    u64::from_le_bytes([
+                        bytes[0], bytes[1], bytes[2], bytes[3], bytes[4], bytes[5], 0, 0,
+                    ])
+                })
+        }
+    }
+
+    deserializer.deserialize_any(ParamIdVisitor)
 }
 
 impl<B, const D: usize> Record<B> for Param<Tensor<B, D>>
