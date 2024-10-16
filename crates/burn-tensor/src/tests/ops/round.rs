4074e87a862c3d66d9f4c42f9a9ecfadc382a9dc
med <medioqrity@gmail.com>
2024-10-16 08:07:09 -0700

    Add tests for halfway cases

--- crates/burn-tensor/src/tests/ops/round.rs
+++ crates/burn-tensor/src/tests/ops/round.rs
@@ -12,5 +12,13 @@ mod tests {
         let expected = TensorData::from([[24., 88., 76.], [60., 44., 95.]]);
 
         output.into_data().assert_approx_eq(&expected, 3);
+
+        let data = TensorData::from([1.5, 2.5, 3.5, 4.5, 5.5, 6.5]);
+        let tensor = Tensor::<TestBackend, 1>::from_data(data, &Default::default());
+
+        let output = tensor.round();
+        let expected = TensorData::from([2., 2., 4., 4., 6., 6.]);
+
+        output.into_data().assert_approx_eq(&expected, 3);
     }
 }
