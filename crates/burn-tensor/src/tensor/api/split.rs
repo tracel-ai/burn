5a14745c005509aee1ae55507bc920577b27c861
Mathias <mathiaswi177@gmail.com>
2024-11-14 01:54:37 -0800

    Add fallbacks for split and split_with_size since a few backends dont support them

---
+++ crates/burn-tensor/src/tensor/api/split.rs
@@ -0,0 +1,41 @@
+use super::narrow::narrow;
+use crate::{backend::Backend, BasicOps, TensorKind};
+use alloc::vec::Vec;
+
+pub fn split<B: Backend, K: TensorKind<B> + BasicOps<B>>(
+    tensor: K::Primitive,
+    split_size: usize,
+    dim: usize,
+) -> Vec<K::Primitive> {
+    let size = K::shape(&tensor).dims[dim];
+    let mut tensors = Vec::new();
+
+    let mut start = 0;
+    while start < size {
+        let length = usize::min(split_size, size - start);
+        tensors.push(narrow::<B, K>(tensor.clone(), dim, start, length));
+        start += length;
+    }
+
+    tensors
+}
+
+pub fn split_with_size<B: Backend, K: TensorKind<B> + BasicOps<B>>(
+    tensor: K::Primitive,
+    split_sizes: &[usize],
+    dim: usize,
+) -> Vec<K::Primitive> {
+    let size = K::shape(&tensor).dims[dim];
+
+    let mut tensors = Vec::new();
+    let mut start = 0;
+    for &length in split_sizes {
+        if length == 0 {
+            continue;
+        }
+        tensors.push(narrow::<B, K>(tensor.clone(), dim, start, length));
+        start += length;
+    }
+
+    tensors
+}
