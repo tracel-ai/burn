334fa0b89c2f93961643bb97213cf27d0c18cd72
Mathias <mathiaswi177@gmail.com>
2024-11-14 11:20:13 -0800

    Add documentation to split functions

--- crates/burn-tensor/src/tensor/api/split.rs
+++ crates/burn-tensor/src/tensor/api/split.rs
@@ -2,6 +2,26 @@ use super::narrow::narrow;
 use crate::{backend::Backend, BasicOps, TensorKind};
 use alloc::vec::Vec;
 
+/// Splits the tensor along the given dimension into equally sized chunks (is possible)
+/// with size `split_size`. Last chunk will be smaller if the tensor size along the given
+/// dimension `dim` is not divisible by `split_size`.
+///
+/// # Arguments
+///
+/// * `tensor` - The tensor.
+/// * `split_size` - The size of a single chunk.
+/// * `dim` - The dimension along which to split the tensor.
+///
+/// # Returns
+///
+/// A vector of tensors
+///
+/// # Remarks
+///
+/// This (and the following) are fallback solutions that is used only when the backend doesn't have the corresponding implementation.
+/// Ideally, it is supposed to be implemented by the backend and the backend implementation will be resolved
+/// by static dispatch. It is not designed for direct usage by users, and not recommended to import
+/// or use this function directly.
 pub fn split<B: Backend, K: TensorKind<B> + BasicOps<B>>(
     tensor: K::Primitive,
     split_size: usize,
@@ -20,6 +40,22 @@ pub fn split<B: Backend, K: TensorKind<B> + BasicOps<B>>(
     tensors
 }
 
+/// Splits the tensor along the given dimension into chunks with sizes in
+/// `dim` according to `split_sizes`.
+///
+/// # Arguments
+///
+/// * `tensor` - The tensor.
+/// * `split_sizes` - Vector of sizes for each chunk.
+/// * `dim` - The dimension along which to split the tensor.
+///
+/// # Returns
+///
+/// A vector of tensors.
+///
+/// # Remarks
+///
+/// Fallback solution for backends with no equivalent functionality.
 pub fn split_with_size<B: Backend, K: TensorKind<B> + BasicOps<B>>(
     tensor: K::Primitive,
     split_sizes: Vec<usize>,
