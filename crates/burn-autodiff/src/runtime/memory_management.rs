1cd956e570e87951995066654a561a65be6eb219
Jun Namikawa <jnamika@gmail.com>
2024-11-14 22:31:58 +0900

    verifying parent nodes' existence (#2488)
    * verifying parent nodes' existence
    
    * delete unnecessary code
    
    ---------
    
    Co-authored-by: Jun Namikawa <jun.namikawa@linecorp.com>

--- crates/burn-autodiff/src/runtime/memory_management.rs
+++ crates/burn-autodiff/src/runtime/memory_management.rs
@@ -84,7 +84,10 @@ impl GraphMemoryManagement {
         for (id, parents) in self.nodes.iter() {
             let is_useful = matches!(self.statuses.get(id), Some(NodeMemoryStatus::Useful));
 
-            if !is_useful && Arc::strong_count(id) == 1 && parents.is_empty() {
+            // Check if parents are either empty or absent from self.nodes
+            let parents_absent = parents.iter().all(|p| !self.nodes.contains_key(p));
+
+            if !is_useful && Arc::strong_count(id) == 1 && parents_absent {
                 to_delete.push(*id.as_ref())
             }
         }
