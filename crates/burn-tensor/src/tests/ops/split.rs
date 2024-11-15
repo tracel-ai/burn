d1624f0ff5220ae4ecda5ce002d86feca6dc4a8e
Mathias <mathiaswi177@gmail.com>
2024-11-14 21:56:55 -0800

    Get initial tests to a working state

--- crates/burn-tensor/src/tests/ops/split.rs
+++ crates/burn-tensor/src/tests/ops/split.rs
@@ -6,8 +6,40 @@ mod tests {
 
     #[test]
     fn test_split_evenly_divisible() {
-        let tensors: Vec<Tensor<TestBackend, 1, Int>> =
-            Tensor::arange(0..12, &Default::default()).split(2, 0);
-        assert_eq!(tensors.len(), 3);
+        let device = Default::default();
+        let tensors =
+            TestTensor::<2>::from_data([[0, 1], [2, 3], [4, 5], [6, 7], [8, 9], [10, 11]], &device);
+
+        let split_tensors = tensors.split(2, 0);
+        assert_eq!(split_tensors.len(), 3);
+
+        let expected = vec![
+            TensorData::from([[0, 1], [2, 3]]),
+            TensorData::from([[4, 5], [6, 7]]),
+            TensorData::from([[8, 9], [10, 11]]),
+        ];
+
+        for (index, tensor) in split_tensors.iter().enumerate() {
+            tensor.to_data().assert_eq(&expected[index], false);
+        }
+    }
+
+    #[test]
+    fn test_split_not_evenly_divisible() {
+        let device = Default::default();
+        let tensors = TestTensor::<2>::from_data([[0, 1], [2, 3], [4, 5], [6, 7], [8, 9]], &device);
+
+        let split_tensors = tensors.split(2, 0);
+        assert_eq!(split_tensors.len(), 3);
+
+        let expected = vec![
+            TensorData::from([[0, 1], [2, 3]]),
+            TensorData::from([[4, 5], [6, 7]]),
+            TensorData::from([[8, 9]]),
+        ];
+
+        for (index, tensor) in split_tensors.iter().enumerate() {
+            tensor.to_data().assert_eq(&expected[index], false);
+        }
     }
 }
