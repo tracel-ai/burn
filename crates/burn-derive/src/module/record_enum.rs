1fb442efc183946eaa5046f0c52ab84383bbd890
Guillaume Lagrange <lagrange.guillaume.1@gmail.com>
2024-12-11 09:41:49 -0500

    Module derive types should inherit visibility

--- crates/burn-derive/src/module/record_enum.rs
+++ crates/burn-derive/src/module/record_enum.rs
@@ -1,18 +1,20 @@
 use crate::shared::enum_variant::EnumVariant;
 use proc_macro2::{Ident, TokenStream};
 use quote::quote;
-use syn::Generics;
+use syn::{Generics, Visibility};
 
 use super::record::ModuleRecordCodegen;
 
 #[derive(new)]
 pub(crate) struct EnumModuleRecordCodegen {
     variants: Vec<EnumVariant>,
+    vis: Visibility,
 }
 
 impl ModuleRecordCodegen for EnumModuleRecordCodegen {
     fn gen_record_type(&self, record_name: &Ident, generics: &Generics) -> TokenStream {
         let mut variants = quote! {};
+        let vis = &self.vis;
 
         // Capture the Record enum variant types
         for variant in self.variants.iter() {
@@ -31,7 +33,7 @@ impl ModuleRecordCodegen for EnumModuleRecordCodegen {
 
             /// The record type for the module.
             #[derive(burn::record::Record)]
-            pub enum #record_name #generics #generics_where {
+            #vis enum #record_name #generics #generics_where {
                 #variants
             }
         }
