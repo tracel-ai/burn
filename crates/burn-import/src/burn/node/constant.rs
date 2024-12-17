9c5d329d819fff2dc9533f0acda7692840086b17
James Hiew <james@hiew.net>
2024-12-17 13:03:44 +0000

    cargo clippy --fix

--- crates/burn-import/src/burn/node/constant.rs
+++ crates/burn-import/src/burn/node/constant.rs
@@ -247,7 +247,7 @@ mod tests {
     #[test]
     fn test_codegen_constant_scalar_float64() {
         assert_codegen_constant_scalar(
-            ConstantValue::Float64(3.14159265358979323846264338327950288f64),
+            ConstantValue::Float64(3.141_592_653_589_793_f64),
             ScalarKind::Float64,
         );
     }
