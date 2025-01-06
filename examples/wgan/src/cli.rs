09dd8ecfdf7311e2e4943650a9c9f655c2eb7f7b
jiawen wang <wangjiawen2013@163.com>
2025-01-06 12:49:33 +0800

    Update cli.rs

--- examples/wgan/src/cli.rs
+++ examples/wgan/src/cli.rs
@@ -17,47 +17,47 @@ pub enum Commands {
         artifact_dir: String,
 
         /// Number of epochs of training
-        #[arg(long, default_value="200")]
+        #[arg(long, default_value = "200")]
         num_epochs: usize,
 
         /// Number of training steps for discriminator before generator is trained per iteration
-        #[arg(long, default_value="5")]
+        #[arg(long, default_value = "5")]
         num_critic: usize,
 
         /// Size of the batches
-        #[arg(long, default_value="256")]
+        #[arg(long, default_value = "256")]
         batch_size: usize,
 
         /// Number of cpu threads to use during batch generation
-        #[arg(long, default_value="8")]
+        #[arg(long, default_value = "8")]
         num_workers: usize,
 
         /// Random seed
-        #[arg(long, default_value="5")]
+        #[arg(long, default_value = "5")]
         seed: u64,
 
         /// Learning rate
-        #[arg(long, default_value="0.00005")]
+        #[arg(long, default_value = "0.00005")]
         lr: f64,
 
         /// Dimensionality of the latent space
-        #[arg(long, default_value="100")]
+        #[arg(long, default_value = "100")]
         latent_dim: usize,
 
         /// Size of each image dimension
-        #[arg(long, default_value="28")]
+        #[arg(long, default_value = "28")]
         image_size: usize,
 
         /// Number of image channels
-        #[arg(long, default_value="1")]
+        #[arg(long, default_value = "1")]
         channels: usize,
 
         /// Lower and upper clip value for disc. weights
-        #[arg(long, default_value="0.01")]
+        #[arg(long, default_value = "0.01")]
         clip_value: f32,
 
         /// Save images every sample_interval batches
-        #[arg(long, default_value="400")]
+        #[arg(long, default_value = "400")]
         sample_interval: usize,
     },
 
