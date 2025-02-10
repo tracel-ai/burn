834dd994b76bcf4f0860355dce5ce5e385f97e1b
Genna Wingert <1058083+wingertge@users.noreply.github.com>
2025-02-10 17:11:58 +0100

    Remove autodiff implementation

--- crates/burn-vision/src/backends/cpu/ops.rs
+++ crates/burn-vision/src/backends/cpu/ops.rs
@@ -1,7 +1,5 @@
 use crate::VisionOps;
 
-#[cfg(feature = "autodiff")]
-use burn_autodiff::{checkpoint::strategy::CheckpointStrategy, Autodiff};
 #[cfg(feature = "candle")]
 use burn_candle::{Candle, FloatCandleElement, IntCandleElement};
 #[cfg(feature = "ndarray")]
@@ -19,34 +17,3 @@ impl<E: FloatNdArrayElement, I: IntNdArrayElement, Q: QuantElement> VisionOps<Se
 impl<F: FloatCandleElement, I: IntCandleElement> VisionOps<Self> for Candle<F, I> {}
 #[cfg(feature = "tch")]
 impl<E: TchElement, Q: burn_tch::QuantElement> VisionOps<Self> for LibTorch<E, Q> {}
-#[cfg(feature = "autodiff")]
-impl<B: burn_tensor::backend::Backend + VisionOps<B>, C: CheckpointStrategy> VisionOps<Self>
-    for Autodiff<B, C>
-{
-    fn connected_components(
-        img: burn_tensor::ops::BoolTensor<Self>,
-        connectivity: crate::Connectivity,
-    ) -> burn_tensor::ops::IntTensor<Self> {
-        B::connected_components(img, connectivity)
-    }
-
-    fn connected_components_with_stats(
-        img: burn_tensor::ops::BoolTensor<Self>,
-        connectivity: crate::Connectivity,
-        opts: crate::ConnectedStatsOptions,
-    ) -> (
-        burn_tensor::ops::IntTensor<Self>,
-        crate::ConnectedStatsPrimitive<Self>,
-    ) {
-        let (labels, stats) = B::connected_components_with_stats(img, connectivity, opts);
-        let stats = crate::ConnectedStatsPrimitive::<Self> {
-            area: stats.area,
-            left: stats.left,
-            top: stats.top,
-            right: stats.right,
-            bottom: stats.bottom,
-            max_label: stats.max_label,
-        };
-        (labels, stats)
-    }
-}
