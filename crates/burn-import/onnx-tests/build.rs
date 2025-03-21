e3dd14d244e92d90475963c0d820461f1784da05
Carl Hurd <carl.m.hurd@gmail.com>
2025-03-21 11:20:12 -0400

    Simple test added for multiple output tracking

--- crates/burn-import/onnx-tests/build.rs
+++ crates/burn-import/onnx-tests/build.rs
@@ -47,6 +47,7 @@ fn main() {
         .input("tests/gemm/gemm_non_unit_alpha_beta.onnx")
         .input("tests/gemm/gemm_no_c.onnx")
         .input("tests/global_avr_pool/global_avr_pool.onnx")
+        .input("tests/graph_multiple_output_tracking/graph_multiple_output_tracking.onnx")
         .input("tests/greater/greater.onnx")
         .input("tests/greater/greater_scalar.onnx")
         .input("tests/greater_or_equal/greater_or_equal.onnx")
