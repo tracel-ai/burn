163ad16267ad5e80da8e14a8b6de8639a6705ca0
chloe <you@example.com>
2025-03-23 00:07:03 +1100

    check channels_in dimension

--- crates/burn-core/src/nn/conv/conv3d.rs
+++ crates/burn-core/src/nn/conv/conv3d.rs
@@ -146,7 +146,13 @@ impl<B: Backend> Conv3d<B> {
     /// - input: `[batch_size, channels_in, depth_in, height_in, width_in]`
     /// - output: `[batch_size, channels_out, depth_out, height_out, width_out]`
     pub fn forward(&self, input: Tensor<B, 5>) -> Tensor<B, 5> {
-        let [_batch_size, _channels_in, depth_in, height_in, width_in] = input.dims();
+        let [_batch_size, channels_in, depth_in, height_in, width_in] = input.dims();
+        let expected = self.weight.dims()[1] * self.groups;
+        assert_eq!(
+            channels_in, expected,
+            "This conv layer requies a channels_in dimension of {expected}, but got {channels_in}"
+        );
+
         let padding = self.padding.calculate_padding_3d(
             depth_in,
             height_in,
@@ -248,5 +254,15 @@ mod tests {
             alloc::format!("{}", conv),
             "Conv3d {stride: [1, 1, 1], kernel_size: [5, 5, 5], dilation: [1, 1, 1], groups: 1, padding: Valid, params: 626}"
         );
+    }
+
+    #[test]
+    #[should_panic = "This conv layer requies a channels_in dimension of 5, but got 3"]
+    fn input_channels_mismatch() {
+        let config = Conv3dConfig::new([5, 5], [5, 5, 5]);
+        let conv = config.init::<TestBackend>(&Default::default());
+
+        let input = Tensor::<TestBackend, 5>::zeros([1, 3, 10, 10, 10], &Default::default());
+        let _ = conv.forward(input);
     }
 }
