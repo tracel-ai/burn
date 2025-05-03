23e6f39a059ac7161a7d74ecff358e5fd2040add
Dilshod Tadjibaev <939125+antimora@users.noreply.github.com>
2025-05-03 00:35:52 -0500

    Moved the remaining tests from test_onnx.rs

---
+++ crates/burn-import/onnx-tests/tests/split/mod.rs
@@ -0,0 +1,24 @@
+use crate::include_models;
+include_models!(split);
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use burn::tensor::{Shape, Tensor};
+
+    type Backend = burn_ndarray::NdArray<f32>;
+
+    #[test]
+    fn split() {
+        let device = Default::default();
+        let model = split::Model::<Backend>::new(&device);
+        let shape = [5, 2];
+        let input = Tensor::ones(shape, &device);
+
+        let (tensor_1, tensor_2, tensor_3) = model.forward(input);
+
+        assert_eq!(tensor_1.shape(), Shape::from([2, 2]));
+        assert_eq!(tensor_2.shape(), Shape::from([2, 2]));
+        assert_eq!(tensor_3.shape(), Shape::from([1, 2]));
+    }
+}
