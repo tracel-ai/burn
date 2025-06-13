b68d6aea40e3778d5085e07afb42943e322407cd
Crutcher Dunnavant <crutcher@gmail.com>
2025-06-13 12:31:36 -0700

    - Unify around IndexConversion - Move IndexConversion to burn::indexing - Reorder to (dim, shift) - improve error messages.

--- crates/burn-tensor/src/tests/ops/roll.rs
+++ crates/burn-tensor/src/tests/ops/roll.rs
@@ -13,7 +13,7 @@ mod tests {
         // Rolling an empty tensor should return the same empty tensor
         input
             .clone()
-            .roll(&[1, 2], &[0, 1])
+            .roll(&[0, 1], &[1, 2])
             .to_data()
             .assert_eq(&input.to_data(), false);
     }
@@ -25,21 +25,48 @@ mod tests {
         // No-op shift:
         input
             .clone()
-            .roll(&[0, 0], &[0, 1])
+            .roll(&[0, 1], &[0, 0])
             .to_data()
             .assert_eq(&input.clone().to_data(), false);
 
         input
             .clone()
-            .roll(&[1, -1], &[0, 1])
+            .roll(&[0, 1], &[1, -1])
             .to_data()
             .assert_eq(&TensorData::from([[5, 3, 4], [2, 0, 1]]), false);
 
         input
             .clone()
-            .roll(&[2 * 32 + 1, 3 * (-400) - 1], &[0, 1])
+            .roll(&[0, 1], &[2 * 32 + 1, 3 * (-400) - 1])
             .to_data()
             .assert_eq(&TensorData::from([[5, 3, 4], [2, 0, 1]]), false);
+    }
+
+    #[should_panic]
+    #[test]
+    fn test_roll_dim_too_big() {
+        let input = TestTensorInt::<2>::from([[0, 1, 2], [3, 4, 5]]);
+
+        // Attempting to roll on a dimension that doesn't exist should panic
+        let _d = input.roll(&[2], &[1]);
+    }
+
+    #[should_panic]
+    #[test]
+    fn test_roll_dim_too_small() {
+        let input = TestTensorInt::<2>::from([[0, 1, 2], [3, 4, 5]]);
+
+        // Attempting to roll on a dimension that doesn't exist should panic
+        let _d = input.roll(&[-3], &[1]);
+    }
+
+    #[should_panic]
+    #[test]
+    fn test_roll_shift_size_mismatch() {
+        let input = TestTensorInt::<2>::from([[0, 1, 2], [3, 4, 5]]);
+
+        // Attempting to roll with a shift size that doesn't match the number of dimensions should panic
+        let _d = input.roll(&[0], &[1, 2]);
     }
 
     #[test]
@@ -48,14 +75,32 @@ mod tests {
 
         input
             .clone()
-            .roll_dim(1, 0)
+            .roll_dim(0, 1)
             .to_data()
             .assert_eq(&TensorData::from([[3, 4, 5], [0, 1, 2]]), false);
 
         input
             .clone()
-            .roll_dim(-1, 1)
+            .roll_dim(1, -1)
             .to_data()
             .assert_eq(&TensorData::from([[2, 0, 1], [5, 3, 4]]), false);
+    }
+
+    #[should_panic]
+    #[test]
+    fn test_roll_dim_dim_too_big() {
+        let input = TestTensorInt::<2>::from([[0, 1, 2], [3, 4, 5]]);
+
+        // Attempting to roll on a dimension that doesn't exist should panic
+        let _d = input.roll_dim(2, 1);
+    }
+
+    #[should_panic]
+    #[test]
+    fn test_roll_dim_dim_too_small() {
+        let input = TestTensorInt::<2>::from([[0, 1, 2], [3, 4, 5]]);
+
+        // Attempting to roll on a dimension that doesn't exist should panic
+        let _d = input.roll_dim(-3, 1);
     }
 }
