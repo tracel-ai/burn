481f04af96a55c36a1c845fcdac8ee3d55ee2ccc
Crutcher Dunnavant <crutcher@gmail.com>
2025-07-21 11:47:37 -0700

    Add SelectionDataset, refactor ShuffledDataset, and add transform tests.
    SelectionDataset is morally a more abstract replacement for ShuffledDataset; all uses could be migrated.

--- crates/burn-dataset/src/transform/random.rs
+++ crates/burn-dataset/src/transform/random.rs
@@ -10,18 +10,29 @@ pub struct ShuffledDataset<D, I> {
     input: PhantomData<I>,
 }
 
+/// Generates a shuffled vector of indices up to a size.
+///
+/// # Arguments
+///
+/// * `size` - The size of the dataset to shuffle.
+///
+/// # Returns
+///
+/// A vector of shuffled indices.
+#[inline(always)]
+pub fn shuffled_indices(size: usize, rng: &mut StdRng) -> Vec<usize> {
+    let mut indices = (0..size).collect::<Vec<_>>();
+    indices.shuffle(rng);
+    indices
+}
+
 impl<D, I> ShuffledDataset<D, I>
 where
     D: Dataset<I>,
 {
     /// Creates a new shuffled dataset.
     pub fn new(dataset: D, rng: &mut StdRng) -> Self {
-        let mut indices = Vec::with_capacity(dataset.len());
-        for i in 0..dataset.len() {
-            indices.push(i);
-        }
-        indices.shuffle(rng);
-
+        let indices = shuffled_indices(dataset.len(), rng);
         Self {
             dataset,
             indices,
@@ -48,5 +59,34 @@ where
 
     fn len(&self) -> usize {
         self.dataset.len()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::FakeDataset;
+
+    #[test]
+    fn test_shuffled_dataset() {
+        let dataset = FakeDataset::<String>::new(27);
+        let source_items = dataset.iter().collect::<Vec<_>>();
+
+        let seed = 42;
+
+        let shuffled = ShuffledDataset::with_seed(dataset, seed);
+
+        let mut rng = StdRng::seed_from_u64(seed);
+        let indices = shuffled_indices(source_items.len(), &mut rng);
+
+        assert_eq!(&shuffled.indices, &indices);
+        assert_eq!(shuffled.len(), source_items.len());
+
+        let expected_items: Vec<_> = shuffled
+            .indices
+            .iter()
+            .map(|&i| source_items[i].to_string())
+            .collect();
+        assert_eq!(&shuffled.iter().collect::<Vec<_>>(), &expected_items);
     }
 }
---
+++ crates/burn-dataset/src/transform/selection.rs
@@ -0,0 +1,119 @@
+use crate::Dataset;
+use crate::transform::shuffled_indices;
+use rand::SeedableRng;
+use rand::rngs::StdRng;
+use std::marker::PhantomData;
+
+/// A dataset that selects a subset of indices from an existing dataset.
+///
+/// Indices may appear multiple times, but they must be within the bounds of the original dataset.
+pub struct SelectionDataset<D, I> {
+    dataset: D,
+    indices: Vec<usize>,
+    input: PhantomData<I>,
+}
+
+impl<D, I> SelectionDataset<D, I>
+where
+    D: Dataset<I>,
+{
+    /// Creates a new selection dataset with the given dataset and indices.
+    ///
+    /// # Arguments
+    ///
+    /// * `dataset` - The original dataset to select from.
+    /// * `indices` - A slice of indices to select from the dataset.
+    ///   These indices must be within the bounds of the dataset.
+    ///
+    /// # Panics
+    ///
+    /// Panics if any index is out of bounds for the dataset.
+    pub fn new(dataset: D, indices: Vec<usize>) -> Self {
+        let size = dataset.len();
+
+        if let Some(idx) = indices.iter().find(|&i| *i >= size) {
+            panic!("Index out of bounds for wrapped dataset size: {idx} >= {size}");
+        }
+
+        Self {
+            dataset,
+            indices,
+            input: PhantomData,
+        }
+    }
+
+    /// Creates a new selection dataset with shuffled indices.
+    pub fn shuffled(dataset: D, rng: &mut StdRng) -> Self {
+        let indices = shuffled_indices(dataset.len(), rng);
+        Self::new(dataset, indices)
+    }
+
+    /// Creates a new selection dataset with shuffled indices using a fixed seed.
+    pub fn shuffled_with_seed(dataset: D, seed: u64) -> Self {
+        let mut rng = StdRng::seed_from_u64(seed);
+        Self::shuffled(dataset, &mut rng)
+    }
+}
+
+impl<D, I> Dataset<I> for SelectionDataset<D, I>
+where
+    D: Dataset<I>,
+    I: Clone + Send + Sync,
+{
+    fn get(&self, index: usize) -> Option<I> {
+        let index = self.indices.get(index)?;
+        self.dataset.get(*index)
+    }
+
+    fn len(&self) -> usize {
+        self.indices.len()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::FakeDataset;
+
+    #[test]
+    fn test_selection_dataset() {
+        let source_dataset = FakeDataset::<String>::new(27);
+
+        let indices: Vec<usize> = vec![15, 1, 12, 12];
+        let expected: Vec<String> = indices
+            .iter()
+            .map(|i| source_dataset.get(*i).unwrap())
+            .collect();
+
+        let selection = SelectionDataset::new(source_dataset, indices.clone());
+
+        assert_eq!(selection.len(), indices.len());
+
+        let items = selection.iter().collect::<Vec<_>>();
+
+        assert_eq!(items, expected);
+    }
+
+    #[test]
+    fn test_shuffled_dataset() {
+        let dataset = FakeDataset::<String>::new(27);
+        let source_items = dataset.iter().collect::<Vec<_>>();
+
+        let seed = 42;
+
+        let shuffled = SelectionDataset::shuffled_with_seed(dataset, seed);
+
+        let mut rng = StdRng::seed_from_u64(seed);
+        let indices = shuffled_indices(source_items.len(), &mut rng);
+
+        assert_eq!(&shuffled.indices, &indices);
+        assert_eq!(shuffled.len(), source_items.len());
+
+        let expected_items: Vec<_> = shuffled
+            .indices
+            .iter()
+            .map(|&i| source_items[i].to_string())
+            .collect();
+        assert_eq!(&shuffled.iter().collect::<Vec<_>>(), &expected_items);
+    }
+}
