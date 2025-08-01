01edc6a52ec187c66f102bcfa63ade808a1f06a8
Cielbird <guilhem.ane3@gmail.com>
2025-08-01 12:56:15 -0400

    Refactoring burn training strategies

--- crates/burn-train/src/learner/strategies/single_device.rs
+++ crates/burn-train/src/learner/strategies/single_device.rs
@@ -1,97 +1,100 @@
+use std::marker::PhantomData;
+
 use burn_core::{module::Module, prelude::Backend};
 
 use crate::{
-    Learner, LearnerDataLoaders, SingleDeviceTrainEpoch, SingleDeviceValidEpoch, TrainLoader,
+    LearnComponents, LearningMethod, SingleDeviceTrainEpoch, SingleDeviceValidEpoch, TrainLoader,
     ValidLoader, components::LearnerComponents,
 };
 
-pub(crate) fn prepare_dataloaders_single_device<LC>(
-    device: &<LC::Backend as Backend>::Device,
-    dataloader_train: TrainLoader<LC>,
-    dataloader_valid: ValidLoader<LC>,
-) -> LearnerDataLoaders<LC>
-where
-    LC: LearnerComponents,
-{
-    // The reference model is always on the first device provided.
-    let train = dataloader_train.to_device(device);
-    let valid = dataloader_valid.to_device(device);
-
-    LearnerDataLoaders::SingleTrainSingleValid {
-        dataloader_train: train,
-        dataloader_valid: valid,
+pub struct SingleDeviceLearningStrategy<LC: LearnerComponents> {
+    device: <LC::Backend as Backend>::Device,
+    _p: PhantomData<LC>,
+}
+impl<LC: LearnerComponents> SingleDeviceLearningStrategy<LC> {
+    pub fn new(device: <LC::Backend as Backend>::Device) -> Self {
+        Self {
+            device,
+            _p: PhantomData,
+        }
     }
 }
 
-pub(crate) fn prepare_model_single_device<LC: LearnerComponents>(
-    device: &<LC::Backend as Backend>::Device,
-    mut learner: Learner<LC>,
-) -> Learner<LC> {
-    learner.model = learner.model.fork(device);
+impl<LC: LearnerComponents> LearningMethod<LC> for SingleDeviceLearningStrategy<LC> {
+    type PreparedDataloaders = (TrainLoader<LC>, ValidLoader<LC>);
 
-    learner
-}
+    type PreparedModel = LC::Model;
 
-pub(crate) fn learn_single_device<LC>(
-    mut learner: Learner<LC>,
-    dataloaders: LearnerDataLoaders<LC>,
-    starting_epoch: usize,
-) -> Learner<LC>
-where
-    LC: LearnerComponents,
-{
-    let LearnerDataLoaders::SingleTrainSingleValid {
-        dataloader_train,
-        dataloader_valid,
-    } = dataloaders
-    else {
-        panic!("Wrong dataloaders for strategy");
-    };
+    fn prepare_dataloaders(
+        &self,
+        dataloader_train: TrainLoader<LC>,
+        dataloader_valid: ValidLoader<LC>,
+    ) -> Self::PreparedDataloaders {
+        // The reference model is always on the first device provided.
+        let train = dataloader_train.to_device(&self.device);
+        let valid = dataloader_valid.to_device(&self.device);
 
-    let mut epoch_train = SingleDeviceTrainEpoch::new(
-        dataloader_train,
-        starting_epoch,
-        learner.num_epochs,
-        learner.grad_accumulation,
-    );
+        (train, valid)
+    }
 
-    for epoch in starting_epoch..learner.num_epochs + 1 {
-        (learner.model, learner.optim) = epoch_train.run::<LC>(
-            learner.model,
-            learner.optim,
-            &mut learner.lr_scheduler,
-            &mut learner.event_processor,
-            &learner.interrupter,
+    fn prepare_model(&self, model: LC::Model) -> Self::PreparedModel {
+        model.fork(&self.device)
+    }
+
+    fn learn(
+        &self,
+        model: Self::PreparedModel,
+        dataloaders: Self::PreparedDataloaders,
+        starting_epoch: usize,
+        mut components: LearnComponents<LC>,
+    ) -> LC::Model {
+        let (dataloader_train, dataloader_valid) = dataloaders;
+        let mut model: LC::Model = model;
+
+        let mut epoch_train = SingleDeviceTrainEpoch::new(
+            dataloader_train,
+            starting_epoch,
+            components.num_epochs,
+            components.grad_accumulation,
         );
 
-        if learner.interrupter.should_stop() {
-            break;
-        }
-
-        let epoch_valid =
-            SingleDeviceValidEpoch::<LC>::new(dataloader_valid.clone(), epoch, learner.num_epochs);
-        epoch_valid.run(
-            &learner.model,
-            &mut learner.event_processor,
-            &learner.interrupter,
-        );
-
-        if let Some(checkpointer) = &mut learner.checkpointer {
-            checkpointer.checkpoint(
-                &learner.model,
-                &learner.optim,
-                &learner.lr_scheduler,
-                epoch,
-                &learner.event_store,
+        for epoch in starting_epoch..components.num_epochs + 1 {
+            (model, components.optim) = epoch_train.run::<LC>(
+                model,
+                components.optim,
+                &mut components.lr_scheduler,
+                components.event_processor,
+                &components.interrupter,
             );
-        }
 
-        if let Some(early_stopping) = &mut learner.early_stopping {
-            if early_stopping.should_stop(epoch, &learner.event_store) {
+            if components.interrupter.should_stop() {
                 break;
             }
-        }
-    }
 
-    learner
+            let epoch_valid = SingleDeviceValidEpoch::<LC>::new(
+                dataloader_valid.clone(),
+                epoch,
+                components.num_epochs,
+            );
+            epoch_valid.run(&model, components.event_processor, &components.interrupter);
+
+            if let Some(checkpointer) = &mut components.checkpointer {
+                checkpointer.checkpoint(
+                    &model,
+                    &components.optim,
+                    &components.lr_scheduler,
+                    epoch,
+                    &components.event_store,
+                );
+            }
+
+            if let Some(early_stopping) = &mut components.early_stopping {
+                if early_stopping.should_stop(epoch, &components.event_store) {
+                    break;
+                }
+            }
+        }
+
+        model
+    }
 }
