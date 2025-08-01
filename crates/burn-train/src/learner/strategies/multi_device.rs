ff9825ea713dd928bbc5622eb79a7f627091b3ac
Cielbird <guilhem.ane3@gmail.com>
2025-08-01 11:12:35 -0400

    Refactoring of generics in learner

--- crates/burn-train/src/learner/strategies/multi_device.rs
+++ crates/burn-train/src/learner/strategies/multi_device.rs
@@ -1,23 +1,15 @@
-use burn_core::{
-    data::dataloader::split::split_dataloader,
-    module::{AutodiffModule, Module},
-    prelude::Backend,
-};
+use burn_core::{data::dataloader::split::split_dataloader, module::Module, prelude::Backend};
 
 use crate::{
     Learner, LearnerDataLoaders, MultiDeviceTrainEpoch, SingleDeviceValidEpoch, TrainLoader,
-    TrainStep, ValidLoader, ValidStep, components::LearnerComponents,
-    metric::processor::EventProcessor,
+    ValidLoader, components::LearnerComponents,
 };
 
-pub(crate) fn prepare_dataloaders_multi_device<LC, TI, VI>(
+pub(crate) fn prepare_dataloaders_multi_device<LC: LearnerComponents>(
     devices: &[<LC::Backend as Backend>::Device],
-    dataloader_train: TrainLoader<LC, TI>,
-    dataloader_valid: ValidLoader<LC, VI>,
-) -> LearnerDataLoaders<LC, TI, VI>
-where
-    LC: LearnerComponents,
-{
+    dataloader_train: TrainLoader<LC>,
+    dataloader_valid: ValidLoader<LC>,
+) -> LearnerDataLoaders<LC> {
     // `MultiDevicesTrainStep` has one worker per device, so we use a fixed device strategy
     // for each (worker) data loader. This matches the expected device on the worker, so we
     // don't have to move the data between devices.
@@ -41,20 +33,12 @@ pub(crate) fn prepare_model_multi_device<LC: LearnerComponents>(
     learner
 }
 
-pub(crate) fn learn_multi_device<LC, TI, VI, TO, VO>(
+pub(crate) fn learn_multi_device<LC: LearnerComponents>(
     devices: &[<LC::Backend as Backend>::Device],
     mut learner: Learner<LC>,
-    dataloaders: LearnerDataLoaders<LC, TI, VI>,
+    dataloaders: LearnerDataLoaders<LC>,
     starting_epoch: usize,
-) -> Learner<LC>
-where
-    TI: Send + 'static,
-    TO: Send + 'static,
-    LC: LearnerComponents,
-    LC::EventProcessor: EventProcessor<ItemValid = VO, ItemTrain = TO>,
-    <LC::Model as AutodiffModule<LC::Backend>>::InnerModule: ValidStep<VI, VO>,
-    <LC as LearnerComponents>::Model: TrainStep<TI, TO>,
-{
+) -> Learner<LC> {
     let LearnerDataLoaders::MultiTrainSingleValid {
         dataloader_train,
         dataloader_valid,
@@ -63,7 +47,7 @@ where
         panic!("Wrong dataloaders for strategy");
     };
 
-    let mut epoch_train = MultiDeviceTrainEpoch::new(
+    let mut epoch_train = MultiDeviceTrainEpoch::<LC>::new(
         dataloader_train,
         starting_epoch,
         learner.num_epochs,
@@ -71,7 +55,7 @@ where
     );
 
     for epoch in starting_epoch..learner.num_epochs + 1 {
-        (learner.model, learner.optim) = epoch_train.run::<LC, TO>(
+        (learner.model, learner.optim) = epoch_train.run(
             learner.model,
             learner.optim,
             &mut learner.lr_scheduler,
@@ -85,8 +69,8 @@ where
         }
 
         let epoch_valid =
-            SingleDeviceValidEpoch::new(dataloader_valid.clone(), epoch, learner.num_epochs);
-        epoch_valid.run::<LC, VO>(
+            SingleDeviceValidEpoch::<LC>::new(dataloader_valid.clone(), epoch, learner.num_epochs);
+        epoch_valid.run(
             &learner.model,
             &mut learner.event_processor,
             &learner.interrupter,
