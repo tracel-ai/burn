4b554016542e817648239c9b1faaf21d9b29575c
Guilhem Ané <guilhem.ane3@gmail.com>
2025-08-12 15:37:45 -0400

    fix grid_sample inf bug (#3523)
    * fix grid_sample inf bug
    
    * regen lockfile
    
    * fix typos and add test

--- crates/burn-tensor/src/tests/ops/grid_sample.rs
+++ crates/burn-tensor/src/tests/ops/grid_sample.rs
@@ -40,4 +40,22 @@ mod tests {
             .to_data()
             .assert_approx_eq::<FloatElem<TestBackend>>(&expected, Tolerance::default());
     }
+
+    #[test]
+    fn should_handle_inf_grid_sample_2d() {
+        let device = Default::default();
+        let tensor = TestTensor::<4>::from_floats(
+            [[[[0.0, 1.0, 2.0], [3.0, 4.0, 5.0], [6.0, 7.0, 8.0]]]],
+            &device,
+        );
+        let grid = TestTensor::<4>::from_floats([[[[0.0, 1.0f32 / 0.0]]]], &device);
+
+        let output = tensor.grid_sample_2d(grid);
+
+        // Should clamp to nearest, regardless of inf grid value: 7.0
+        let expected = TensorData::from([[[[7.0]]]]);
+        output
+            .to_data()
+            .assert_approx_eq::<FloatElem<TestBackend>>(&expected, Tolerance::default());
+    }
 }
