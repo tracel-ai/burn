af805d3dabb046797bf1483927b091e45a873334
Guillaume Lagrange <lagrange.guillaume.1@gmail.com>
2025-08-15 14:32:10 -0400

    Fix backward scatter dim

--- crates/burn-autodiff/src/tests/maxmin.rs
+++ crates/burn-autodiff/src/tests/maxmin.rs
@@ -56,4 +56,31 @@ mod tests {
             .to_data()
             .assert_approx_eq::<FT>(&expected, Tolerance::default());
     }
+
+    #[test]
+    fn should_diff_min_dim_3d_dim1() {
+        let device = Default::default();
+        let tensor_1 = TestAutodiffTensor::<3>::from_floats([[[1.0, 7.0], [-2.0, -3.0]]], &device)
+            .require_grad();
+        let tensor_2 =
+            TestAutodiffTensor::<3>::from_floats([[[4., -7.], [2., 3.]]], &device).require_grad();
+
+        let tensor_3 = tensor_1.clone().mul(tensor_2.clone());
+        let tensor_4 = tensor_3.min_dim(1);
+
+        let grads = tensor_4.backward();
+
+        let grad_1 = tensor_1.grad(&grads).unwrap();
+        let grad_2 = tensor_2.grad(&grads).unwrap();
+
+        let expected = TensorData::from([[[0., -7.], [2., 0.]]]);
+        grad_1
+            .to_data()
+            .assert_approx_eq::<FT>(&expected, Tolerance::default());
+
+        let expected = TensorData::from([[[0., 7.], [-2., -0.]]]);
+        grad_2
+            .to_data()
+            .assert_approx_eq::<FT>(&expected, Tolerance::default());
+    }
 }
--- crates/burn-autodiff/src/tests/sort.rs
+++ crates/burn-autodiff/src/tests/sort.rs
@@ -57,4 +57,30 @@ mod tests {
             .to_data()
             .assert_approx_eq::<FT>(&expected, Tolerance::default());
     }
+
+    #[test]
+    fn should_diff_sort_3d_dim1() {
+        let device = Default::default();
+        let tensor_1 = TestAutodiffTensor::<3>::from_floats([[[1.0, 7.0], [-2.0, -3.0]]], &device)
+            .require_grad();
+        let tensor_2 =
+            TestAutodiffTensor::from_floats([[[4.0, -7.0], [2.0, 3.0]]], &device).require_grad();
+
+        let tensor_3 = tensor_1.clone().matmul(tensor_2.clone());
+        let tensor_4 = tensor_1.clone().mul(tensor_3.sort(1));
+        let grads = tensor_4.backward();
+
+        let grad_1 = tensor_1.grad(&grads).unwrap();
+        let grad_2 = tensor_2.grad(&grads).unwrap();
+
+        let expected = TensorData::from([[[-1., -8.], [-27., 37.]]]);
+        grad_1
+            .to_data()
+            .assert_approx_eq::<FT>(&expected, Tolerance::default());
+
+        let expected = TensorData::from([[[-4., -17.], [-17., -42.]]]);
+        grad_2
+            .to_data()
+            .assert_approx_eq::<FT>(&expected, Tolerance::default());
+    }
 }
