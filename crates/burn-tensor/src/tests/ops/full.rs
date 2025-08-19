34c20d4daf3d79bf35560105ad17647ad0839c81
Crutcher Dunnavant <crutcher@gmail.com>
2025-08-19 05:29:19 -0700

    Lift .full()/.full_like() into base Tensor - support Tensor<B, D, Bool>::full()/full_like(). (#3562)
    * [WIP] Lift .full()/.full_like() into base Tensor.
    
    This is most of the work to lift `.full()` and `.full_like()`
    into the base Tensor ops.
    
    I need some help or more time with burn-router; the associated internal enum
    for operation descriptions has to change.
    
    Future Work:
    1. I would like to also lift `.zeros()` into the base; it's the logical
       pair to `.empty()`, and "all tensors have a default value" is easy
       to defend.
    
    2. I'd also like some form of `FromBoolOps` for things that can be
       constructed from bools, and lift `ones()` into that; but that
       is later.
    
    * work on router
    
    * bool full tests
    
    * change test per review

--- crates/burn-tensor/src/tests/ops/full.rs
+++ crates/burn-tensor/src/tests/ops/full.rs
@@ -15,26 +15,25 @@ mod tests {
         let device = Default::default();
         // Test full with f32
         let tensor = TestTensor::<2>::full([2, 3], 2.1, &device);
-
         tensor
             .into_data()
             .assert_eq(&TensorData::from([[2.1, 2.1, 2.1], [2.1, 2.1, 2.1]]), false);
 
         // Test full with Int
         let int_tensor = TestTensorInt::<2>::full([2, 2], 2, &device);
-
         int_tensor
             .into_data()
             .assert_eq(&TensorData::from([[2, 2], [2, 2]]), false);
 
-        // TODO enable after adding support for bool
-        // // Test full with bool
-        // let bool_tensor = TestTensorBool::<2>::full([2, 2], true, &device);
-        // let data_expected = TensorData::from([[true, true], [true, true]]);
-        // assert_eq!(data_expected, bool_tensor.into_data());
+        // Test full with bool
+        let bool_tensor = TestTensorBool::<2>::full([2, 2], true, &device);
+        bool_tensor
+            .into_data()
+            .assert_eq(&TensorData::from([[true, true], [true, true]]), false);
 
-        // let bool_tensor = TestTensorBool::<2>::full([2, 2], false, &device);
-        // let data_expected = TensorData::from([[false, false], [false, false]]);
-        // assert_eq!(data_expected, bool_tensor.into_data());
+        let bool_tensor = TestTensorBool::<2>::full([2, 2], false, &device);
+        bool_tensor
+            .into_data()
+            .assert_eq(&TensorData::from([[false, false], [false, false]]), false);
     }
 }
