a273f791424cd0d9c6f1268ffb51e223d1fed04c
buttfa <1662332017@qq.com>
2025-08-23 11:14:57 +0800

    fix(burn-dataset): rename cifar feature to builtin-sources

--- crates/burn-dataset/src/vision/mod.rs
+++ crates/burn-dataset/src/vision/mod.rs
@@ -1,9 +1,9 @@
-#[cfg(feature = "cifar")]
+#[cfg(feature = "builtin-sources")]
 mod cifar;
 mod image_folder;
 mod mnist;
 
-#[cfg(feature = "cifar")]
+#[cfg(feature = "builtin-sources")]
 pub use cifar::*;
 pub use image_folder::*;
 pub use mnist::*;
