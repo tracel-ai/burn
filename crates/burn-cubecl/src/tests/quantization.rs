982c83dd1f933890f9f4bb16f7514529b3d5b5d9
Guillaume Lagrange <lagrange.guillaume.1@gmail.com>
2025-08-26 10:58:47 -0400

    Fix q2 block size

--- crates/burn-cubecl/src/tests/quantization.rs
+++ crates/burn-cubecl/src/tests/quantization.rs
@@ -123,8 +123,15 @@ mod tests {
     }
 
     #[test]
-    fn should_quantize_dequantize_symmetric_per_block_q2s_packed() {
+    #[should_panic = "Block size must be divisible by 16"]
+    fn should_panic_when_block_size_cannot_store_num_quants() {
+        // num_quants in u32 = 32 bits / 2 bits = 16
         should_quantize_dequantize_symmetric_per_block(QuantValue::Q2S, 8, QuantStore::U32)
+    }
+
+    #[test]
+    fn should_quantize_dequantize_symmetric_per_block_q2s_packed() {
+        should_quantize_dequantize_symmetric_per_block(QuantValue::Q2S, 16, QuantStore::U32)
     }
 
     #[test]
