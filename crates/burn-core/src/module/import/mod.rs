3e96c4287a98b9c161b6d91a40b0901c7ed87026
Dilshod Tadjibaev <939125+antimora@users.noreply.github.com>
2025-09-02 16:09:00 -0500

    Clean up old import locations

--- crates/burn-core/src/module/import/mod.rs
+++
@@ -1,7 +0,0 @@
-mod appliers;
-mod module_ext;
-mod reader;
-
-pub use appliers::{ImportError, ImportResult, TensorApplier};
-pub use module_ext::ModuleImport;
-pub use reader::{DType, ReaderError, TensorMetadata, TensorReader};
