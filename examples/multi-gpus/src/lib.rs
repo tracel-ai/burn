375e363b8ed3a373667f754ed70631d8fdcd93f3
nathaniel <nathaniel.simard.42@gmail.com>
2025-09-09 15:43:37 -0400

    Add a sync

--- examples/multi-gpus/src/lib.rs
+++ examples/multi-gpus/src/lib.rs
@@ -33,7 +33,7 @@ pub fn run<B: Backend>(mut devices: Vec<B::Device>) {
         );
 
         while let Ok(tensor) = receiver.recv() {
-            println!("{tensor}");
+            B::sync(&tensor.device());
             let main = tensor.to_device(&aggregation_device);
             input = input + main.clone() / 2;
             let value = main.sum().into_scalar().elem::<f32>();
