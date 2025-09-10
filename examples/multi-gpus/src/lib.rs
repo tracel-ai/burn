888e90b36e4c063742c6a69dfe11e2d60966b28d
nathaniel <nathaniel.simard.42@gmail.com>
2025-09-10 11:33:53 -0400

    Check

--- examples/multi-gpus/src/lib.rs
+++ examples/multi-gpus/src/lib.rs
@@ -166,9 +166,9 @@ fn task_grad_all_reduce<B: AutodiffBackend>(
         .enumerate()
         .map(|(id, device)| {
             let model_main = model_main.clone();
-            let mut model = model_main.to_device(&device);
 
             std::thread::spawn(move || {
+                let mut model = model_main.fork(&device);
                 let id = PeerId::from(id);
                 let config_col = CollectiveConfig::default()
                     .with_num_devices(num_devices)
@@ -188,9 +188,7 @@ fn task_grad_all_reduce<B: AutodiffBackend>(
 
                     let x = TransformerEncoderInput::new(x);
                     let x = model.forward(x);
-
                     let grads = x.backward();
-                    println!("{grads:?}");
                     let grads = GradientsParams::from_grads(grads, &model);
                     println!("[{id}] grad {}", grads.len());
 
