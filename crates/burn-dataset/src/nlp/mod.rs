7ce2da4ebf69344d6e5dfde7ab94718741717e8c
buttfa <1662332017@qq.com>
2025-09-15 16:15:00 +0800

    fix(burn-dataset): remove FDU CLF dataset

--- crates/burn-dataset/src/nlp/mod.rs
+++ crates/burn-dataset/src/nlp/mod.rs
@@ -1,11 +1,7 @@
 #[cfg(feature = "builtin-sources")]
 mod ag_news;
-#[cfg(feature = "builtin-sources")]
-mod fdu_clf;
 mod text_folder;
 
 #[cfg(feature = "builtin-sources")]
 pub use ag_news::*;
-#[cfg(feature = "builtin-sources")]
-pub use fdu_clf::*;
 pub use text_folder::*;
