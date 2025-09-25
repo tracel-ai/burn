05f8b7db177e665ebb98e7d6b2de3f3d0f04eded
nathaniel <nathaniel.simard.42@gmail.com>
2025-09-25 09:03:07 -0400

    WIP

--- examples/text-classification/src/data/batcher.rs
+++ examples/text-classification/src/data/batcher.rs
@@ -11,7 +11,10 @@
 // generates a padding mask, and returns a batch object.
 
 use super::{dataset::TextClassificationItem, tokenizer::Tokenizer};
-use burn::{data::dataloader::batcher::Batcher, nn::attention::generate_padding_mask, prelude::*};
+use burn::{
+    backend::NdArray, data::dataloader::batcher::Batcher, nn::attention::generate_padding_mask,
+    prelude::*,
+};
 use std::sync::Arc;
 
 /// Struct for batching text classification items
@@ -47,30 +50,30 @@ impl<B: Backend> Batcher<B, TextClassificationItem, TextClassificationTrainingBa
         device: &B::Device,
     ) -> TextClassificationTrainingBatch<B> {
         let mut tokens_list = Vec::with_capacity(items.len());
-        let mut labels_list = Vec::with_capacity(items.len());
+        let mut labels_list: Vec<Tensor<NdArray, 1, Int>> = Vec::with_capacity(items.len());
 
         // Tokenize text and create label tensor for each item
         for item in items {
             tokens_list.push(self.tokenizer.encode(&item.text));
             labels_list.push(Tensor::from_data(
                 TensorData::from([(item.label as i64).elem::<B::IntElem>()]),
-                device,
+                &Default::default(),
             ));
         }
 
         // Generate padding mask for tokenized text
-        let mask = generate_padding_mask(
+        let mask = generate_padding_mask::<NdArray>(
             self.tokenizer.pad_token(),
             tokens_list,
             Some(self.max_seq_length),
-            device,
+            &Default::default(),
         );
 
         // Create and return training batch
         TextClassificationTrainingBatch {
-            tokens: mask.tensor,
-            labels: Tensor::cat(labels_list, 0),
-            mask_pad: mask.mask,
+            tokens: Tensor::from_data(mask.tensor.into_data(), device),
+            labels: Tensor::from_data(Tensor::cat(labels_list, 0).into_data(), device),
+            mask_pad: Tensor::from_data(mask.mask.into_data(), device),
         }
     }
 }
