ddc5ddbfb802d84c321140664edff8496b0f61f9
Crutcher Dunnavant <crutcher@gmail.com>
2025-10-08 17:14:12 -0700

    float traits

--- crates/burn-tensor/src/tensor/ops/int_tensor.rs
+++ crates/burn-tensor/src/tensor/ops/int_tensor.rs
@@ -7,6 +7,7 @@ use crate::{
 use crate::{TensorMetadata, argsort, sort, sort_with_indices};
 use alloc::vec::Vec;
 use core::ops::Range;
+use num_traits::Float;
 
 /// Int Tensor API for basic and numeric operations, see [tensor](crate::Tensor)
 /// for documentation on each function.
