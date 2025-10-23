530e82abb11606c51950c20f33418029592fccfb
Charles23R <renaudcharles14@hotmail.com>
2025-10-23 16:10:58 -0400

    WIP custom example

--- examples/custom-learning-strategy/src/lib.rs
+++ examples/custom-learning-strategy/src/lib.rs
@@ -1,1 +1,18 @@
+use std::marker::PhantomData;
 
+use burn::tensor::{Device, backend::AutodiffBackend};
+
+pub fn run<B: AutodiffBackend>(device: B::Device) {}
+
+pub struct SingleDeviceLearningStrategy<LC: LearnerComponentTypes> {
+    device: Device<LC::Backend>,
+    _p: PhantomData<LC>,
+}
+impl<LC: LearnerComponentTypes> SingleDeviceLearningStrategy<LC> {
+    pub fn new(device: Device<LC::Backend>) -> Self {
+        Self {
+            device,
+            _p: PhantomData,
+        }
+    }
+}
