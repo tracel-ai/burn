3508b9e885e181bda0df66845710c02b849d433a
Keeley Hoek <keeley@hoek.io>
2025-11-26 15:50:56 +1100

    Fix float_cat backprop (CatStep.step()) correctness
    CatStep zips nodes (which are Option<NodeRef>) with dim_sizes, then does a filter_map to drop Nones before advancing current_index. That causes gradient slices to be misaligned whenever some inputs don't require grad.

--- crates/burn-autodiff/src/ops/tensor.rs
+++ crates/burn-autodiff/src/ops/tensor.rs
@@ -2407,18 +2407,22 @@ impl<B: Backend, C: CheckpointStrategy> FloatTensorOps<Self> for Autodiff<B, C> 
         impl<B: Backend> Step for CatStep<B> {
             fn step(self: Box<Self>, grads: &mut Gradients, _checkpointer: &mut Checkpointer) {
                 let grad = grads.consume::<B>(&self.output);
-                let ranges: Vec<_> = grad.shape().iter().map(|v| 0..*v).collect();
-
-                let mut current_index = 0;
+                let ranges_template: Vec<_> = grad.shape().iter().map(|&v| 0..v).collect();
 
                 self.nodes
                     .into_iter()
                     .zip(self.dim_sizes)
-                    .filter_map(|(node, dim_size)| node.map(|node| (node, dim_size)))
-                    .for_each(|(node, dim_size)| {
-                        let mut ranges = ranges.clone();
-                        ranges[self.dim] = current_index..dim_size + current_index;
-                        current_index += dim_size;
+                    .scan(0, |offset, (node_opt, dim_size)| {
+                        let start = *offset;
+                        let end = start + dim_size;
+                        *offset = end;
+                        Some(node_opt.map(|node| (node, start, end)))
+                    })
+                    .flatten()
+                    .for_each(|(node, start, end)| {
+                        let mut ranges = ranges_template.clone();
+                        ranges[self.dim] = start..end;
+
                         let slices: Vec<burn_tensor::Slice> = ranges
                             .iter()
                             .map(|r| {
