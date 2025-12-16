482237760908041217889ce6d1281d758f97e3b9
mertalev <101130780+mertalev@users.noreply.github.com>
2025-12-16 16:31:38 -0500

    pr feedback

--- crates/burn-import/src/burn/node/prelu.rs
+++ crates/burn-import/src/burn/node/prelu.rs
@@ -1,5 +1,21 @@
 use super::prelude::*;
 use burn_store::TensorSnapshot;
+use onnx_ir::ir::ArgType;
+
+/// Calculate num_parameters from slope tensor's static shape.
+fn num_parameters(node: &onnx_ir::prelu::PReluNode) -> usize {
+    node.inputs
+        .get(1)
+        .and_then(|slope| {
+            if let ArgType::Tensor(tensor) = &slope.ty {
+                tensor.static_shape.as_ref()
+            } else {
+                None
+            }
+        })
+        .map(|shape| shape.iter().product())
+        .unwrap_or(1)
+}
 
 impl NodeCodegen for onnx_ir::prelu::PReluNode {
     fn inputs(&self) -> &[Argument] {
@@ -12,16 +28,12 @@ impl NodeCodegen for onnx_ir::prelu::PReluNode {
 
     fn field(&self) -> Option<Field> {
         let name = Ident::new(&self.name, Span::call_site());
-        let num_parameters = self.config.num_parameters;
-
-        let init = if num_parameters > 1 {
-            let n = num_parameters.to_tokens();
-            quote! { let #name = PReluConfig::new().with_num_parameters(#n).init(device); }
-        } else {
-            quote! { let #name = PReluConfig::new().init(device); }
-        };
-
-        Some(Field::new(self.name.clone(), quote! { PRelu<B> }, init))
+        let n = num_parameters(self).to_tokens();
+        Some(Field::new(
+            self.name.clone(),
+            quote! { PRelu<B> },
+            quote! { let #name = PReluConfig::new().with_num_parameters(#n).init(device); },
+        ))
     }
 
     fn collect_snapshots(&self, field_name: &str) -> Vec<TensorSnapshot> {
@@ -34,7 +46,7 @@ impl NodeCodegen for onnx_ir::prelu::PReluNode {
             let alpha_path = format!("{}.alpha", field_name);
             if let Some(mut snapshot) = create_lazy_snapshot(alpha_input, &alpha_path, "PRelu") {
                 // Squeeze dimensions to 1D
-                snapshot.shape = vec![self.config.num_parameters];
+                snapshot.shape = vec![num_parameters(self)];
                 snapshots.push(snapshot);
             }
         }
@@ -63,23 +75,18 @@ mod tests {
     use super::super::test_helpers::*;
     use burn::tensor::DType;
     use insta::assert_snapshot;
-    use onnx_ir::prelu::{PReluConfig, PReluNodeBuilder};
-
-    fn create_prelu_node(name: &str, num_parameters: usize) -> onnx_ir::prelu::PReluNode {
-        PReluNodeBuilder::new(name)
-            .input_tensor("input", 2, DType::F32)
-            .input_tensor("slope", 1, DType::F32)
-            .output_tensor("output", 2, DType::F32)
-            .config(PReluConfig::new(num_parameters))
-            .build()
-    }
+    use onnx_ir::prelu::PReluNodeBuilder;
 
     #[test]
     fn test_prelu_forward() {
-        let node = create_prelu_node("prelu1", 64);
+        let node = PReluNodeBuilder::new("prelu1")
+            .input_tensor("input", 4, DType::F32)
+            .input_tensor_shape("slope", vec![64, 1, 1], DType::F32)
+            .output_tensor("output", 4, DType::F32)
+            .build();
         let code = codegen_forward_default(&node);
         assert_snapshot!(code, @r"
-        pub fn forward(&self, input: Tensor<B, 2>, slope: Tensor<B, 1>) -> Tensor<B, 2> {
+        pub fn forward(&self, input: Tensor<B, 4>, slope: Tensor<B, 3>) -> Tensor<B, 4> {
             let output = self.prelu1.forward(input);
             output
         }
@@ -88,15 +95,23 @@ mod tests {
 
     #[test]
     fn test_prelu_field_with_channel_slope() {
-        let node = create_prelu_node("prelu1", 64);
+        let node = PReluNodeBuilder::new("prelu1")
+            .input_tensor("input", 4, DType::F32)
+            .input_tensor_shape("slope", vec![64, 1, 1], DType::F32)
+            .output_tensor("output", 4, DType::F32)
+            .build();
         let code = codegen_field_init(&node);
         assert_snapshot!(code, @"let prelu1 = PReluConfig::new().with_num_parameters(64).init(device);");
     }
 
     #[test]
     fn test_prelu_field_with_scalar_slope() {
-        let node = create_prelu_node("prelu1", 1);
+        let node = PReluNodeBuilder::new("prelu1")
+            .input_tensor("input", 4, DType::F32)
+            .input_tensor_shape("slope", vec![1], DType::F32)
+            .output_tensor("output", 4, DType::F32)
+            .build();
         let code = codegen_field_init(&node);
-        assert_snapshot!(code, @"let prelu1 = PReluConfig::new().init(device);");
+        assert_snapshot!(code, @"let prelu1 = PReluConfig::new().with_num_parameters(1).init(device);");
     }
 }
