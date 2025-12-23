f25c76b4a8c50554c20ce9e2ccf4160edba7c452
Crutcher Dunnavant <crutcher@gmail.com>
2025-12-22 17:47:35 -0800

    merge/update

--- crates/burn-std/src/tensor/shape.rs
+++ crates/burn-std/src/tensor/shape.rs
@@ -3,7 +3,6 @@
 use super::indexing::ravel_index;
 use super::{AsIndex, Slice, SliceArg};
 use alloc::format;
-use alloc::string::String;
 use alloc::string::ToString;
 use alloc::vec;
 use alloc::vec::Vec;
