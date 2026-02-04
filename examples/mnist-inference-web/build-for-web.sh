d33fa1de26314e882b0ebee7a65021471d301a46
keke <zhoukekestar@163.com>
2026-02-04 10:44:08 +0800

    Simplify build script by removing wasm-bindgen check
    Removed installation check for wasm-bindgen.

--- examples/mnist-inference-web/build-for-web.sh
+++ examples/mnist-inference-web/build-for-web.sh
@@ -9,14 +9,8 @@ then
     cargo install wasm-pack
 fi
 
-if ! command -v wasm-bindgen &> /dev/null
-then
-    echo "wasm-bindgen could not be found. Installing ..."
-    cargo install wasm-bindgen-cli
-fi
-
 # Set optimization flags
-export RUSTFLAGS="-C embed-bitcode=yes -C codegen-units=1 -C opt-level=3 --cfg web_sys_unstable_apis --cfg getrandom_backend=\"wasm_js\""
+RUSTFLAGS="-C embed-bitcode=yes -C codegen-units=1 -C opt-level=3 --cfg web_sys_unstable_apis --cfg getrandom_backend=\"wasm_js\""
 
 # Run wasm pack tool to build JS wrapper files and copy wasm to pkg directory.
 mkdir -p pkg
