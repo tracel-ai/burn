83fd0e7a63fae44906a371f08a96fac0d7514b94
Genna Wingert <1058083+wingertge@users.noreply.github.com>
2026-02-17 15:29:21 +0100

    Revert temp fix

--- crates/burn-cubecl-fusion/src/engine/launch/executor.rs
+++ crates/burn-cubecl-fusion/src/engine/launch/executor.rs
@@ -71,8 +71,6 @@ impl<'a, R: Runtime> LaunchPlanExecutor<'a, R> {
             return Ok(tune_output);
         }
 
-        let mut configs = Vec::with_capacity(plan.blocks.len());
-
         let mut inputs = GlobalArgsLaunch::default();
         let mut outputs = GlobalArgsLaunch::default();
 
@@ -94,6 +92,8 @@ impl<'a, R: Runtime> LaunchPlanExecutor<'a, R> {
             }
         }
 
+        let mut configs = Vec::with_capacity(plan.blocks.len());
+
         for (block_plan, block) in plan.blocks.into_iter().zip(self.blocks) {
             let reference = match block_plan.reference {
                 ReferenceSelection::Concrete { layout, .. } => RefLayout::Concrete(layout),
@@ -113,8 +113,6 @@ impl<'a, R: Runtime> LaunchPlanExecutor<'a, R> {
                     RefLayout::Virtual(VirtualLayout::Runtime { pos })
                 }
                 ReferenceSelection::Searching => {
-                    drop(inputs);
-                    drop(outputs);
                     return Err(ExecutionError::new(
                         TraceError::ReferenceNotFound,
                         plan.handle_inputs,
