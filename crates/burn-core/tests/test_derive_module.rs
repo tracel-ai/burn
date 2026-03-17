7915e8d7e358d2882c22b09eb6f372732c35ad03
Charles23R <renaudcharles14@hotmail.com>
2026-03-17 17:16:30 -0400

    test

--- crates/burn-core/tests/test_derive_module.rs
+++ crates/burn-core/tests/test_derive_module.rs
@@ -670,7 +670,7 @@ mod require_grad {
         let y = transformation(module.weight_basic.val(), x);
 
         let mut grads = y.backward();
-        TestAutodiffBackend::<F = f32, I = i32>::collective_sync_native(&device);
+        TestAutodiffBackend::<f32, i32>::collective_sync_native(&device);
         module.weight_basic.grad_remove(&mut grads)
     }
 }
