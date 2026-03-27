72bdd16b3267eef34c64c7b3918f4db38a2ccdae
Charles23R <renaudcharles14@hotmail.com>
2026-03-27 11:16:39 -0400

    test

--- examples/text-classification/examples/ag-news-train.rs
+++ examples/text-classification/examples/ag-news-train.rs
@@ -41,7 +41,7 @@ pub fn launch<B: AutodiffBackend>(devices: Vec<B::Device>) {
         AgNewsDataset::train(),
         AgNewsDataset::test(),
         config,
-        "/tmp/text-classification-ag-news",
+        "/home/charles/text-classification-ag-news",
     );
 }
 
