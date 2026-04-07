ec712c7afcbc5315da4fb017375216940e2d6fcf
Charles23R <renaudcharles14@hotmail.com>
2026-04-07 11:16:44 -0400

    async collective api

--- crates/burn-cubecl/src/ops/distributed.rs
+++ crates/burn-cubecl/src/ops/distributed.rs
@@ -1,5 +1,5 @@
 use burn_backend::{
-    DeviceOps, StreamId, TensorMetadata,
+    DeviceId, DeviceOps, StreamId, TensorMetadata,
     distributed::{DistributedBackend, ReduceOperation},
     tensor::{Device, FloatTensor},
 };
@@ -13,38 +13,65 @@ where
     I: IntElement,
     BT: BoolElement,
 {
+    // unsafe fn all_reduce(
+    //     tensors: Vec<FloatTensor<Self>>,
+    //     op: ReduceOperation,
+    // ) -> Vec<FloatTensor<Self>> {
+    //     let all_ids = tensors.iter().map(|t| t.device.id()).collect::<Vec<_>>();
+    //     let mut output = vec![];
+
+    //     for tensor in tensors {
+    //         let device = &tensor.device;
+    //         let old = unsafe { StreamId::swap(tensor.handle.stream) };
+    //         let out_tensor = empty(tensor.shape(), device, tensor.dtype());
+
+    //         let op = match op {
+    //             ReduceOperation::Sum => cubecl::server::ReduceOperation::Sum,
+    //             ReduceOperation::Mean => cubecl::server::ReduceOperation::Mean,
+    //         };
+
+    //         let client = R::client(device);
+    //         client.all_reduce(
+    //             tensor.handle.clone(),
+    //             out_tensor.handle.clone(),
+    //             tensor.dtype.into(),
+    //             all_ids.clone(),
+    //             op,
+    //         );
+
+    //         output.push(out_tensor);
+
+    //         unsafe { StreamId::swap(old) };
+    //     }
+
+    //     output
+    // }
+
     unsafe fn all_reduce(
-        tensors: Vec<FloatTensor<Self>>,
+        tensor: FloatTensor<Self>,
         op: ReduceOperation,
-    ) -> Vec<FloatTensor<Self>> {
-        let all_ids = tensors.iter().map(|t| t.device.id()).collect::<Vec<_>>();
-        let mut output = vec![];
+        device_ids: Vec<DeviceId>,
+    ) -> FloatTensor<Self> {
+        let device = &tensor.device;
+        let old = unsafe { StreamId::swap(tensor.handle.stream) };
+        let out_tensor = empty(tensor.shape(), device, tensor.dtype());
 
-        for tensor in tensors {
-            let device = &tensor.device;
-            let old = unsafe { StreamId::swap(tensor.handle.stream) };
-            let out_tensor = empty(tensor.shape(), device, tensor.dtype());
+        let op = match op {
+            ReduceOperation::Sum => cubecl::server::ReduceOperation::Sum,
+            ReduceOperation::Mean => cubecl::server::ReduceOperation::Mean,
+        };
 
-            let op = match op {
-                ReduceOperation::Sum => cubecl::server::ReduceOperation::Sum,
-                ReduceOperation::Mean => cubecl::server::ReduceOperation::Mean,
-            };
+        let client = R::client(device);
+        client.all_reduce(
+            tensor.handle.clone(),
+            out_tensor.handle.clone(),
+            tensor.dtype.into(),
+            device_ids.clone(),
+            op,
+        );
 
-            let client = R::client(device);
-            client.all_reduce(
-                tensor.handle.clone(),
-                out_tensor.handle.clone(),
-                tensor.dtype.into(),
-                all_ids.clone(),
-                op,
-            );
-
-            output.push(out_tensor);
-
-            unsafe { StreamId::swap(old) };
-        }
-
-        output
+        unsafe { StreamId::swap(old) };
+        out_tensor
     }
 
     fn sync_collective(device: &Device<Self>) {
