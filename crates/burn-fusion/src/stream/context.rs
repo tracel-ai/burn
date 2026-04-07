9894da66b1e0b7a6189d7f1f20218d4ec9b96c4a
Sublime <80297802+Sublime12@users.noreply.github.com>
2026-04-07 09:11:06 -0400

    Feat/implement fusion for rfft (#4735)
    * ignore my tags file in .gitignore
    
    * add fusion support for rfft
    
    * remove warnings for missing documentation
    
    * update router runner, add rfft call
    
    * fix typos in doc for rfft

--- crates/burn-fusion/src/stream/context.rs
+++ crates/burn-fusion/src/stream/context.rs
@@ -485,6 +485,12 @@ impl RelativeOps for ModuleOperationIr {
                     out: desc.out.to_relative(converter),
                 })
             }
+            ModuleOperationIr::Rfft(desc) => ModuleOperationIr::Rfft(RfftOpIr {
+                signal: desc.signal.to_relative(converter),
+                dim: desc.dim,
+                out_re: desc.out_re.to_relative(converter),
+                out_im: desc.out_re.to_relative(converter),
+            }),
             ModuleOperationIr::Attention(desc) => ModuleOperationIr::Attention(AttentionOpIr {
                 query: desc.query.to_relative(converter),
                 key: desc.key.to_relative(converter),
