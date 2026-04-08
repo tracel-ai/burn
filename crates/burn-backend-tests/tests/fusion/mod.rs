a8a78a76f445d35282ba8c99395740575cebfa39
Guillaume Lagrange <lagrange.guillaume.1@gmail.com>
2026-04-08 15:04:13 -0400

    Temporarily disable fusion tests using burn::nn

--- crates/burn-backend-tests/tests/fusion/mod.rs
+++ crates/burn-backend-tests/tests/fusion/mod.rs
@@ -1,5 +1,6 @@
 pub use super::*;
 
-mod fusion_f16_broadcast;
-mod fusion_f16_write_vectorization;
+// TODO: remove nn usage and re-include
+// mod fusion_f16_broadcast;
+// mod fusion_f16_write_vectorization;
 mod reduce_broadcasted;
