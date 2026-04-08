bcc685e11eff05ce7f1dcf0cd8b67750fa5557f0
Charles23R <renaudcharles14@hotmail.com>
2026-04-08 13:25:32 -0400

    fork

--- crates/burn-core/tests/test_derive_module.rs
+++ crates/burn-core/tests/test_derive_module.rs
@@ -644,7 +644,8 @@ mod grad_distributed {
 
         for _ in 0..num_iter {
             // module = set_distributed(&module, &device);
-            let grads_x = calculate_grads(&module.fork(&device), transformation);
+            let mut module = module.clone().fork(&device);
+            let grads_x = calculate_grads(&module, transformation);
             let data = grads_x.unwrap().to_data();
             println!("data : {:?}", data.to_vec::<f32>());
             if !is_main {
