814b517721bc77d190d20d6570bd7d5c2eaecc26
Charles23R <renaudcharles14@hotmail.com>
2026-04-09 11:55:50 -0400

    update gradients syncing test

--- crates/burn-core/tests/test_derive_module.rs
+++ crates/burn-core/tests/test_derive_module.rs
@@ -549,7 +549,12 @@ mod grad_distributed {
         let device_count = <B as Backend>::device_count(type_id);
         let devices = create_devices::<B::Device>(type_id, device_count);
         let module = ModuleBasic::<B>::new(&devices[0]);
-        let (senders, receivers) = create_channels(device_count);
+        let (synced_senders, synced_receivers) = (1..device_count)
+            .map(|_| std::sync::mpsc::channel())
+            .unzip();
+        let (original_senders, original_receivers) = (1..device_count)
+            .map(|_| std::sync::mpsc::channel())
+            .unzip();
 
         let config = DistributedConfig { all_reduce_op: op };
         B::start_communication_server(devices.as_slice(), config);
@@ -557,8 +562,10 @@ mod grad_distributed {
         let join_handles = spawn_peer_threads(
             &module,
             &devices,
-            senders,
-            receivers,
+            synced_senders,
+            original_senders,
+            synced_receivers,
+            original_receivers,
             transformation,
             NUM_ITERATIONS,
         );
@@ -587,8 +594,10 @@ mod grad_distributed {
     fn spawn_peer_threads<B: AutodiffBackend>(
         module: &ModuleBasic<B>,
         devices: &[<B as Backend>::Device],
-        senders: Vec<Sender<TensorData>>,
-        receivers: Vec<Receiver<TensorData>>,
+        synced_senders: Vec<Sender<TensorData>>,
+        original_senders: Vec<Sender<Tensor<B::InnerBackend, 2>>>,
+        synced_receivers: Vec<Receiver<TensorData>>,
+        original_receivers: Vec<Receiver<Tensor<B::InnerBackend, 2>>>,
         transformation: fn(Tensor<B, 2>, Tensor<B, 2>) -> Tensor<B, 2>,
         num_iter: usize,
     ) -> Vec<std::thread::JoinHandle<()>> {
@@ -601,11 +610,13 @@ mod grad_distributed {
             run_peer_sharded(
                 &module_clone,
                 None,
+                None,
                 transformation,
                 device,
                 num_iter,
                 true,
-                receivers,
+                synced_receivers,
+                original_receivers,
             )
         }));
 
@@ -613,15 +624,18 @@ mod grad_distributed {
         for i in 1..devices.len() {
             let module_clone = module.clone();
             let device = devices[i].clone();
-            let sender = Some(senders[i - 1].clone());
+            let synced_sender = Some(synced_senders[i - 1].clone());
+            let original_sender = Some(original_senders[i - 1].clone());
             handles.push(std::thread::spawn(move || {
                 run_peer_sharded(
                     &module_clone,
-                    sender,
+                    synced_sender,
+                    original_sender,
                     transformation,
                     device,
                     num_iter,
                     false,
+                    vec![],
                     vec![],
                 )
             }));
@@ -632,26 +646,43 @@ mod grad_distributed {
 
     pub fn run_peer_sharded<B: AutodiffBackend>(
         module: &ModuleBasic<B>,
-        output: Option<Sender<TensorData>>,
+        synced_sender: Option<Sender<TensorData>>,
+        original_sender: Option<Sender<Tensor<B::InnerBackend, 2>>>,
         transformation: fn(Tensor<B, 2>, Tensor<B, 2>) -> Tensor<B, 2>,
         device: B::Device,
         num_iter: usize,
         is_main: bool,
-        recvs: Vec<Receiver<TensorData>>,
+        synced_recvs: Vec<Receiver<TensorData>>,
+        original_recvs: Vec<Receiver<Tensor<B::InnerBackend, 2>>>,
     ) {
         let mut module = module.clone().fork(&device);
 
         for _ in 0..num_iter {
             module = set_distributed(&module, &device);
-            let grads_x = calculate_grads(&module, transformation);
-            let data = grads_x.unwrap().to_data();
+            let (grads_synced, grads_original) = calculate_grads(&module, transformation);
+            let data = grads_synced.unwrap().to_data();
             if !is_main {
-                output.clone().unwrap().send(data).unwrap();
+                original_sender
+                    .clone()
+                    .unwrap()
+                    .send(grads_original.unwrap())
+                    .unwrap();
+                synced_sender.clone().unwrap().send(data).unwrap();
             } else {
-                for r in recvs.iter().by_ref() {
+                let mut expected = None;
+                for r in original_recvs.iter().by_ref() {
                     let t = r.recv().unwrap();
-                    // TODO: should compute expected and compare to it.
-                    assert_eq!(data, t);
+                    expected = match expected {
+                        Some(prev) => prev.add(t),
+                        None => Some(t),
+                    }
+                }
+                for r in synced_recvs.iter().by_ref() {
+                    let data = r.recv().unwrap();
+                    data.assert_approx_eq::<FloatElem>(
+                        &expected.unwrap().to_data(),
+                        Tolerance::default(),
+                    );
                 }
             }
         }
@@ -671,18 +702,30 @@ mod grad_distributed {
     fn calculate_grads<B: AutodiffBackend>(
         module: &ModuleBasic<B>,
         transformation: fn(Tensor<B, 2>, Tensor<B, 2>) -> Tensor<B, 2>,
-    ) -> Option<Tensor<B::InnerBackend, 2>> {
+    ) -> (
+        Option<Tensor<B::InnerBackend, 2>>,
+        Option<Tensor<B::InnerBackend, 2>>,
+    ) {
         let device = module.weight_basic.device();
         let data = TensorData::random::<f32, _, _>(
             module.weight_basic.shape(),
             burn_tensor::Distribution::Default,
             &mut StdRng::try_from_rng(&mut SysRng).unwrap(),
         );
-        let x = Tensor::from_data(data, &device).require_grad();
+        let x = Tensor::from_data(data.clone(), &device).require_grad();
         let t = module.weight_basic.val();
         let y = transformation(t, x);
 
         let mut grads = y.backward();
-        module.weight_basic.grad_remove(&mut grads)
+        let grads_synced = module.weight_basic.grad_remove(&mut grads);
+
+        // Kind of hacky, but running the backward pass again without marking the tensor as distributed will not sync gradients.
+        // We can use this to compute the expected sum.
+        let x = Tensor::from_data(data, &device).require_grad();
+        let t = module.weight_basic.val();
+        let y = transformation(t, x);
+        let mut grads = y.backward();
+        let grads_original = module.weight_basic.grad_remove(&mut grads);
+        (grads_sync, grads_original)
     }
 }
