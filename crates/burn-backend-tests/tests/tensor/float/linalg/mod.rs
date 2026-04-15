6212496149151decdce81ad990adc9b96d69c45c
Softmaximalist <121367647+softmaximalist@users.noreply.github.com>
2026-04-15 15:51:52 -0400

    Add tests for linalg::det

--- crates/burn-backend-tests/tests/tensor/float/linalg/mod.rs
+++ crates/burn-backend-tests/tests/tensor/float/linalg/mod.rs
@@ -1,9 +1,9 @@
 use super::*;
 
 pub(crate) mod cosine_similarity;
+pub(crate) mod det;
 pub(crate) mod diag;
 pub(crate) mod lu;
-// pub(crate) mod lu_decomposition;
 pub(crate) mod matvec;
 pub(crate) mod outer;
 pub(crate) mod trace;
