fb4e865ddbda966a25f7c96bec588e802ad1c71a
Charles23R <renaudcharles14@hotmail.com>
2026-04-21 09:40:57 -0400

    update all_reduce test

--- crates/burn-backend-tests/tests/tensor/distributed.rs
+++ crates/burn-backend-tests/tests/tensor/distributed.rs
@@ -75,7 +75,7 @@ fn run_all_reduce<B: AutodiffBackend + DistributedBackend>(
         for tensor in out_tensors {
             let tensor_resolved: Tensor<B, 2, Float> =
                 Tensor::new(TensorPrimitive::Float(output.resolve()));
-            let data = tensor.flatten::<1>(0, 1).to_data();
+            let data = tensor_resolved.flatten::<1>(0, 1).to_data();
             data.assert_approx_eq::<FloatElem>(
                 &TensorData::from(expected.as_slice()),
                 Tolerance::default(),
