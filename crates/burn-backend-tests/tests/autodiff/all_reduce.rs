f62a168f20ada554e5a8fb0521c8eee17962d521
Charles23R <renaudcharles14@hotmail.com>
2026-04-23 11:32:02 -0400

    test smtg

--- crates/burn-backend-tests/tests/autodiff/all_reduce.rs
+++ crates/burn-backend-tests/tests/autodiff/all_reduce.rs
@@ -32,7 +32,8 @@ fn test_all_reduce() {
         ReduceOperation::Sum,
         device_ids.clone(),
     );
-    let tensor_2: TestTensor<1> = TestTensor::new(TensorPrimitive::Float(tensor_2.resolve()));
+    let tensor_2: TestTensor<1> =
+        TestTensor::new(TensorPrimitive::Float(tensor_2.resolve())).require_grad();
     let grads_0 = tensor_2.backward();
 
     let tensor_3 = B::all_reduce(
@@ -40,7 +41,8 @@ fn test_all_reduce() {
         ReduceOperation::Sum,
         device_ids,
     );
-    let tensor_3: TestTensor<1> = TestTensor::new(TensorPrimitive::Float(tensor_3.resolve()));
+    let tensor_3: TestTensor<1> =
+        TestTensor::new(TensorPrimitive::Float(tensor_3.resolve())).require_grad();
     let grads_1 = tensor_3.backward();
 
     println!(
