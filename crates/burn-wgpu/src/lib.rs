f8fe91296ff34105592d3a6943727c9b2b1eeed5
nathaniel <nathaniel.simard.42@gmail.com>
2026-05-22 10:29:03 -0400

    WIP

--- crates/burn-wgpu/src/lib.rs
+++ crates/burn-wgpu/src/lib.rs
@@ -14,6 +14,13 @@ pub use burn_cubecl::{CubeBackend, tensor::CubeTensor};
 pub use cubecl::CubeDim;
 pub use cubecl::flex32;
 
+#[cfg(feature = "metal")]
+use cubecl::wgpu::MslCompiler;
+#[cfg(feature = "vulkan")]
+use cubecl::wgpu::SpirvCompiler;
+#[cfg(feature = "webgpu")]
+use cubecl::wgpu::WgslCompiler;
+
 pub use cubecl::wgpu::{
     AutoCompiler, MemoryConfiguration, RuntimeOptions, WgpuDevice, WgpuResource, WgpuRuntime,
     WgpuSetup, WgpuStorage, init_device, init_setup, init_setup_async,
@@ -23,91 +30,26 @@ pub mod graphics {
     pub use cubecl::wgpu::{AutoGraphicsApi, Dx12, GraphicsApi, Metal, OpenGl, Vulkan, WebGpu};
 }
 
-#[cfg(feature = "cubecl-wgsl")]
-pub use cubecl::wgpu::WgslCompiler;
-#[cfg(feature = "cubecl-spirv")]
-pub use cubecl::wgpu::vulkan::VkSpirvCompiler;
-
 #[cfg(feature = "fusion")]
-/// Tensor backend that uses the wgpu crate for executing GPU compute shaders.
-///
-/// This backend can target multiple graphics APIs, including:
-///   - [Vulkan][crate::graphics::Vulkan] on Linux, Windows, and Android.
-///   - [OpenGL](crate::graphics::OpenGl) on Linux, Windows, and Android.
-///   - [DirectX 12](crate::graphics::Dx12) on Windows.
-///   - [Metal][crate::graphics::Metal] on Apple hardware.
-///   - [WebGPU](crate::graphics::WebGpu) on supported browsers and `wasm` runtimes.
-///
-/// To configure the wgpu backend, eg. to select what graphics API to use or what memory strategy to use,
-/// you have to manually initialize the runtime. For example:
-///
-/// ```rust, ignore
-/// fn custom_init() {
-///     let device = Default::default();
-///     burn::backend::wgpu::init_setup::<burn::backend::wgpu::graphics::Vulkan>(
-///         &device,
-///         Default::default(),
-///     );
-/// }
-/// ```
-/// will mean the given device (in this case the default) will be initialized to use Vulkan as the graphics API.
-/// It's also possible to use an existing wgpu device, by using `init_device`.
-///
-/// # Notes
-///
-/// This version of the wgpu backend uses [burn_fusion] to compile and optimize streams of tensor
-/// operations for improved performance.
-///
-/// You can disable the `fusion` feature flag to remove that functionality, which might be
-/// necessary on `wasm` for now.
-pub type Wgpu<F = f32, I = i32, B = u32> =
-    burn_fusion::Fusion<CubeBackend<cubecl::wgpu::WgpuRuntime, F, I, B>>;
+type WgpuInner<C, F, I, B> =
+    burn_fusion::Fusion<CubeBackend<cubecl::wgpu::WgpuRuntime<C>, F, I, B>>;
 
 #[cfg(not(feature = "fusion"))]
-/// Tensor backend that uses the wgpu crate for executing GPU compute shaders.
-///
-/// This backend can target multiple graphics APIs, including:
-///   - [Vulkan] on Linux, Windows, and Android.
-///   - [OpenGL](crate::OpenGl) on Linux, Windows, and Android.
-///   - [DirectX 12](crate::Dx12) on Windows.
-///   - [Metal] on Apple hardware.
-///   - [WebGPU](crate::WebGpu) on supported browsers and `wasm` runtimes.
-///
-/// To configure the wgpu backend, eg. to select what graphics API to use or what memory strategy to use,
-/// you have to manually initialize the runtime. For example:
-///
-/// ```rust, ignore
-/// fn custom_init() {
-///     let device = Default::default();
-///     burn::backend::wgpu::init_setup::<burn::backend::wgpu::graphics::Vulkan>(
-///         &device,
-///         Default::default(),
-///     );
-/// }
-/// ```
-/// will mean the given device (in this case the default) will be initialized to use Vulkan as the graphics API.
-/// It's also possible to use an existing wgpu device, by using `init_device`.
-///
-/// # Notes
-///
-/// This version of the wgpu backend doesn't use [burn_fusion] to compile and optimize streams of tensor
-/// operations.
-///
-/// You can enable the `fusion` feature flag to add that functionality, which might improve
-/// performance.
-pub type Wgpu<F = f32, I = i32, B = u32> = CubeBackend<cubecl::wgpu::WgpuRuntime, F, I, B>;
+type WgpuInner<C, F, I, B> = CubeBackend<cubecl::wgpu::WgpuRuntime<C>, F, I, B>;
+
+pub type Wgpu<F = f32, I = i32, B = u32> = WgpuInner<AutoCompiler, F, I, B>;
 
 #[cfg(feature = "vulkan")]
 /// Tensor backend that leverages the Vulkan graphics API to execute GPU compute shaders compiled to SPIR-V.
-pub type Vulkan<F = f32, I = i32, B = u8> = Wgpu<F, I, B>;
+pub type Vulkan<F = f32, I = i32, B = u8> = WgpuInner<SpirvCompiler, F, I, B>;
 
 #[cfg(feature = "webgpu")]
 /// Tensor backend that uses the wgpu crate to execute GPU compute shaders written in WGSL.
-pub type WebGpu<F = f32, I = i32, B = u32> = Wgpu<F, I, B>;
+pub type WebGpu<F = f32, I = i32, B = u32> = WgpuInner<WgslCompiler, F, I, B>;
 
 #[cfg(feature = "metal")]
 /// Tensor backend that leverages the Metal graphics API to execute GPU compute shaders compiled to MSL.
-pub type Metal<F = f32, I = i32, B = u8> = Wgpu<F, I, B>;
+pub type Metal<F = f32, I = i32, B = u8> = WgpuInner<MslCompiler, F, I, B>;
 
 #[cfg(test)]
 mod tests {
