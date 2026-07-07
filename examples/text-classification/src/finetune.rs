f4e013c1f9bdb0ce3ff26742a6748acf4ce925fc
Charles23R <renaudcharles14@hotmail.com>
2026-07-07 16:20:19 -0400

    reset text cla example

--- examples/text-classification/src/finetune.rs
+++ examples/text-classification/src/finetune.rs
@@ -2,9 +2,7 @@ use std::sync::Arc;
 
 use burn::data::dataloader::DataLoaderBuilder;
 use burn::data::dataset::transform::SamplerDataset;
-use burn::lr_scheduler::cosine::CosineAnnealingLrSchedulerConfig;
-use burn::lr_scheduler::policy::{LrPolicyConfig, LrPolicyScheduler};
-use burn::module::{LoraConfig, ParamGroup};
+use burn::module::LoraConfig;
 use burn::train::metric::{
     AccuracyMetric, CudaMetric, IterationSpeedMetric, LearningRateMetric, LossMetric,
 };
@@ -48,15 +46,10 @@ pub fn lora_finetuning<D: TextClassificationDataset + 'static>(
     let model = model.load_file(&"model.bpk");
 
     // Apply LoRA to the attention module's query, value, output and feed-forward weights.
-    let r = 4.0;
+    let r = 8.0;
     let mut model = model.apply_lora(&mut LoraMapper::new(LoraConfig::new(r as usize, 2.0 * r)));
     // Reset the classification head with the current dataset's number of classes.
     model.reset_head(D::num_classes());
-
-    // model.reset_head(D::num_classes());
-    // model.transformer = model.transformer.no_grad();
-    // model.embedding_pos = model.embedding_pos.no_grad();
-    // model.embedding_token = model.embedding_token.no_grad();
 
     // Initialize data loaders for training and testing data
     let dataloader_train = DataLoaderBuilder::new(batcher.clone())
@@ -73,86 +66,6 @@ pub fn lora_finetuning<D: TextClassificationDataset + 'static>(
 
     // Initialize learning rate scheduler
     let lr_scheduler = 1e-3;
-
-    // Initialize learner
-    let training = SupervisedTraining::new(artifact_dir, dataloader_train, dataloader_test)
-        .metric_train(CudaMetric::new())
-        .metric_valid(CudaMetric::new())
-        .metric_train(IterationSpeedMetric::new())
-        .metric_train_numeric(LossMetric::new())
-        .metric_valid_numeric(LossMetric::new())
-        .metric_train_numeric(AccuracyMetric::new())
-        .metric_valid_numeric(AccuracyMetric::new())
-        .metric_train_numeric(LearningRateMetric::new())
-        .with_default_checkpointers()
-        .with_training_strategy(strategy.into())
-        .num_epochs(config.num_epochs)
-        .summary();
-
-    // Train the model
-    let result = training.launch(Learner::new(model, optim, lr_scheduler));
-
-    // Save the configuration and the trained model
-    config.save(format!("{artifact_dir}/config.json")).unwrap();
-    result
-        .model
-        .into_record()
-        .save(format!("{artifact_dir}/model"))
-        .unwrap();
-}
-
-// Do a full finetuning of the pre-trained weights.
-pub fn full_finetuning<D: TextClassificationDataset + 'static>(
-    strategy: ExecutionStrategy,
-    dataset_train: D,         // Training dataset
-    dataset_test: D,          // Testing dataset
-    config: ExperimentConfig, // Experiment configuration
-    artifact_dir: &str,       // Directory to save model and config files
-    num_class_before: usize,  // The number of class of the pretrained model
-) {
-    create_artifact_dir(artifact_dir);
-
-    // Initialize tokenizer
-    let tokenizer = Arc::new(BertCasedTokenizer::default());
-
-    // Initialize batcher
-    let batcher = TextClassificationBatcher::new(tokenizer.clone(), config.seq_length);
-
-    // Initialize model
-    let model = TextClassificationModelConfig::new(
-        config.transformer.clone(),
-        num_class_before,
-        tokenizer.vocab_size(),
-        config.seq_length,
-    )
-    .init(&strategy.main_device().clone().autodiff());
-
-    // Load pre-trained weights.
-    let mut model = model.load_file(&"model.bpk");
-
-    // Reset the classification head with the current dataset's number of classes.
-    model.reset_head(D::num_classes());
-
-    // Initialize data loaders for training and testing data
-    let dataloader_train = DataLoaderBuilder::new(batcher.clone())
-        .batch_size(config.batch_size)
-        .num_workers(1)
-        .build(SamplerDataset::new(dataset_train, 25_000));
-    let dataloader_test = DataLoaderBuilder::new(batcher)
-        .batch_size(config.batch_size)
-        .num_workers(1)
-        .build(SamplerDataset::new(dataset_test, 2500));
-
-    // Initialize optimizer
-    let optim = config.optimizer.init();
-
-    // Initialize learning rate scheduler
-    let mut head_group = ParamGroup::from_predicate("output");
-    head_group.exclude(&ParamGroup::from_predicate("transformer"));
-    let lr_scheduler = LrPolicyConfig::new(3e-5.into())
-        .add_group(head_group, 1e-3)
-        .init()
-        .unwrap();
 
     // Initialize learner
     let training = SupervisedTraining::new(artifact_dir, dataloader_train, dataloader_test)
