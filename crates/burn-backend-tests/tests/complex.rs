13d3079c74dae0a195c20346505c84c40bc98d41
Joshua Ferguson <joshua.ferguson.273@gmail.com>
2026-08-10 19:25:59 +0000

    clean up complex test warnings

--- crates/burn-backend-tests/tests/complex.rs
+++ crates/burn-backend-tests/tests/complex.rs
@@ -1,21 +1,15 @@
 use burn_tensor::{Float, Tensor};
 pub type FloatTensor<const D: usize> = Tensor<D, Float>;
+//it's used in the modules
+#[allow(unused)]
 type TestTensor<const D: usize> = burn_tensor::Tensor<D, burn_tensor::Complex>;
-#[cfg(all(
-    feature = "flex",
-    not(any(feature = "fusion", feature = "autodiff", feature = "remote"))
-))]
+
+#[cfg(all(feature = "flex", not(any(feature = "fusion", feature = "remote"))))]
 #[path = "complex/basic.rs"]
 mod basic;
-#[cfg(all(
-    feature = "flex",
-    not(any(feature = "fusion", feature = "autodiff", feature = "remote"))
-))]
+#[cfg(all(feature = "flex", not(any(feature = "fusion", feature = "remote"))))]
 #[path = "complex/numeric.rs"]
 mod numeric;
-#[cfg(all(
-    feature = "flex",
-    not(any(feature = "fusion", feature = "autodiff", feature = "remote"))
-))]
+#[cfg(all(feature = "flex", not(any(feature = "fusion", feature = "remote"))))]
 #[path = "complex/ops.rs"]
 mod ops;
